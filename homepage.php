c2c997a7f934ceaaa55a8d34aa8bb537a2bf2799
24ep <jaroonwit.pool@gmail.com>
2021-06-03 19:50:58 +0700

    update

--- homepage.php
+++ homepage.php
@@ -1,7 +1,7 @@
     <?php
     session_start();
     if (!$_SESSION["login_csg"]){  //check session
-        Header("Location: login_page.php"); //testuusdsd
+        Header("Location: login_page.php"); //testuusdsdddddd
     }else{
     include('get/get_card_content_request.php'); 
     include_once('get/get_count_status.php');
