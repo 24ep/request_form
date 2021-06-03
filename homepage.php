52b4693d662306d682c93b5a72d97d6db223fc6d
24ep <jaroonwit.pool@gmail.com>
2021-06-03 19:57:26 +0700

    update

--- homepage.php
+++ homepage.php
@@ -1,7 +1,7 @@
     <?php
     session_start();
     if (!$_SESSION["login_csg"]){  //check session
-        Header("Location: login_page.php"); //ssdsdsdsdsd
+        Header("Location: login_page.php"); //ssdsdsdsdsdgfgfg
     }else{
     include('get/get_card_content_request.php'); 
     include_once('get/get_count_status.php');
