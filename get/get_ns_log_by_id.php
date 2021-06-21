d2b8c2b158573c355611f5fa6227bc549d8f6242
24ep <jaroonwit.pool@gmail.com>
2021-06-21 18:25:39 +0700

    debug

--- get/get_ns_log_by_id.php
+++ get/get_ns_log_by_id.php
@@ -1,18 +1,34 @@
 <?php
 $id = $_GET['id'];
 $action_table = $_GET['action_table'];
-
-  $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+$actiob_data = $_GET['action_table'];
+$job_number = $_GET['job_number'];
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+if($_GET['action_table']=="24ep"){;
+  $query = "SELECT * FROM u749625779_cdscontent.log_cms where action_at_table='".$action_table."' and job_number =".$job_number or die("Error:" . mysqli_error());
+  $result = mysqli_query($con, $query);
+  while($row = mysqli_fetch_array($result)) {
+     $tr .= 
+     '<tr>
+       <th scope="row">'.$row["action_date"].'</th>
+       <td>'.$row["value_change"].'</td>
+       <td>'.$row["action_from_user"].'</td>
+     </tr>';
+  }
+}elseif($_GET['action_table']=="csg"){
   $query = "SELECT * FROM all_in_one_project.log where action_table='".$action_table."' and action_data_id =".$id or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
-   while($row = mysqli_fetch_array($result)) {
-      $tr .= 
-      '<tr>
-        <th scope="row">'.$row["action_date"].'</th>
-        <td>'.$row["action"].'</td>
-        <td>'.$row["action_by"].'</td>
-      </tr>';
-   }
+  while($row = mysqli_fetch_array($result)) {
+     $tr .= 
+     '<tr>
+       <th scope="row">'.$row["action_date"].'</th>
+       <td>'.$row["action"].'</td>
+       <td>'.$row["action_by"].'</td>
+     </tr>';
+  }
+}
+
+
 ?>
 
 <!doctype html>
@@ -25,17 +41,18 @@ $action_table = $_GET['action_table'];
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>log NS-<?php echo $id;?></title>
   </head>
   <body>
-    <h3>Content Service Gate | log ticket ID NS-<?php echo $id;?></h3>
+    
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <div class="container-md">
-    <table class="table">
+    <h3>Content Service Gate | log ticket ID NS-<?php echo $id;?></h3>
+    <table class="table table-bordered">
       <thead>
         <tr>
           <th scope="col">action date</th>
