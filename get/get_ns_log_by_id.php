95a90e6388a96de6d34e1d717f2a286d5af2d8e1
24ep <jaroonwit.pool@gmail.com>
2021-06-21 18:09:57 +0700

    debug

--- get/get_ns_log_by_id.php
+++ get/get_ns_log_by_id.php
@@ -1,6 +1,9 @@
 <?php
+$id = $_GET['id'];
+$action_table = $_GET['action_table'];
+
   $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
-  $query = "SELECT * FROM all_in_one_project.log " or die("Error:" . mysqli_error());
+  $query = "SELECT * FROM all_in_one_project.log where action_table='".$action_table."' and action_data_id =".$id or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
    while($row = mysqli_fetch_array($result)) {
       $tr .= 
