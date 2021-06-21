2474fd1b4091b00a99d36077fbd9ee28898c2aab
24ep <jaroonwit.pool@gmail.com>
2021-06-21 13:48:12 +0700

    debug

--- get/get_comment_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $query = "SELECT * FROM comment WHERE ticket_type = 'add_new' and ticket_id = 66 ORDER BY id ASC" or die("Error:" . mysqli_error());
-  $result = mysqli_query($con, $query);
-   while($row = mysqli_fetch_array($result)) {
-    $arr_comment = array('type' => 'comment');
-    $arr_comment = array('value' => $row['comment']);
-   }
-   echo $arr_comment["comment"];
-?>
