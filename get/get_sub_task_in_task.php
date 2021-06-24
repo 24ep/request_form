a4505fca55a3a3039ff7ed2f6c7619d3eeae5a41
24ep <jaroonwit.pool@gmail.com>
2021-06-24 12:16:05 +0700

    debug

--- get/get_sub_task_in_task.php
+++ get/get_sub_task_in_task.php
@@ -9,7 +9,7 @@
 
       $subtask .='
         <tr>
-              <td scope="col" style="background: #ededed;"><a href="https://content-service-gate.cdsecommercecontent.ga/homepage.php?tab=v-pills-request_list&fopenticket='.$row["id"].'"></a>
+              <td scope="col" style="background: #ededed;"><a href="https://content-service-gate.cdsecommercecontent.ga/homepage.php?tab=v-pills-request_list&fopenticket='.$row["id"].'">
                NS-'.$row["parent"].'-'.$i.' ('.$row["id"].')</a></td>
               <td scope="col" style="background: #ededed;">'. $row["sku"].'</td>
               <td scope="col" style="background: #ededed;">'. $row["status"].'</td>
