8554317b27db37e8713ec24c789e6cb7a9b79172
24ep <jaroonwit.pool@gmail.com>
2021-06-24 20:54:50 +0700

    debug

--- get/get_list_new_job.php
+++ get/get_list_new_job.php
@@ -159,7 +159,8 @@ if($_POST["from_post"] ==true ){
       echo  "</tr>";
 
       //get sub ticket
-      $result_count=mysqli_query("SELECT count(*) as total from add_new_job where parent = ".$row["id"]);
+      $query_count=mysqli_query("SELECT count(*) as total from add_new_job where parent = ".$row["id"]);
+      $result_count = mysqli_query($con, $query_count);
       $data_count=mysqli_fetch_assoc($result_count);
       $subtask_count = $data_count['total'];
       $query_child = "SELECT * FROM add_new_job where parent = ".$row["id"]." order by id ASC"  or die("Error:" . mysqli_error());
