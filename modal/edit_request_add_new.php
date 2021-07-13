798b2e7f04146ec44d08cf687ae92ae9916610ce
24ep <jaroonwit.pool@gmail.com>
2021-07-13 18:46:11 +0700

    debug

--- modal/edit_request_add_new.php
+++ modal/edit_request_add_new.php
@@ -377,7 +377,7 @@ label#label_file_cme {
                                             $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
                                             $query = "SELECT account.username as username,account.nickname as nickname,account.department as department,sum(new_job.sku) as backlog_sku 
                                             FROM account as account 
-                                            left join add_new_job as new_job on account.username = new_job.follow_up_by and new_job.status <> 'accepted' and  new_job.status <> 'cancel'
+                                            left join add_new_job as new_job on account.username = new_job.follow_assign_name and new_job.status <> 'accepted' and  new_job.status <> 'cancel'
                                             group by account.username 
                                             having account.department like '%follow%'" or die("Error:" . mysqli_error());
                                             $result = mysqli_query($con, $query);
