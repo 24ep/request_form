601ded58b85c7e0e069a565eb4809f237a0ff58f
24ep <jaroonwit.pool@gmail.com>
2021-07-14 14:14:51 +0700

    denug

--- get/get_list_new_job.php
+++ get/get_list_new_job.php
@@ -101,8 +101,12 @@ if($_POST["from_post"] ==true ){
   $status_filter ="1=1";
  }
  if($_SESSION['brand_filter']<>""){
-   
-  $brand_filter ="(brand like '%".$_SESSION['brand_filter']."%' or id = ".str_replace('NS-','',$_SESSION['brand_filter']).")";
+   if(is_numeric($_SESSION['brand_filter'])){
+    $brand_filter ="id = ".str_replace('NS-','',$_SESSION['brand_filter']);
+   }else{
+    $brand_filter ="brand like '%".$_SESSION['brand_filter']."%'";
+   }
+  
  }else{
   $brand_filter ="1=1";
  }
