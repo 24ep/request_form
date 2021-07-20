77ccef64bd176dc1c1c20f72c57249d431f72c5f
24ep <jaroonwit.pool@gmail.com>
2021-07-20 21:18:15 +0700

    debug

--- modal/edit_request_add_new.php
+++ modal/edit_request_add_new.php
@@ -375,11 +375,11 @@ label#label_file_cme {
                                         <select class="form-select" id="op_follow_assign_name" name="op_follow_assign_name" aria-label="Default select example">
                                         <?php
                                             $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
-                                            $query = "SELECT account.username as username,account.nickname as nickname,account.department as department,sum(new_job.sku) as backlog_sku 
+                                            $query = "SELECT account.username as username,account.nickname as nickname,account.department as department,account.status as status ,sum(new_job.sku) as backlog_sku 
                                             FROM account as account 
                                             left join add_new_job as new_job on account.username = new_job.follow_assign_name and new_job.status <> 'accepted' and  new_job.status <> 'cancel' and new_job.status <> 'none'
                                             group by account.username 
-                                            having account.department like '%follow%'" or die("Error:" . mysqli_error());
+                                            having account.department like '%follow%' and account.status = 'Enabled' " or die("Error:" . mysqli_error());
                                             $result = mysqli_query($con, $query);
                                             echo  '<option value="unassign">unassign</option>';
                                             while($row = mysqli_fetch_array($result)) {
