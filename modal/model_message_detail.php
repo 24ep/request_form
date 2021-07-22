134ef7d735e02c1ca5612b76a8c563366c2f63a3
24ep <jaroonwit.pool@gmail.com>
2021-07-22 19:26:03 +0700

    debug

--- modal/model_message_detail.php
+++ modal/model_message_detail.php
@@ -3,11 +3,7 @@ $id=$_POST["id"];
 date_default_timezone_set("Asia/Bangkok");
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
-$query = "SELECT message_box.id as targe_ms_id,
-target.target_username as username,
-target.msid as msid,
-message_box.title as title,
-message_box.description as description 
+$query = "SELECT message_box.id as target_ms_id,target.target_username as username,target.msid as msid,message_box.title as title,message_box.description as description 
   FROM all_in_one_project.target_message_box as target
   left join all_in_one_project.message_box as message_box on target.msid = message_box.id 
   where target.target_username = '".$_SESSION["username"]."' and target.msid=".$id." ORDER BY message_box.id  DESC " or die("Error:" . mysqli_error());
@@ -21,16 +17,16 @@ while($row = mysqli_fetch_array($result)) {
     <div class="modal-body" id="messagebody">
     '.$description.'
     </div>';
-    $sql_update_read = "UPDATE all_in_one_project.target_message_box SET readable = 1,read_date=CURRENT_TIMESTAMP where id=".$row['targe_ms_id'] ;
-    $query_update_read = mysqli_query($con,$sql_update_read);
-    if($query_update_read){
-        // echo '<script>
-        // document.getElementById("ms_bt_id_'.$row['id'].'").innerHTML  ="ตรวจสอบแล้ว";
-        // document.getElementById("ms_bt_id_'.$row['id'].'").className ="btn btn-secondary btn-sm";
-        // </script>';    
-    }
+  
 }
-
+$sql_update_read = "UPDATE all_in_one_project.target_message_box SET readable = 1,read_date=CURRENT_TIMESTAMP where id=".$row['target_ms_id'] ;
+$query_update_read = mysqli_query($con,$sql_update_read);
+if($query_update_read){
+    echo '<script>
+    document.getElementById("ms_bt_id_'.$id.'").innerHTML  ="ตรวจสอบแล้ว";
+    document.getElementById("ms_bt_id_'$id.'").className ="btn btn-secondary btn-sm";
+    </script>';    
+}
 
 
 ?>
