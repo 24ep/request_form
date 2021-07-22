a04853378f58e0c512ba883a6b33beb8544404c5
24ep <jaroonwit.pool@gmail.com>
2021-07-22 16:33:31 +0700

    debug

--- get/get_list_message.php
+++ get/get_list_message.php
@@ -34,12 +34,11 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="messagemodelLabel">Modal
-                    title</h5>
+                <h5 class="modal-title" id="messagemodelLabel"><?php echo $row["title"]; ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                ...
+                <?php echo $row["description"]; ?>
             </div>
 
         </div>
