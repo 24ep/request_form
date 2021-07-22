0d863dce534d156381b166fa1a6bc39370fff9d3
24ep <jaroonwit.pool@gmail.com>
2021-07-22 15:12:22 +0700

    debug

--- get/get_list_message.php
+++ get/get_list_message.php
@@ -15,15 +15,15 @@
 
       if($row["readable"]==0){
         $button_check = "<td style='width: 15%;'><button type='button' class='btn btn-danger btn-sm'><ion-icon name='mail-unread-outline'></ion-icon> ตรวจสอบ</button></td>";
-        unsent($st_font_color);
+        unset($st_font_color);
       }else{
         $button_check = "<td style='width: 15%;'><button type='button' class='btn btn-secondary btn-sm'><ion-icon name='mail-open-outline'></ion-icon> ตรวจสอบ</button></td>";
         $st_font_color = "color: grey"; 
       }
         echo "<tr class='shadow-sm p-3 mb-5 bg-body rounded' style='border-bottom: 1px solid #e0e0e0;'>";
-        echo "<td style='width: 10%;' >".$important."</td>";
-        echo "<td style='width: 15%;' >MS-".$row["id"]."</td>";
-        echo "<td style='width: 60%;width: 60%;' >".$row["title"]."</td>";  
+        echo "<td style='width: 10%;". $st_font_color."' >".$important."</td>";
+        echo "<td style='width: 15%;". $st_font_color."' >MS-".$row["id"]."</td>";
+        echo "<td style='width: 60%;width: 60%;". $st_font_color."' >".$row["title"]."</td>";  
         echo $button_check;
         echo "</tr>";
     } 
