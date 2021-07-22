cf413f5cfe9e6a8eb51c2cbd2973cfe116b0b49e
24ep <jaroonwit.pool@gmail.com>
2021-07-22 20:04:54 +0700

    debug

--- modal/model_message_detail.php
+++ modal/model_message_detail.php
@@ -6,7 +6,7 @@ mysqli_query($con, "SET NAMES 'utf8' ");
 $query = "SELECT message_box.id as target_ms_id,target.target_username as username,target.msid as msid,message_box.title as title,message_box.description as description 
   FROM all_in_one_project.target_message_box as target
   left join all_in_one_project.message_box as message_box on target.msid = message_box.id 
-  where target.msid = ".$id." ORDER BY message_box.id  DESC " or die("Error:" . mysqli_error());
+  where target.username = '".$_SESSION["username"]."' and target.msid = ".$id." ORDER BY message_box.id  DESC " or die("Error:" . mysqli_error());
 $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
     $description = htmlspecialchars_decode($row["description"],ENT_NOQUOTES);
@@ -19,14 +19,14 @@ while($row = mysqli_fetch_array($result)) {
     </div>';
   
 }
-// $sql_update_read = "UPDATE all_in_one_project.target_message_box SET readable = 1,read_date=CURRENT_TIMESTAMP where id=".$row['target_ms_id'] ;
-// $query_update_read = mysqli_query($con,$sql_update_read);
-// if($query_update_read){
-//     // echo '<script>
-//     // document.getElementById("ms_bt_id_'.$id.'").innerHTML  ="ตรวจสอบแล้ว";
-//     // document.getElementById("ms_bt_id_'$id.'").className ="btn btn-secondary btn-sm";
-//     // </script>';    
-// }
+$sql_update_read = "UPDATE all_in_one_project.target_message_box SET readable = 1,read_date=CURRENT_TIMESTAMP where id=".$row['target_ms_id'] ;
+$query_update_read = mysqli_query($con,$sql_update_read);
+if($query_update_read){
+    echo '<script>
+    document.getElementById("ms_bt_id_'.$id.'").innerHTML  ="ตรวจสอบแล้ว";
+    document.getElementById("ms_bt_id_'$id.'").className ="btn btn-secondary btn-sm";
+    </script>';    
+}
 
 
 ?>
