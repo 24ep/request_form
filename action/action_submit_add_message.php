39d4c796151bbc97bb87d9164d8d370bcfb22200
24ep <jaroonwit.pool@gmail.com>
2021-07-22 12:05:15 +0700

    debug

---
+++ action/action_submit_add_message.php
@@ -0,0 +1,119 @@
+<?php
+  function is_image($path)
+  {
+      $a = getimagesize($path);
+      $image_type = $a[2];
+      if(in_array($image_type , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP)))
+      {
+          return true;
+      }
+      return false;
+  }
+ session_start();
+ include('action_send_line_api.php');
+ include('action_add_participant.php');
+ $cr_title = htmlspecialchars($_POST["ms_title"], ENT_QUOTES);
+ $cr_description = htmlspecialchars($_POST["ms_description"], ENT_QUOTES);
+//check size file
+    //action upload file
+    if(isset($_FILES['ms_attachment'])){
+        foreach($_FILES['ms_attachment']['tmp_name'] as $key => $val){
+            $file_name = $_FILES['ms_attachment']['name'][$key];
+            $file_size =$_FILES['ms_attachment']['size'][$key];
+            $file_tmp =$_FILES['ms_attachment']['tmp_name'][$key];
+            $file_type=$_FILES['ms_attachment']['type'][$key];
+            $is_image = is_image($file_tmp);
+            if (($file_size > 2097152 or $file_size ==0) and $file_name <> ""  ) { 
+                $result = '<div class="alert alert-danger">ขนาดไฟล์ต้องไม่เกิน 2MB โปรดทำใฟล์เล็กลง หรือแชร์เป็น link เพื่อเข้าถึงไฟล์</div>';
+                echo '<script>alert("ขนาดไฟล์ต้องไม่เกิน 2MB โปรดทำใฟล์เล็กลง หรือแชร์เป็น link เพื่อเข้าถึงไฟล์")</script>';
+                header( "location: /homepage.php?tab=v-pills-cr&result_cr=".$result);
+                exit();
+        }
+    }
+}
+            foreach($_POST["ms_online_channel"] as $product_website)
+                {
+                    if($sum_product_website <> ""){
+                        $sum_product_website .= ", ".$product_website;
+                    }else{
+                        $sum_product_website .= $product_website;
+                    }
+                };
+        //calculate job number
+        if($_POST["ms_title"]<>""){ $insert_head .= "title";$insert_value .= "'".$cr_title."'";}
+        if($_POST["ms_description"]<>""){ $insert_head .= ",description";$insert_value .= ",'".$cr_description."'";}
+        if($_SESSION["username"]<>""){ $insert_head .= ",create_by";$insert_value .= ",'".$_SESSION["username"]."'";}
+        $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+        mysqli_query($con, "SET NAMES 'utf8' ");
+            $sql = "INSERT INTO message_box (
+            ".$insert_head."
+            )
+            VALUES (
+            ".$insert_value."
+            )";
+            $query = mysqli_query($con,$sql);
+            if($query) {
+                $last_id = $con->insert_id;
+                  //create forder
+                  $fullpath = '../../attachment/csg/'.$last_id."/";
+                  mkdir($fullpath, 0777, true);
+                  // upload image
+                  foreach($_FILES['ms_attachment']['tmp_name'] as $key => $val){
+                    $file_name = $_FILES['ms_attachment']['name'][$key];
+                    $file_size =$_FILES['ms_attachment']['size'][$key];
+                    $file_tmp =$_FILES['ms_attachment']['tmp_name'][$key];
+                    $file_type=$_FILES['ms_attachment']['type'][$key];
+                    $is_image = is_image($file_tmp);
+                    if(isset($_FILES["ms_attachment"])){
+                        if ($file_name <> ""  ) { 
+                        $sql = "INSERT INTO attachment (
+                            file_name,
+                            file_path,
+                            file_size,
+                            file_type,
+                            is_image,
+                            file_owner,
+                            ticket_id,
+                            ticket_type
+                            )       
+                            VALUES(
+                            '".$file_name."',
+                            '".$fullpath."',
+                            '".$file_size."',
+                            '".$file_type."',
+                            '".$is_image."',
+                            '".$_SESSION["username"]."',
+                            '".$last_id."',
+                            'message_box'
+                            )";
+                        $query = mysqli_query($con,$sql);
+                        move_uploaded_file($file_tmp,$fullpath.$file_name);
+                            }
+                        }
+                    }
+                 add_participant($last_id,"message_box");
+                //get key
+                date_default_timezone_set("Asia/Bangkok");
+                $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+                mysqli_query($con, "SET NAMES 'utf8' ");
+                $query = "SELECT  * FROM message_box as ms
+                left join account as account
+                ON ms.create_by = account.username WHERE ms.id = ".$last_id
+                or die("Error:" . mysqli_error());
+                $result =  mysqli_query($con, $query);
+                    while($row = mysqli_fetch_array($result)) {
+                        $key = $row["token_line"];
+                        $title = $row["title"];
+                        //echo '<script>alert("'.$key.'");</script>';
+                    }
+                    if($key<>"" and $key<>null){
+                        sent_line_noti("\n• Created new request\n----------------------------\n• คุณได้ทำการส่ง message\n• Ticket ID : MS-".$last_id."\n".$title,$key);
+                    }
+                $result='<div class="alert alert-success">already create message !<strong> ID '.$last_id.'</strong></div>';
+                header( "location: /homepage.php?tab=v-pills-ms_admin");
+            }else{
+                echo '<div class="alert alert-danger">Error: ' . $sql . '<hr>' . $con->error.'</div>';
+            }
+            mysqli_close($con);
+   // header( "location: https://cdsecommercecontent.ga/powerappsp/job_manage.php");
+?>
