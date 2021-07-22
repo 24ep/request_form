d09b7f37ee603ccce8334e3c576a9a4630c84de8
24ep <jaroonwit.pool@gmail.com>
2021-07-22 17:16:44 +0700

    debug

---
+++ modal/model_message_detail.php
@@ -0,0 +1,21 @@
+<?php
+$id=$_POST["id"];
+date_default_timezone_set("Asia/Bangkok");
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
+$query = "SELECT * FROM all_in_one_project.message_box
+where id = ".$id." ORDER BY id  DESC " or die("Error:" . mysqli_error());
+$result = mysqli_query($con, $query);
+while($row = mysqli_fetch_array($result)) {
+
+    echo '<div class="modal-header">
+    <h5 class="modal-title" id="messagemodelLabel">'.$row["title"].'</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body" id="messagebody">
+    '.$row["description"].'
+    </div>';
+}
+
+
+?>
