d042babf7de03408bf21502d24b4cdf889a33721
24ep <jaroonwit.pool@gmail.com>
2021-07-22 13:34:21 +0700

    debug

--- get/get_list_message.php
+++ get/get_list_message.php
@@ -2,9 +2,9 @@
   date_default_timezone_set("Asia/Bangkok");
   $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
   mysqli_query($con, "SET NAMES 'utf8' ");
-  $query_tg = "SELECT message_box.id as id, target.target_username as username, target.read as read, target.accepted_date as accepted_date, target.read_date as read_date, target.create_date as create_date, target.update_date as update_date, target.msid as msid, message_box.title as title,message_box.description as description 
+  $query_tg = "SELECT message_box.id as id, target.target_username as username, target.readable as readable, target.accepted_date as accepted_date, target.read_date as read_date, target.create_date as create_date, target.update_date as update_date, target.msid as msid, message_box.title as title,message_box.description as description 
   FROM all_in_one_project.target_message_box as target
-  left join all_in_one_project.message_box as message_box on target.msid = message_box.id where target.target_username = '".$_SESSION["username"]."' ORDER BY id DESC " or die("Error:" . mysqli_error());
+  left join all_in_one_project.message_box as message_box on target.msid = message_box.id where target.target_username = '".$_SESSION["username"]."' ORDER BY message_box.id  DESC " or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query_tg);
   while($row = mysqli_fetch_array($result)) {
         echo "<tr>";
