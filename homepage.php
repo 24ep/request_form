4b92535f816a95b035c2c6d42b54b62767f7a30d
24ep <jaroonwit.pool@gmail.com>
2021-07-22 14:28:08 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -848,15 +848,16 @@
                                 </div>
                                 <div class="container-fluid" style="border-radius: 10px;width: 95%;">
                                 <div class="row">
-                                    <div class="col-4">
+                                    <div class="col-2">
                                         <div class="list-group" id="list-tab" role="tablist">
-                                        <a class="list-group-item list-group-item-action active" id="list-inbox-list" data-bs-toggle="list" href="#list-inbox" role="tab" aria-controls="list-inbox">Inbox</a>
+                                        <a class="list-group-item list-group-item-action active" id="list-important-list" data-bs-toggle="list" href="#list-important" role="tab" aria-controls="list-important">Important</a>
+                                        <a class="list-group-item list-group-item-action active" id="list-update-list" data-bs-toggle="list" href="#list-update" role="tab" aria-controls="list-update">Updated</a>
                                         <a class="list-group-item list-group-item-action" id="list-send-list" data-bs-toggle="list" href="#list-send" role="tab" aria-controls="list-send">Send</a>
                                         </div>
                                     </div>
-                                    <div class="col-8">
+                                    <div class="col-10">
                                         <div class="tab-content" id="nav-tabContent">
-                                        <div class="tab-pane fade show active" id="list-inbox" role="tabpanel" aria-labelledby="list-inbox-list">
+                                        <div class="tab-pane fade" id="list-important" role="tabpanel" aria-labelledby="list-important-list">
                                         <table class="table table-hover table-borderless  "
                                                 style="margin: 0px;font-size: 13px;vertical-align:middle;text-align:center;width:100%">
                                                 <thead style="background-color: rgba(0, 0, 0, 0);color: #908e8e;" class="fixed">
@@ -871,10 +872,27 @@
                                                     <?php include('get/get_list_message.php'); ?>
                                                 </tbody>
                                             </table>
-
-
                                         </div>
-                                        <div class="tab-pane fade" id="list-send" role="tabpanel" aria-labelledby="list-send-list">...</div>
+                                        <div class="tab-pane fade show active" id="list-update" role="tabpanel" aria-labelledby="list-update-list">
+                                        
+                                        </div>
+                                        <div class="tab-pane fade" id="list-send" role="tabpanel" aria-labelledby="list-send-list">
+                                        <table class="table table-hover table-borderless  "
+                                                style="margin: 0px;font-size: 13px;vertical-align:middle;text-align:center;width:100%">
+                                                <thead style="background-color: rgba(0, 0, 0, 0);color: #908e8e;" class="fixed">
+                                                    <tr>
+                                                        <th scope="col">Important</th>
+                                                        <th scope="col">Message ID</th>
+                                                        <th scope="col">title</th>
+                                                        <th scope="col">check</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="">
+                                                    <?php include('get/get_list_send_message.php'); ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+
                                    
                                         </div>
                                     </div>
