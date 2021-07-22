d94aa4c5e4d58d96ef10d91edc0f919a7fff6f46
24ep <jaroonwit.pool@gmail.com>
2021-07-22 22:46:05 +0700

    debug

--- get/get_list_new_job.php
+++ get/get_list_new_job.php
@@ -231,11 +231,11 @@ if($_POST["from_post"] ==true ){
       }
       if($_SESSION['status_filter']<>""){
         if($row["config_type"]=="parent"){
-          if($sub_ticket<>"" and $sub_ticket<>null){
+          if( isset($sub_ticket)){
             echo $ticket.$sub_ticket;
           }
         }else{
-          if($sub_ticket<>"" and $sub_ticket<>null){
+          if(isset($sub_ticket)){
             echo $ticket.$sub_ticket;
           }else{
             echo $ticket;
@@ -243,7 +243,7 @@ if($_POST["from_post"] ==true ){
           
         }
        }else{
-        if($sub_ticket<>"" and $sub_ticket<>null){
+        if(isset($sub_ticket)){
           echo $ticket.$sub_ticket;
         }else{
           echo $ticket;
