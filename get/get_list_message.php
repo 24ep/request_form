18aa69256b117f84f204a66eede77008b5433b7e
24ep <jaroonwit.pool@gmail.com>
2021-07-23 13:26:09 +0700

    debug

--- get/get_list_message.php
+++ get/get_list_message.php
@@ -17,7 +17,7 @@
         $button_check = "<td style='width: 20%;'><button type='button' class='btn btn-danger btn-sm' name='ms_bt_id_".$row["id"]."' id='ms_bt_id_".$row["id"]."' data-bs-toggle='modal' onclick='message_get(".$row["id"].")' data-bs-target='#messagemodel'><ion-icon name='mail-unread-outline'></ion-icon> ตรวจสอบ</button></td>";
         unset($st_font_color);
       }else{
-        $button_check = "<td style='width: 20%;'><button type='button' class='btn btn-light btn-sm' name='ms_bt_id_".$row["id"]."' id='ms_bt_id_".$row["id"]."' data-bs-toggle='modal 'onclick='message_get(".$row["id"].")'  data-bs-target='#messagemodel'><ion-icon name='mail-open-outline'></ion-icon> ตรวจสอบแล้ว</button></td>";
+        $button_check = "<td style='width: 20%;'><button type='button' class='btn btn-light btn-sm' name='ms_bt_id_".$row["id"]."' id='ms_bt_id_".$row["id"]."' data-bs-toggle='modal' onclick='message_get(".$row["id"].")' data-bs-target='#messagemodel'><ion-icon name='mail-open-outline'></ion-icon> ตรวจสอบ</button></td>";
         $st_font_color = "color: grey"; 
         unset($st_font_color);
       }
