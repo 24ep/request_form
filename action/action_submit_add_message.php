4a3f9f78507da8437324e8f99e5a89b840784836
24ep <jaroonwit.pool@gmail.com>
2021-07-30 14:16:35 +0700

    debug

--- action/action_submit_add_message.php
+++ action/action_submit_add_message.php
@@ -38,7 +38,7 @@
         if($_SESSION["username"]<>""){ $insert_head .= ",create_by";$insert_value .= ",'".$_SESSION["username"]."'";}
         // $description = htmlspecialchars_decode($_POST["ms_description"],ENT_QUOTES);
         $description = htmlspecialchars_decode($_POST["ms_description"]);
-        $description  = strip_tags($description);
+        // $description  = strip_tags($description);
         $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
         mysqli_query($con, "SET NAMES 'utf8' ");
             $sql = "INSERT INTO message_box (
