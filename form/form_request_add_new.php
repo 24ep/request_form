367cf7a1a952a279dc9f3fc3fd2a9880235d444f
24ep <jaroonwit.pool@gmail.com>
2021-07-30 16:02:25 +0700

    debug

--- form/form_request_add_new.php
+++ form/form_request_add_new.php
@@ -8,11 +8,11 @@ session_start();
             attribute_option.attribute_id as attribute_id,
             attribute_option.attribute_option as attribute_option,
             attribute_option.function as function,
-            attribute_entity.attribute_code 
+            attribute_entity.attribute_code as attribute_code
             FROM content_service_gate.attribute_option as attribute_option
             left join content_service_gate.attribute_entity as attribute_entity
             on attribute_option.attribute_id = attribute_entity.attribute_id 
-            where attribute_code =  '".$attribute_code."' and function='".$function."' 
+            where attribute_entity.attribute_code =  '".$attribute_code."' and attribute_option.function='".$function."' 
             ORDER BY id asc" or die("Error:" . mysqli_error());
             $result = mysqli_query($con, $query);
             $option_set .= '<option value=""></option>';
