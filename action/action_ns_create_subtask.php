30f7559e41fbb82ebad55eed521f8349663cbe50
24ep <jaroonwit.pool@gmail.com>
2021-08-03 13:25:07 +0700

    debug

--- action/action_ns_create_subtask.php
+++ action/action_ns_create_subtask.php
@@ -56,7 +56,9 @@ foreach ($array_number_subtask as $number_of_sku) {
                 }
             }
                 add_participant($_POST['id'],"add_new_job");
+                add_participant($last_id,"add_new_job");
                 insert_log("create new sub-ticket (".$last_id.")","add_new_job",$_POST['id']);
+                insert_log("create new sub-ticket (".$last_id.")","add_new_job",$last_id);
                 echo 'success';
 
 
