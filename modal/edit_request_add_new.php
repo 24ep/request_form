e5ca56cdf98a11eb2bfbf5b92b675e0aa5cec301
24ep <jaroonwit.pool@gmail.com>
2021-08-04 14:13:56 +0700

    debug

--- modal/edit_request_add_new.php
+++ modal/edit_request_add_new.php
@@ -252,7 +252,10 @@ label#label_file_cme {
                     if($start_checking_date<>"" and $start_checking_date<>null){
                         $allow_task_ticket = "disabled";
                         $help_start = "<small style='color:Red'><strong>".$follow_up_by."</strong> has been start checking information</small>";
-                    }elseif($follow_assign_name <> $_SESSION["username"]){
+                        elseif(!isset($follow_assign_name)){
+                            $allow_task_ticket = "disabled";
+                            $help_start = "<small style='color:Red'>this ticket is unassign</small>";}
+                    elseif($follow_assign_name <> $_SESSION["username"]){
                         $allow_task_ticket = "disabled";
                         $help_start = "<small style='color:Red'>Only ".$follow_assign_name."(assignee) can start checking information</small>";
                     }else{unset($allow_task_ticket);unset($help_start);} 
