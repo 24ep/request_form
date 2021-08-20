656c7951a0f0442df93d5a9401c1e1aced0ee691
24ep <jaroonwit.pool@gmail.com>
2021-08-20 14:59:57 +0700

    debug

--- modal/edit_request_add_new.php
+++ modal/edit_request_add_new.php
@@ -355,7 +355,7 @@ label#label_file_cme {
                                             <?php  if($_SESSION['username']=='poojaroonwit'){?>
                                             <div >
                                             <label for="sku_accepted">SKU list</label>
-                                                <textarea class="form-control" id="sku_accepted" name="sku_accepted"  placeholder="CDS3466644 \r\n CDS23443356 \r\n CDS23445356" rows="3" style="height: 100px"></textarea>
+                                                <textarea style="font-size:12px" class="form-control" id="sku_accepted" name="sku_accepted"  placeholder="CDS3466644&#10;CDS23443356&#10;CDS23445356&#10;CDS267356" rows="3" style="height: 100px"></textarea>
                                                 
                                             </div>
                                             <?php } ?>
