26d3e0331044071191a2fa0d4f74be74b7255eeb
24ep <jaroonwit.pool@gmail.com>
2021-08-29 01:27:27 +0700

    debug

---
+++ action/action_stamp_send_mail_itemize.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Bangkok");
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
+$id = $_POST["id"];
+$ticket_type = "ins_add_new_job";
+$dt= new DateTime();
+$dt_formate = $dt->format('Y-m-d H:i:s');
+$nickname = $_SESSION["username"];
+$send_type= $_POST["send_type"];
+$subject_mail =$_POST["subject_mail"];
+  //add comment
+  	$sql = "INSERT INTO comment (
+      ticket_id,
+      comment,
+      ticket_type,
+      comment_by
+    )
+      VALUES(
+        ".$id.",
+        '[stamp_send_mail] ".$subject_mail."',
+        'ins_add_new_job',
+        '".$nickname."'
+      )";
+      $query = mysqli_query($con,$sql);
+
+      $sql_update = "UPDATE add_new_job SET subject_mail = '".$subject_mail."' WHERE id=".$id;
+      $query = mysqli_query($con,$sql_update);
+
+      if($query){
+          echo 'success !';
+      }else{
+        echo 'Error: ' . $sql . '<hr>' . $con->error.'';
+      }
+
+      mysqli_close($con);
+
+?>
