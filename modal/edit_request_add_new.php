1310f29c9fbcfb3dc8e62061e091c3b133debe04
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-09-01 22:41:01 +0700

    debug

--- modal/edit_request_add_new.php
+++ modal/edit_request_add_new.php
@@ -261,8 +261,7 @@ label#label_file_cme {
                                             <textarea style="font-size:12px" onchange="sku_checking()" class="form-control" id="sku_checking"
                                                 name="sku_accepted"
                                                 placeholder="ตรวจสอบ sku ตามตัวอย่างด้านล่าง วางตามตัวอย่างด้านล่าง&#10;&#10;CDS3466644&#10;CDS2443356&#10;CDS2487356"
-                                                rows="20" style="height: 300px">
-                                            </textarea>
+                                                rows="20" style="height: 300px"></textarea>
                                     </div>
                                     <div class="col-6">
                                             <div id="sku_checking_result">
