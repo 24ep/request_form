1bb0ec1e96481b9a2b22ed078bc7d4220c0143bb
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-09-04 02:13:59 +0700

    debug

--- action/action_force_change_csg_id_of_sku.php
+++ action/action_force_change_csg_id_of_sku.php
@@ -6,6 +6,7 @@
         $sku_change =trim($sku_change," ");
         date_default_timezone_set("Asia/Bangkok");
         $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+        
         $sku_change_array = explode("\n", $sku_change);
         $sku_list_array = array();
         $sku_just_array = array();
@@ -16,23 +17,24 @@
             }
             
         }
-        if($_POST["be_status_on_change"]=="cancel"){
-            // cancel old ticket
-            $sku_just =  implode(',',$sku_just_array);
-            $query_dulp_sku = "SELECT * FROM sku_list where sku in (".$sku_just.") ORDER BY id DESC " or die("Error:" . mysqli_error());
-            $result_dulp_sku = mysqli_query($con, $query_dulp_sku);
-            $sku_csg_ticket_id_nc = array();
-            while($row_dulp_sku = mysqli_fetch_array($result_dulp_sku)) {
-                array_push($sku_csg_ticket_id_nc,$row_dulp_sku["csg_id"]);
-            }
-            $sku_csg_ticket_id_nc = array_unique($sku_csg_ticket_id_nc);
-            $sku_csg_ticket_id_nc_text =  implode(',',$sku_csg_ticket_id_nc);
-            $sql_cancel_old  = "UPDATE add_new_job SET cancel_resone = '".$_SESSION["username"]." had been cancel sine of move sku to ticket NS-".$id." ".date("Y-m-d H:i:s")."' , status = 'cancel',cancel_date = CURRENT_TIMESTAMP  WHERE id in (".$sku_csg_ticket_id_nc_text .")";
-            $query_time_zone = mysqli_query($con,"SET time_zone = 'Asia/Bangkok';");
-            $query_cancel_old = mysqli_query($con,$sql_cancel_old);
-            // end cancel old ticket
-        }
         $sku_list = implode(',',$sku_list_array);
+        // if($_POST["be_status_on_change"]=="cancel"){
+        //     // cancel old ticket
+        //     $sku_just =  implode(',',$sku_just_array);
+        //     $query_dulp_sku = "SELECT * FROM sku_list where sku in (".$sku_just.") ORDER BY id DESC " or die("Error:" . mysqli_error());
+        //     $result_dulp_sku = mysqli_query($con, $query_dulp_sku);
+        //     $sku_csg_ticket_id_nc = array();
+        //     while($row_dulp_sku = mysqli_fetch_array($result_dulp_sku)) {
+        //         array_push($sku_csg_ticket_id_nc,$row_dulp_sku["csg_id"]);
+        //     }
+        //     $sku_csg_ticket_id_nc = array_unique($sku_csg_ticket_id_nc);
+        //     $sku_csg_ticket_id_nc_text =  implode(',',$sku_csg_ticket_id_nc);
+        //     $sql_cancel_old  = "UPDATE add_new_job SET cancel_resone = '".$_SESSION["username"]." had been cancel sine of move sku to ticket NS-".$id." ".date("Y-m-d H:i:s")."' , status = 'cancel',cancel_date = CURRENT_TIMESTAMP  WHERE id in (".$sku_csg_ticket_id_nc_text .")";
+        //     $query_time_zone = mysqli_query($con,"SET time_zone = 'Asia/Bangkok';");
+        //     $query_cancel_old = mysqli_query($con,$sql_cancel_old);
+        //     // end cancel old ticket
+        // }
+       
         $sql_sku = "INSERT INTO sku_list (
             sku,create_by,csg_id )
              VALUES 
