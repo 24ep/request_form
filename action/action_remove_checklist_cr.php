be47959721edb5d22f4de4d1c549137f42aa39a5
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-15 14:47:11 +0700

    debug

--- action/action_remove_checklist_cr.php
+++ action/action_remove_checklist_cr.php
@@ -5,7 +5,7 @@ session_start();
   date_default_timezone_set("Asia/Bangkok");
   $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
   mysqli_query($con, "SET NAMES 'utf8' ");
-	$sql = "DELETE FROM checklist_of_content_request WHERE id = ".$ticket_id ;
+	$sql = "DELETE FROM checklist_of_content_request WHERE id = ".$list_id ;
 	$query = mysqli_query($con,$sql);
     mysqli_close($con);
     include('http://content-service-gate.cdsecommercecontent.ga/get/get_checklist_cr.php?id='.$ticket_id);
