36e1b25a5b4ee95050775dd3efb4715182a85301
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-15 12:58:50 +0700

    debug

--- get/get_content_request_detail.php
+++ get/get_content_request_detail.php
@@ -418,8 +418,10 @@ function comment_cr_id(id) {
 }
 
 function add_cr_list() {
+    var node = document.createElement("DIV");
     var new_list = document.getElementById("list_cr")
     var list_box = document.getElementById("list_cr_box")
+    node.appendChild(list_box);
     document.getElementById("list_box").appendChild(new_list);
    
 }
