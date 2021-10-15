4bc37ee7455b3c8d21e9b164db31c8083d0fa873
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-15 17:54:01 +0700

    debug

--- get/get_checklist_cr.php
+++ get/get_checklist_cr.php
@@ -6,7 +6,7 @@ function return_s_select_box_cl_cr($current_value,$attr_id){
       WHERE attribute_id = ".$attr_id." and function = 'cl_content_request' ORDER BY option_id ASC" or die("Error:" . mysqli_error());
       $result_op = mysqli_query($con, $query_op);
       while($option = mysqli_fetch_array($result_op)) {
-      if($option["attribute_option"]==""){
+      if($current_value==""){
             $option_element .= "<option selected value=''></option>";
        }else{
         if($option["attribute_option"]==$current_value){
