b46e472a9a46fc13b0ff9bbe92a0a6914166e63a
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-18 23:32:08 +0700

    debug

--- line_api/register/action/update_account_detail.php
+++ line_api/register/action/update_account_detail.php
@@ -2,12 +2,12 @@
  $username = $_POST['username'];
  $attribute_update_name = $_POST['attribute_update_name'];
  $attribute_update_value = $_POST['attribute_update_value'];
- $displayName = $_POST['displayName'];
+ $pictureUrl = $_POST['pictureUrl'];
  session_start();
     $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
     mysqli_query($con, "SET NAMES 'utf8' ");
     date_default_timezone_set("Asia/Bangkok");
-    $sql = "UPDATE account SET ".$attribute_update_name." = '".$attribute_update_value."',profile_url = '". $displayName."'  WHERE username='".$username."'";
+    $sql = "UPDATE account SET ".$attribute_update_name." = '".$attribute_update_value."',profile_url = '". $pictureUrl."'  WHERE username='".$username."'";
     $query_time_zone = mysqli_query($con,"SET time_zone = 'Asia/Bangkok';");
     $query = mysqli_query($con,$sql);
 	if($query) {
