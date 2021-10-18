d3016cfa4c09b12d6f06f8a7ee64d9daa2800545
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-19 00:41:18 +0700

    debug

--- line_api/register/action/update_account_detail.php
+++ line_api/register/action/update_account_detail.php
@@ -27,14 +27,14 @@
 
         }
         include("https://content-service-gate.cdsecommercecontent.ga/line_api/register/action/send_bubble_register.php?user_id=".$attribute_update_value."&id=".$id."&username=".$username."&tell=".$tell."&dept=".$dept."&header=Connected !");
-        echo '<div style="  height: 200px;
-        width: 400px;
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        text-align: center;
-        margin-top: -100px;
-        margin-left: -200px;"><h6><strong><ion-icon name="checkmark-circle-outline"></ion-icon>Success ! </strong></h6><p>คุณสามารถปิดหน้าต่างนี้เพื่อส่งเริ่มส่งข้อความ</p></div>';
+        // echo '<div style="  height: 200px;
+        // width: 400px;
+        // position: fixed;
+        // top: 50%;
+        // left: 50%;
+        // text-align: center;
+        // margin-top: -100px;
+        // margin-left: -200px;"><h6><strong><ion-icon name="checkmark-circle-outline"></ion-icon>Success ! </strong></h6><p>คุณสามารถปิดหน้าต่างนี้เพื่อส่งเริ่มส่งข้อความ</p></div>';
        
 	}else{
         echo '<script>alert("Error: ' . $sql . '\n\n' . $con->error.'")</script>';
