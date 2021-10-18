b6e3080e25a224c1c3f0fedca9936203c5471757
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2021-10-18 13:11:59 +0700

    release checkllist content request

--- get/get_content_request_detail.php
+++ get/get_content_request_detail.php
@@ -233,6 +233,23 @@ $result = mysqli_query($con, $query);
         $cr_edit_effective_date = "'cr_edit_effective_date'";
         $cr_edit_content_request_reson= "'cr_edit_content_request_reson'";
         $cr_edit_request_by= "'cr_edit_request_by'";
+           if(strpos($_SESSION["department"],"Content")!==false or $_SESSION["department"]==""){
+            ?>
+         
+              <div class="d-grid gap-1 d-md-flex justify-content-md-end" style="margin-bottom: 8px;">
+                <button class="btn btn-primary btn-sm" onclick="add_cr_list(<?php echo $id; ?>)" ><ion-icon name="add-outline"></ion-icon> Add</button>
+              </div>
+             <?php 
+           }
+            ?>
+              <ul id="checklist_box" style="padding: 5px;">
+               <?php     include('http://content-service-gate.cdsecommercecontent.ga/get/get_checklist_cr.php?id='.$id); ?>
+              </ul>
+         
+            
+
+
+            <?php
         
 echo "<script>console.log('".$_SESSION["department"]."');</script>";
         if(strpos($_SESSION["department"],"Content")!==false or $_SESSION["department"]==""){
@@ -256,22 +273,9 @@ echo "<script>console.log('".$_SESSION["department"]."');</script>";
             <hr>';
             // end contact
             // ---check list
-          if($_SESSION['username']=='poojaroonwit'){
-            ?>
-    
-              <div class="d-grid gap-1 d-md-flex justify-content-md-end" style="margin-bottom: 8px;">
-                <button class="btn btn-primary btn-sm" onclick="add_cr_list(<?php echo $id; ?>)" ><ion-icon name="add-outline"></ion-icon> Add</button>
-              </div>
-              <ul id="checklist_box" style="padding: 5px;">
-               <?php     include('http://content-service-gate.cdsecommercecontent.ga/get/get_checklist_cr.php?id='.$id); ?>
-              </ul>
-         
-            
+      
 
-
-            <?php
-
-          }
+        
             echo '<hr>';
             // ---- end check list
             // start detail
