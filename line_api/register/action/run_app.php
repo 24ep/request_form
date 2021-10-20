7b848f074391f302504814cbfb5755900845819e
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-21 01:04:00 +0700

    debug

--- line_api/register/action/run_app.php
+++ line_api/register/action/run_app.php
@@ -1,3 +1,40 @@
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200&display=swap" rel="stylesheet">
+    <title>Content Service Gate</title>
+    <style>
+    /* #pictureUrl { } */
+    body {
+        font-family: 'Prompt', sans-serif;
+    }
+
+    .form-label {
+        margin-bottom: .5rem;
+        font-weight: 600;
+        font-size: smaller;
+    }
+
+    .form-floating>.form-control {
+        font-weight: 600;
+    }
+
+    .form-floating {
+        font-size: small;
+        ;
+        font-weight: 600;
+    }
+    </style>
+</head>
+
+<body>
 <script src="https://static.line-scdn.net/liff/edge/versions/2.9.0/sdk.js"></script>
     <script>
     function runApp() {
@@ -39,3 +76,4 @@
         }
     }, err => console.error(err.code, error.message));
     </script>
+    </body>
