026502c33fec65f34632b9b0f5327ed8febc3f00
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-21 16:07:20 +0700

    debug

--- line_api/register/form/content_request_form.php
+++ line_api/register/form/content_request_form.php
@@ -59,6 +59,8 @@
             <input type="file" id="file-input_camera" name="image" accept="image/*" multiple capture="environment">
             <label for="formFile" class="form-label">ถ่ายภาพ หรือ เลือกไฟล์ 2</label>
             <input type="file" id="file-input" name="image"  multiple capture>
+            <label for="formFile" class="form-label">ถ่ายภาพ หรือ เลือกไฟล์ 2</label>
+            <input type="file" id="file-input" name="image"  multiple >
             
         </div>
         <div class="mb-3">
@@ -106,6 +108,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
