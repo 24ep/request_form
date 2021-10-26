005ab00403c2cbde93b33ee60305d1930567a68c
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-26 13:17:02 +0700

    debug

--- line_api/register/form/get_list_request.php
+++ line_api/register/form/get_list_request.php
@@ -8,17 +8,18 @@ date_default_timezone_set("Asia/Bangkok");
     on ac.username=cr.request_by WHERE line_user_id ='".$userId."'";
     $query_gb  = mysqli_query($con,$sql_gb);
     while($row = mysqli_fetch_array($query_gb)) {
-        $list_cr  .=   '<li class="list-group-item">\
+        $list_cr  .=   '<li class="list-group-item"><div class="row">
         <div class="col-10">
-        <span><strong style="color:red"> CR-'.$row["id"].'</strong> | <span style="
+        <span><ion-icon name="ticket-outline"></ion-icon><strong style="color:red"> CR-'.$row["id"].'</strong> | <span style="
         background: bisque;
-        padding: 0px 5px 0px 5px;
+        padding: 0px 10px 0px 10px;
         font-weight:600
     "> '.$row["status"].'</span></span>
         <br><small style="color:#b8aeae:font-size:11px">Created : '.$row["create_date"].'</small>
         </div>
         <div class="col-2">
         <ion-icon name="chevron-forward-outline"></ion-icon>
+        </div>
         </div>
         </li>';
     }
