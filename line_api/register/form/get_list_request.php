9991f00a4d2c499a7a3c9e2c3f1e99dc346f32e1
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-26 13:01:53 +0700

    denig

--- line_api/register/form/get_list_request.php
+++ line_api/register/form/get_list_request.php
@@ -9,8 +9,8 @@ date_default_timezone_set("Asia/Bangkok");
     $query_gb  = mysqli_query($con,$sql_gb);
     while($row = mysqli_fetch_array($query_gb)) {
         $list_cr  .=   '<li class="list-group-item">
-        <span><strong> CR-'.$row["id"].'</strong> | '.$row["status"].'</span>
-        <small>'.$row["create_date"].'</small>
+        <span><strong style="color:red"> CR-'.$row["id"].' | </strong> '.$row["status"].'</span>
+        <br><small style="color:#b8aeae:font-size:11px">create : '.$row["create_date"].'</small>
         </li>';
     }
 echo $list_cr;
