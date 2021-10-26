b38c4932c7fe8e7bf947c91b934c7de8b92cf2a9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-26 12:01:45 +0700

    debug

--- line_api/register/form/list_request.php
+++ line_api/register/form/list_request.php
@@ -4,10 +4,12 @@ $userId = "<script>document.writeln(user_id_line);</script>";
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 date_default_timezone_set("Asia/Bangkok");
-    $sql_gb = "SELECT ac.username,ar.description,ar.title.ar.status from account as ac left join content_request as cr on ac.username=cr.request_by WHERE line_user_id ='".$userId."'";
+    $sql_gb = "SELECT ac.username,ar.description,ar.title.ar.status from account as ac 
+    left join content_request as cr 
+    on ac.username=cr.request_by WHERE line_user_id ='".$userId."'";
     $query_gb  = mysqli_query($con,$sql_gb);
     while($row = mysqli_fetch_array($query_gb)) {
-        $list_cr  .=   '<li class="list-group-item">'.$row["title"].' | '.$row["status"]..'</li>';
+        $list_cr  .=   '<li class="list-group-item">'.$row["title"].' | '.$row["status"].'</li>';
     }
 
     
