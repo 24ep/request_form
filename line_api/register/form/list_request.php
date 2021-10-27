b723061b01bc5165958572e1e985c01a21d5090a
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-27 13:42:51 +0700

    debug

--- line_api/register/form/list_request.php
+++ line_api/register/form/list_request.php
@@ -10,7 +10,7 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@400&display=swap" rel="stylesheet">
     <title>Content Service Gate | Profile</title>
     <style>
     /* #pictureUrl { } */
