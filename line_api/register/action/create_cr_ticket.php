e665a5123834785e4e2b8a67b4ec8e1f306381a8
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-10-28 11:32:38 +0700

    debug

--- line_api/register/action/create_cr_ticket.php
+++ line_api/register/action/create_cr_ticket.php
@@ -314,7 +314,12 @@ date_default_timezone_set("Asia/Bangkok");
                     }
                   $pre_detail_request =  substr($_POST["detail_request"],0,80);
                  add_participant($last_id,"content_request",$username);
-                 bb_confirm_ticket($last_id ,$userId,$pre_detail_request."...",$_POST["priority"],"https://cdsecommercecontent.ga/attachment/csg/".$last_id."/".$file_name, date("Y-m-d h:i:sa"));
+                 if($file_name==""){
+                  bb_confirm_ticket($last_id ,$userId,$pre_detail_request."...",$_POST["priority"],"https://cdsecommercecontent.ga/img/defa.png", date("Y-m-d h:i:sa"));
+                 }else{
+                  bb_confirm_ticket($last_id ,$userId,$pre_detail_request."...",$_POST["priority"],"https://cdsecommercecontent.ga/attachment/csg/".$last_id."/".$file_name, date("Y-m-d h:i:sa"));
+                 }
+                 
                 //get key
                 // date_default_timezone_set("Asia/Bangkok");
                 // $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
