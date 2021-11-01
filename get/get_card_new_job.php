a4b51f6cab60f00c34e351c00a3f0c64e0d0624e
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-11-01 12:59:20 +0700

    debug

--- get/get_card_new_job.php
+++ get/get_card_new_job.php
@@ -10,16 +10,25 @@ function get_card_new_job($status,$username){
     $limit = 'limit 5';
     $sort = 'update_date DESC';
   }else{
-    $sort = 'id ASC';
+    $sort = 'launch_date ASC';
   }
   $query = "SELECT * FROM all_in_one_project.add_new_job  where follow_assign_name = '".$username."' and ".$status." ORDER by ".$sort." ".$limit ;
   $result = mysqli_query($con, $query);
   $result_count = mysqli_query($con, $query_count);
      while($row = mysqli_fetch_array($result)) {
+      $border =   'border-color: transparent;';
     echo    '
     <div class="card" id="card_fl_'.$row["id"].'" style="margin-top:15px;'.$border.'">
         <div class="card-body shadow" >
-            <h6 class="card-title" style="font-size:14px"><strong style="color:red">NS-'.$row["id"].'</strong> '.$row["brand"].' '.$row["sku"].' </h6>
+            <h6 class="card-title" style="font-size:14px"><strong style="color:red">NS-'.$row["id"].'</strong> '.$row["brand"].' '.$row["sku"].' SKUs </h6>
+            <div class="row">
+              <div class="col" style="color:gray;font-size:12px" >status : </div>
+              <div class="col">'.$row["status"].' </div>
+            </div>
+            <div class="row" style="color:gray;font-size:12px">
+              <div class="col">launch  : </div>
+              <div class="col">'.$row["launch_date"].' </div>
+            </div>
         </div>
     </div>   
     ';
