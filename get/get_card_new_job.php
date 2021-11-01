654c08594c23d1bddfcd36c5cb00260018421906
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-11-01 13:44:01 +0700

    debug

--- get/get_card_new_job.php
+++ get/get_card_new_job.php
@@ -1,7 +1,7 @@
 <?php
 function get_card_new_job($status,$username){
-  if($username==""){
-      $username==$_SESSION["username"];
+  if($username==''){
+      $username=$_SESSION["username"];
   }
   date_default_timezone_set("Asia/Bangkok");
   $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
@@ -18,17 +18,20 @@ function get_card_new_job($status,$username){
   $result_count = mysqli_query($con, $query_count);
      while($row = mysqli_fetch_array($result)) {
       $border =   'border-color: transparent;';
+      if($row["launch_date"]==""){
+        $launch_date="no launch date";
+      }
     echo    '
     <div class="" id="card_fl_'.$row["id"].'" onclick="call_edit_add_new_modal('.$row['id'].')" style="margin-top:15px;'.$border.'">
         <div class="card-body shadow" >
             <h6 class="card-title" onclick="call_edit_add_new_modal('.$row['id'].')" style="font-size:14px"><strong style="color:red">NS-'.$row["id"].'</strong> '.$row["brand"].' '.$row["sku"].' SKUs </h6>
-            <div class="row" style="margin-bottom:3px;margin-top:3px" onclick="call_edit_add_new_modal('.$row['id'].')">
-              <div class="col-3" style="color:gray;font-size:12px" >status : </div>
+            <div class="row" style="margin-bottom:3px;margin-top:3px;color:gray;font-size:12px" onclick="call_edit_add_new_modal('.$row['id'].')">
+              <div class="col-3">status : </div>
               <div class="col-9">'.$row["status"].' </div>
             </div>
-            <div class="row" style="margin-bottom:3px;margin-top:3px" onclick="call_edit_add_new_modal('.$row['id'].')" style="color:gray;font-size:12px">
+            <div class="row" style="margin-bottom:3px;margin-top:3px;color:gray;font-size:12px" onclick="call_edit_add_new_modal('.$row['id'].')" >
               <div class="col-3">launch  : </div>
-              <div class="col-9">'.$row["launch_date"].' </div>
+              <div class="col-9">'. $launch_date.' </div>
             </div>
         </div>
     </div>   
