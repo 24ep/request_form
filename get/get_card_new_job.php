ea14d2a6d104b63312fcdd5fb98757c08c78f030
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2021-11-01 13:12:46 +0700

    dbug

--- get/get_card_new_job.php
+++ get/get_card_new_job.php
@@ -13,21 +13,22 @@ function get_card_new_job($status,$username){
     $sort = 'launch_date ASC';
   }
   $query = "SELECT * FROM all_in_one_project.add_new_job  where follow_assign_name = '".$username."' and ".$status." ORDER by ".$sort." ".$limit ;
+  echo '<script>console.log('.$query.')</script>';
   $result = mysqli_query($con, $query);
   $result_count = mysqli_query($con, $query_count);
      while($row = mysqli_fetch_array($result)) {
       $border =   'border-color: transparent;';
     echo    '
-    <div class="card" id="card_fl_'.$row["id"].'" onclick="call_edit_add_new_modal('.$row['id'].')" style="margin-top:15px;'.$border.'">
+    <div class="" id="card_fl_'.$row["id"].'" onclick="call_edit_add_new_modal('.$row['id'].')" style="margin-top:15px;'.$border.'">
         <div class="card-body shadow" >
             <h6 class="card-title" onclick="call_edit_add_new_modal('.$row['id'].')" style="font-size:14px"><strong style="color:red">NS-'.$row["id"].'</strong> '.$row["brand"].' '.$row["sku"].' SKUs </h6>
-            <div class="row" onclick="call_edit_add_new_modal('.$row['id'].')">
-              <div class="col" style="color:gray;font-size:12px" >status : </div>
-              <div class="col">'.$row["status"].' </div>
+            <div class="row" style="margin-bottom:3px;margin-top:3px" onclick="call_edit_add_new_modal('.$row['id'].')">
+              <div class="col-2" style="color:gray;font-size:12px" >status : </div>
+              <div class="col-10">'.$row["status"].' </div>
             </div>
-            <div class="row" onclick="call_edit_add_new_modal('.$row['id'].')" style="color:gray;font-size:12px">
-              <div class="col">launch  : </div>
-              <div class="col">'.$row["launch_date"].' </div>
+            <div class="row" style="margin-bottom:3px;margin-top:3px" onclick="call_edit_add_new_modal('.$row['id'].')" style="color:gray;font-size:12px">
+              <div class="col-2">launch  : </div>
+              <div class="col-10">'.$row["launch_date"].' </div>
             </div>
         </div>
     </div>   
