a9ad133b55513079fc730b9bf519a3128ee91a8b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-01-01 18:53:17 +0700

    debug

--- get/get_checklist_cr.php
+++ get/get_checklist_cr.php
@@ -195,9 +195,9 @@ function getoption_return_edit_job($col,$table,$select_option,$sorm) {
                   echo '
                   <div class="card text-dark bg-light mb-3 shadow-sm" style="border-color: transparent;padding-left: 15px;padding-right: 15px;background-color: #e9ecef!important;" >
                     <div class="card-body">
-                      <h6 class="card-title">N0 '.$i.'.</h6>
+                      
                       <p class="card-text">
-                        <textarea placeholder="input task '.$i.' here ." style="border-color: transparent;background-color:transparent;" class="form-control" onchange="update_cl_detail('.$row["id"].','.$cl_edit_description.')" id="cl_edit_description" name="cl_edit_description" rows="1"></textarea>
+                      <h6 class="card-title">N0 '.$i.'.</h6> <textarea placeholder="input task '.$i.' here ." style="border-color: transparent;background-color:transparent;" class="form-control" onchange="update_cl_detail('.$row["id"].','.$cl_edit_description.')" id="cl_edit_description" name="cl_edit_description" rows="1"></textarea>
                       </p>
                     </div>
                     <div class="card-footer bg-transparent border-success" style="border-color: #dfd9d9!important">
