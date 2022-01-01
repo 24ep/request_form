506f93f87289a3684f086635f40f75d96eb3e282
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-01-01 13:48:30 +0700

    debug

--- form/form_create_content_request.php
+++ form/form_create_content_request.php
@@ -99,15 +99,15 @@
             <?php echo $product_website_op; ?>
         </select>
     </div>
-    <?php if(strpos($_SESSION["department"],"Content")!==false or $_SESSION["department"]==''){ ?>
+  
     <div class="form-group col-md-3">
         <label for="cr_ticket_template" class="form-label">ticket template</label>
         <select id="cr_ticket_template" name="cr_ticket_template" class="form-select form-select-sm">
-           <option selected>CR</option>
-           <option>PJ</option>
+           <option value="CR" selected>Content Request</option>
+           <option value="PJ">Project</option>
         </select>
     </div>
-    <?php } ?>
+
   </div>
   <hr>
   <div class="row">
