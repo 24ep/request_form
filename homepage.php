f66041084a1075375b96ff4adc83e1eb1d96d1db
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-01-09 00:08:23 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -541,7 +541,7 @@
                                     <li><a target="_Blank" href=""
                                             class="d-inline-flex align-items-center rounded link-light">Setting</a>
                                     </li>
-                                    <li><a href="action_logout"
+                                    <li><a href="base/action_logout/action_logout.php"
                                             class="d-inline-flex align-items-center rounded link-light">Logout</a></li>
 
                                 </ul>
