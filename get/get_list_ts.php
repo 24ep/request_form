43be781c5318fb56404cacfe5658cb48fbd0bffd
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-01-12 15:42:18 +0700

    debug

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -241,7 +241,7 @@ $i++;
         <form class="d-flex">
             <?php $memory_usage = get_server_memory_usage();
                                         $CPU_usage = get_server_cpu_usage() ;
-                                        echo "<small>CPU usage ".$CPU_usage."% | memory usage : ".$memory_usage."%</small>"; ?>
+                                        echo "<small  style='align-self: center;'>CPU usage ".$CPU_usage."% | memory usage : ".$memory_usage."%</small>"; ?>
             <button class="btn btn-primary btn-sm " style="margin-left:10px;" type="button" data-bs-toggle="offcanvas"
                 data-bs-target="#content_request_canvas" aria-controls="offcanvasExample">
                 <ion-icon size="small" name="add-outline" role="img" class="md icon-small hydrated"
