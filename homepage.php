28c67433b8b6da74804190bed44e9562fb82537d
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-01-12 21:52:43 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -866,42 +866,7 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- -- -->
-                        <!-- <div class="col-auto" style="right: 20px;position: absolute;margin-top: 10px;">
-                                    <nav aria-label="Page navigation example">
-                                        <ul class="pagination pagination-sm">
-                                            <div class="offcanvas offcanvas-start" style="width:70%" tabindex="-1"
-                                                id="ts_request_canvas" aria-labelledby="offcanvasExampleLabel">
-                                                <div class="offcanvas-header" style="    background: #313131;color: white;">
-                                                    <h5 class="offcanvas-title" id="offcanvasExampleLabel"
-                                                        style="padding-left:50px;font-weight: bold;">
-                                                        <ion-icon style="margin-right:10px" name="add-circle-outline">
-                                                        </ion-icon>Request Creation
-                                                    </h5>
-                                                    <button type="button" class="btn-close btn-close-white text-reset"
-                                                        data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                                                </div>
-                                                <div class="offcanvas-body" style="background-color: #fcfbf9;">
-                                                    <div class="container-md" style="padding:0px 50px 50px 50px;">
-                                                        <form class="row g-3"
-                                                            action="base/action/action_submit_add_content_request.php"
-                                                            method="POST" enctype="multipart/form-data">
-                                                            <div id="add_new_cr_result"></div>
-                                                            <?php //include_once('form/form_create_content_request.php')?>
-                                                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                                                <button type="submit" class="btn btn-primary btn-sm"
-                                                                    style="width:100%">Submit</button>
-                                                            </div>
-                                                        </form>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    </ul>
-                                </nav>
-                            </div> -->
-                        <!-- -- -->
+                    
                     </div>
                     <!-- fl console -->
                     <div class="tab-pane fade" id="v-pills-fl_board" role="tabpanel"
