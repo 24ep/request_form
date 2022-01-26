0cbf7b80e702d30e77e26e606bb2466e0b88e8d1
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-01-26 21:02:23 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -1769,6 +1769,51 @@ tinymce.init({
     selector: '#des_cr_inline',
     inline: true
   });
+
+  
+var emailHeaderConfig = {
+  selector: '.tinymce-heading',
+  menubar: false,
+  inline: true,
+  plugins: [
+    'lists',
+    'powerpaste',
+    'autolink'
+  ],
+  toolbar: 'undo redo | bold italic underline',
+  valid_elements: 'strong,em,span[style],a[href]',
+  valid_styles: {
+    '*': 'font-size,font-family,color,text-decoration,text-align'
+  },
+  powerpaste_word_import: 'clean',
+  powerpaste_html_import: 'clean',
+};
+
+var emailBodyConfig = {
+  selector: '#des_cr_inline',
+  menubar: false,
+  inline: true,
+  plugins: [
+    'link',
+    'lists',
+    'powerpaste',
+    'autolink',
+    'tinymcespellchecker'
+  ],
+  toolbar: [
+    'undo redo | bold italic underline | fontselect fontsizeselect',
+    'forecolor backcolor | alignleft aligncenter alignright alignfull | numlist bullist outdent indent'
+  ],
+  valid_elements: 'p[style],strong,em,span[style],a[href],ul,ol,li',
+  valid_styles: {
+    '*': 'font-size,font-family,color,text-decoration,text-align'
+  },
+  powerpaste_word_import: 'clean',
+  powerpaste_html_import: 'clean',
+};
+
+tinymce.init(emailHeaderConfig);
+tinymce.init(emailBodyConfig);
   </script>
 
     <script>
