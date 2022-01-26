339dbc44d5d10439c908b4410e3dfa0618b748d3
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-01-26 12:35:40 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -514,6 +514,16 @@
         </style>
     </head>
     <body onload="doAutoRefresh();filter_update();doAutoRefresh_cr();doAutoRefresh_ts_admin();">
+    <!-- toast -->
+         <div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                    <div id="toast" class="toast-body">
+                        Hello, world! This is a toast message.
+                    </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+        </div>
+    <!-- toast -->
         <!--         <nav class="navbar sticky-top navbar-danger bg-danger">
           <marquee>
            <span class="navbar-text" style="color:white" >
