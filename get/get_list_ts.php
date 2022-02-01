821349e65de3055e2e08fdeff569388c75048430
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2022-02-01 12:24:29 +0700

    Update get_list_ts.php

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -60,7 +60,7 @@ function badge_status_cr($status){
   }elseif($status=="Waiting Buyer"){
     $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #FE7A6F;color:#a80c1b;border:#FE7A6F">waiting buyer</button>';
   }elseif($status=="Inreview"){
-    $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #2bf2ca;color:white;border:#2bf2ca">In-review"</button>';
+    $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #2bf2ca;color:black;border:#2bf2ca">Inreview</button>';
   }else{
     $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #a9a9a94f;color:#8f8f8f;border:#8f8f8f">'.$status.'</button>';
   }
