34c982ba8086936775ffb958fe4e20c112d00a0b
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2022-02-02 15:31:45 +0700

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -559,6 +559,9 @@
         font-size: 14px;
         font-family: 'Prompt', sans-serif !important;
     }
+            .hr_nav_bra{
+            wwidth:80%
+            }
 
 <?php
         }
@@ -673,7 +676,7 @@
                         aria-controls="v-pills-cr" onclick="updateURL('v-pills-cr');" aria-selected="false">
                         <ion-icon style="color:white" name="ticket-outline"></ion-icon>Content Request
                     </a>
-                    <hr>
+                    <hr class="hr_nav_bra">
                     <?php if(strpos($_SESSION["department"],'Admin')!==false){?>
                     <!-- <a class="nav-link" id="v-pills-cr_admin-tab" data-toggle="pill" href="#v-pills-cr_admin" role="tab"
                         aria-controls="v-pills-cr_admin" onclick="updateURL('v-pills-cr_admin');" aria-selected="false">
@@ -690,7 +693,7 @@
                         <ion-icon style="color:white" name="grid-outline"></ion-icon> Follow-up Board
                     </a>
                     <?php }?>
-                    <hr>
+                    <hr class="hr_nav_bra">
                     <ul class="list-unstyled ps-0">
                         <li class="mb-1">
                             <button style="color:#eee;padding:8px 16px;font-size:14px"
