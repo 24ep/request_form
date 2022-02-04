f38bdbb217384ff7e1910fc90fe9b6f4cf32eb40
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-02-04 20:07:56 +0700

    debug

--- get/get_list_project.php
+++ get/get_list_project.php
@@ -6,22 +6,23 @@
   mysqli_query($con, "SET NAMES 'utf8' ");
   $query = "SELECT * FROM project_bucket where status = 'Open'" or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
- ?><div class="row overflow-auto"><?php
+ ?><div class="row overflow-auto" style=""><?php
      while($row = mysqli_fetch_array($result)) {
          echo ' 
-         <div class="col-sm-6">
-         <span class="position-absolute top-0 start-0 translate-middle p-2 bg-info border border-light rounded-circle" style="background:#0dcaf0;">
-         <span class="visually-hidden">New alerts</span>
-         </span>
+    
+         <div class="card-group">
             <div class="card">
-         
+                <span class="position-absolute top-0 start-0 translate-middle p-2 bg-info border border-light rounded-circle" style="background:#0dcaf0;">
+                <span class="visually-hidden">New alerts</span>
+                </span>
           
-            <div class="card-body" style="border: solid 2px #0dcaf0;">
-                <h5 class="card-title">'.$row["project_name"].'</h5>
-                <span class="card-text">Owner : '.$row["owner"].'.</span>
+                <div class="card-body" style="border: solid 2px #0dcaf0;">
+                    <h5 class="card-title">'.$row["project_name"].'</h5>
+                    <span class="card-text">Owner : '.$row["owner"].'.</span>
+                </div>
             </div>
             </div>
-        </div>';
+       ';
 
      }
   mysqli_close($con);
