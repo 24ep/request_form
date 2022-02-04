2e5c099c51c67d8117ac02e474c379d3dbeb74f4
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-02-04 19:47:19 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -1256,7 +1256,7 @@
                                         </form>
                                     </nav>
                                     <?php if(strpos($_SESSION["username"],'poojaroonwit')!==false){ 
-                                        include('get/get_list_project.pho');
+                                        include('get/get_list_project.php');
                                     } ?>
                                     <div id="get_ts_admin_console">
                                         <?php include('get/get_list_ts.php'); ?>
