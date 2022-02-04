4499d4c47840f93ea8caa74a56badbd67882ad65
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-02-04 20:09:48 +0700

    debug

--- get/get_list_project.php
+++ get/get_list_project.php
@@ -6,11 +6,11 @@
   mysqli_query($con, "SET NAMES 'utf8' ");
   $query = "SELECT * FROM project_bucket where status = 'Open'" or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
- ?><div class="row overflow-auto" style=""><?php
+ ?><div  class="card-group overflow-auto" style=""><?php
      while($row = mysqli_fetch_array($result)) {
          echo ' 
     
-         <div class="card-group">
+         
             <div class="card">
                 <span class="position-absolute top-0 start-0 translate-middle p-2 bg-info border border-light rounded-circle" style="background:#0dcaf0;">
                 <span class="visually-hidden">New alerts</span>
@@ -21,7 +21,7 @@
                     <span class="card-text">Owner : '.$row["owner"].'.</span>
                 </div>
             </div>
-            </div>
+          
        ';
 
      }
