bd988690dd5213d5bfa080bf3edb59eb172c8d07
24ep <jaroonwit.pool@gmail.com>
2022-02-05 19:09:44 +0700

    debug

--- get/get_project_model.php
+++ get/get_project_model.php
@@ -33,10 +33,57 @@ while($row = mysqli_fetch_array($result)) {
                     <small><ion-icon name="document-attach-outline"></ion-icon> Attachments</small>
                     <hr>
                     <small><ion-icon name="file-tray-stacked-outline"></ion-icon> Task</small>
+                    ';
+                    ?>
+                    <div class="row">
+    <div class="col border-0 border-end">
+        <small class="row m-3">Your Assignment</small>
+        <small class="row m-3">Pending</small>
+        <hr>
+        <div id="list_cr_task_pending">
+            <?php list_ts($_SESSION["ts_query_input"]." and ticket.status = 'Pending' and  ticket.ticket_template = 'CR'",100,'ticket');
+                      echo '<script>console.log("'.$_SESSION["ts_query_input"].' ");</script>';
+                      ?>
+        </div>
+        <small class="row m-3">inprogress</small>
+        <hr>
+        <div id="list_cr_task_inprogress">
+            <?php list_ts($_SESSION["ts_query_input"]."   and ticket.status = 'Inprogress' and  ticket.ticket_template = 'CR'",100,'ticket'); ?>
+        </div>
+        <small class="row m-3">Waiting Execution</small>
+        <hr>
+        <div id="list_cr_task_we">
+            <?php list_ts($_SESSION["ts_query_input"]."  and ticket.status = 'Waiting Execution' and  ticket.ticket_template = 'CR'",100,'ticket'); ?>
+        </div>
+        <small class="row m-3">Waiting CTO</small>
+        <hr>
+        <div id="list_cr_task_wcto">
+            <?php list_ts($_SESSION["ts_query_input"]."   and ticket.status = 'Waiting CTO' and  ticket.ticket_template = 'CR'",100,'ticket'); ?>
+        </div>
+        <small class="row m-3">Waiting Buyer</small>
+        <hr>
+        <div id="list_cr_task_wb">
+            <?php list_ts($_SESSION["ts_query_input"]."   and ticket.status = 'Waiting Buyer' and  ticket.ticket_template = 'CR'",100,'ticket'); ?>
+        </div>
+        <small class="row m-3">Close [lastest 5 ticket]</small>
+        <hr>
+        <div id="list_cr_task_wb">
+            <?php list_ts($_SESSION["ts_query_input"]."   and ticket.status = 'Close' and  ticket.ticket_template = 'CR'",5,'ticket'); ?>
+        </div>
+    </div>
+    <div class="col">
+        <small class="row m-3">Unassign</small>
+        <?php list_ts("ticket.ticket_template = 'CR' and ticket.case_officer = 'unassign' and ticket.status <> 'Close'",100,'ticket'); ?>
+    </div>
+</div>
+                    
+                    <?php
+                    echo '
                 </div>
                 <div class="col-3">
                 <small>status</small>
-                    <select class="form-select" aria-label="Default select example">
+                    <select class="form-select" style="border: 0px;background-color: #f5f5f5;"
+                    aria-label="Default select example">
                         <option selected value="Open">Open</option>
                         <option value="Close">Close</option>
                     </select>
