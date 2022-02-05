e81e6ec65b9d764ee9aab6f13991e8647c07cbe3
24ep <jaroonwit.pool@gmail.com>
2022-02-05 14:46:37 +0700

    debug

--- get/get_list_project.php
+++ get/get_list_project.php
@@ -7,18 +7,15 @@
   $query = "SELECT * FROM project_bucket where status = 'Open'" or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
   
- ?><div class="overflow-auto" style="padding: 10px;">
+ ?><div class="overflow-auto" style="padding: 10px;margin-bottom:10px">
  <div class="card-group" style="width: max-content;"><?php
      while($row = mysqli_fetch_array($result)) {
+       if($row["sticky"]==1){
+        $sticky_style= 'style="border: solid 3px #75dfa5c7;border-radius: 10px!important;"';
+       }
          echo ' 
-    
-         
             <div class="card" style="margin: 10px;border-radius: 10px!important;">
-                <span class="position-absolute top-0 start-0 translate-middle p-2 bg-info border border-light rounded-circle" style="background:#0dcaf0;">
-                <span class="visually-hidden">New alerts</span>
-                </span>
-          
-                <div class="card-body" style="border: solid 3px #0dcaf0;border-radius: 10px!important;">
+                <div class="card-body" '.$sticky_style.'>
                     <h6 class="card-title">'.$row["prefix"]." | ".$row["project_name"].'</h6>
                     <span class="card-text">Owner : '.$row["owner"].'.</span>
                 </div>
