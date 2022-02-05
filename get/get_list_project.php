9437478d8b00770f91dbb295bd3dd22b68975ab4
24ep <jaroonwit.pool@gmail.com>
2022-02-05 17:59:48 +0700

    debug

--- get/get_list_project.php
+++ get/get_list_project.php
@@ -18,7 +18,7 @@
   $result = mysqli_query($con, $query);
   
  ?><div class="overflow-auto slide" style="padding: 10px;margin-bottom:10px">
- <div class="card-group" style="width: max-content;"><?php
+    <div class="card-group" style="width: max-content;"><?php
      while($row = mysqli_fetch_array($result)) {
        
        // sticky project
@@ -46,8 +46,9 @@
        }
 
          echo ' 
-            <button type="button"  onclick="get_project_model('.$row["id"].')" data-bs-toggle="modal" data-bs-target="#project_model">
+            
               <div class="card shadow-sm" style="margin: 10px;border-radius: 10px!important;">
+              <button type="button"  onclick="get_project_model('.$row["id"].')" data-bs-toggle="modal" data-bs-target="#project_model">
               '.$count_nc_style.'
                   <div class="card-body" '.$sticky_style.'>
                       <h6 class="card-title" style="font-weight:900;margin-bottom:15px"><span style="color:red">'.$row["prefix"]."</span> | ".$row["project_name"].'</h6>
@@ -55,8 +56,9 @@
                       <div style="margin:5px;font-size: 14px;color: #6b6b6b!important;"><ion-icon name="people-outline" style="font-size: 18px;color: #6b6b6b!important;"></ion-icon> 
                       <img src="'.$row["profile_url"].'" class="rounded-circle" alt="...">'.$row["owner"].'.</div>
                   </div>
+                  </button>
               </div>
-            </button>
+            
           
        ';
 
@@ -64,8 +66,8 @@
   mysqli_close($con);
   ?>
 
-</div>
-<!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    </div>
+    <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
