3f138277c454d2a5ec17dde7f162b9b5c893e736
24ep <jaroonwit.pool@gmail.com>
2022-02-07 00:29:43 +0700

    debug

--- get/get_list_project.php
+++ get/get_list_project.php
@@ -28,10 +28,19 @@
   on pb.owner = ac.username 
   where pb.prefix in (".$_SESSION['prefix_project_sticky'].") order by pb.sticky DESC" or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
-  
+
+ 
+
  ?><div class="overflow-auto slide" style="padding: 10px;margin-bottom:10px">
     <div class="card-group" style="width: max-content;"><?php
      while($row = mysqli_fetch_array($result)) {
+
+      $query_participant = "select case_officer from all_in_one_project.content_request where ticket_template = '".$row["prefix"]."' group by case_officer";
+      $result_participant = mysqli_query($con, $query_participant);
+      unset($project_participants);
+      while($row_participant = mysqli_fetch_array($result_participant)) {
+        $project_participants .= ' <img width="30px" height="30px" src="base/image/user_profile/'.$row_participant["case_officer"].'.jpg" class="rounded-circle img-fluid" alt="'.$row_participant["case_officer"].'"></div>';
+      }
        
        // sticky project
        if($row["sticky"]==1){
@@ -67,7 +76,8 @@
                       <h6 class="card-title" style="font-weight:900;margin-bottom:15px;color:'.$row["color_project"].'"><span>'.$row["prefix"]."</span> | ".$row["project_name"].'</h6>
                       <div style="margin:5px;font-size: 14px;color: #6b6b6b!important;"><ion-icon name="file-tray-stacked-outline" style="font-size: 18px;color: #6b6b6b!important;"></ion-icon> '.$count_ticket.' Tickets</div>
                       <div style="margin:5px;font-size: 14px;color: #6b6b6b!important;"><ion-icon name="people-outline" style="font-size: 18px;color: #6b6b6b!important;"></ion-icon> 
-                      <img width="30px" height="30px" src="base/image/user_profile/'.$row["owner"].'.jpg" class="rounded-circle img-fluid" alt="'.$row["owner"].'"></div>
+                      <img width="30px" height="30px" src="base/image/user_profile/'.$row["owner"].'.jpg" class="rounded-circle img-fluid" alt="'.$row["owner"].'"></div> | 
+                      '.$project_participants.'
                   </div>
                   </button>
               </div>
