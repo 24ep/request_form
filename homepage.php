dc0ebd42c87192063946fbb5f213d0c61f324ec5
24ep <jaroonwit.pool@gmail.com>
2022-02-06 16:29:50 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -699,7 +699,7 @@
     <body onload="doAutoRefresh();filter_update();doAutoRefresh_cr();doAutoRefresh_ts_admin();">
         <!-- Modal -->
         <div class="modal fade " id="project_model" tabindex="-1" aria-labelledby="project_modelLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-dialog modal-dialog modal-lg modal-dialog-centered">
                 <div class="modal-content" style="border-radius: 3%;">
                     <div id="return_project_model">
                     </div>
