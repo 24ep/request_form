743639c4e721e35dcc2b8b17643b57c43cca0300
24ep <jaroonwit.pool@gmail.com>
2022-02-06 22:57:42 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -1430,7 +1430,7 @@
                             <div style="margin-bottom:10px">
                                 Select project you wnt to see , this option will show only project are not close
                             </div>
-                            <select class="form-select" size="25" id="project_sticky_mse" onclick="update_project_sticky();" multiple aria-label="multiple select example">
+                            <select class="form-select" size="25" id="project_sticky_mse" onclick="update_project_sticky();run_ts_command('task');" multiple aria-label="multiple select example">
 
                                 <?php
                                
