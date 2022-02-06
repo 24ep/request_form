f88fdeab352a2388b0a625a96b04b218c6165ba3
24ep <jaroonwit.pool@gmail.com>
2022-02-06 21:44:00 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -1419,7 +1419,7 @@
                     <div class="offcanvas offcanvas-start" tabindex="-1" id="project_sticky"
                         aria-labelledby="offcanvasExampleLabel">
                         <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Project sticky</h5>
                             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
                                 aria-label="Close"></button>
                         </div>
