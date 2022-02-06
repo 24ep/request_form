cf29a4718a9496211df8d3348bbab205c63e1c27
24ep <jaroonwit.pool@gmail.com>
2022-02-07 01:20:36 +0700

    debug

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -147,7 +147,7 @@ while($row_status = mysqli_fetch_array($result_status)) {
   echo '<hr>';
 }
 
-echo '<small class="row m-3">Assigned to other/small>';
+echo '<small class="row m-3">Assigned to other</small>';
   list_ts("(ticket.case_officer <> '".$_SESSION["username"]."' and ticket.ticket_template in (".$_SESSION['prefix_project_sticky'].")  and ticket.status = '".$row_status["attribute_option"]."'",$limit ,'ticket');
   echo '<hr>';
 
