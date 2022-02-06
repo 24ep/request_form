d12464679ff7ea230bd21c2730cd4508862eef46
24ep <jaroonwit.pool@gmail.com>
2022-02-06 16:39:48 +0700

    debug

--- get/get_project_model.php
+++ get/get_project_model.php
@@ -1,4 +1,25 @@
 <?php 
+function badge_status($status){
+    if($status=="Pending"){
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #a9a9a94f;color:#8f8f8f;border:#8f8f8f">pending</button>';
+    }elseif( $status=="Inprogress"){
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #ff9a59;color:white;border:#ff9a59">Inprogress</button>';
+    }elseif($status=="Close" or $status=="approved"){
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #7befb2;color:#115636;border:#115636">'.$status.'</button>';
+    }elseif($status=="Waiting CTO"){
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #499CF7;color:#093f8e;border:#499CF7">Waiting CTO</button>';
+    }elseif($status=="Waiting Execution"){
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #FA9193;color:#white;border:#FA9193">Waiting Execution</button>';
+    }elseif($status=="Waiting Buyer"){
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #FE7A6F;color:#a80c1b;border:#FE7A6F">waiting buyer</button>';
+    }elseif($status=="Inreview"){
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #2bf2ca;color:black;border:#2bf2ca">Inreview</button>';
+    }else{
+      $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #a9a9a94f;color:#8f8f8f;border:#8f8f8f">'.$status.'</button>';
+    }
+    
+  return $status;
+  }
 date_default_timezone_set("Asia/Bangkok");
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
@@ -63,10 +84,11 @@ while($row = mysqli_fetch_array($result)) {
         $query_task = "SELECT title,status,case_officer,id from all_in_one_project.content_request where status <> 'Close' and ticket_template='".$row["prefix"]."'";
         $result_task = mysqli_query($con, $query_task);
         while($row_task = mysqli_fetch_array($result_task)) {
+            $status=badge_status($row_task["status"]);
             echo '<tr>';
             echo '<td style="padding:20px">'.$row_task["id"].'</td>';
             echo '<td style="padding:20px">'.$row_task["title"].'</td>';
-            echo '<td style="text-align: -webkit-center;padding:20px">'.$row_task["status"].'</td>';
+            echo '<td style="text-align: -webkit-center;padding:20px">'.$status.'</td>';
             echo '<td style="text-align: -webkit-center;padding:20px">'.$row_task["case_officer"].'</td>';
             echo '<td style="text-align: -webkit-center;padding:20px">
             <ion-icon name="ellipsis-horizontal-outline" data-bs-toggle="offcanvas"
