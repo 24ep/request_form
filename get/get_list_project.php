2b888a43328b9cb2a19e859106f9bf360b6074df
24ep <jaroonwit.pool@gmail.com>
2022-02-07 00:40:07 +0700

    debug

--- get/get_list_project.php
+++ get/get_list_project.php
@@ -39,7 +39,7 @@
       $result_participant = mysqli_query($con, $query_participant);
       unset($project_participants);
       while($row_participant = mysqli_fetch_array($result_participant)) {
-        $project_participants .= ' <img width="30px" height="30px" src="base/image/user_profile/'.$row_participant["case_officer"].'.jpg" class="rounded-circle img-fluid" alt="'.$row_participant["case_officer"].'">';
+        $project_participants .= ' <img width="30px" height="30px" src="base/image/user_profile/'.$row_participant["case_officer"].'.jpg" class="rounded-circle" alt="'.$row_participant["case_officer"].'">';
       }
        
        // sticky project
@@ -76,7 +76,7 @@
                       <h6 class="card-title" style="font-weight:900;margin-bottom:15px;color:'.$row["color_project"].'"><span>'.$row["prefix"]."</span> | ".$row["project_name"].'</h6>
                       <div style="margin:5px;font-size: 14px;color: #6b6b6b!important;"><ion-icon name="file-tray-stacked-outline" style="font-size: 18px;color: #6b6b6b!important;"></ion-icon> '.$count_ticket.' Tickets</div>
                       <div style="margin:5px;font-size: 14px;color: #6b6b6b!important;"><ion-icon name="people-outline" style="font-size: 18px;color: #6b6b6b!important;"></ion-icon> 
-                      <img width="30px" height="30px" src="base/image/user_profile/'.$row["owner"].'.jpg" class="rounded-circle img-fluid" alt="'.$row["owner"].'"> | '.$project_participants.'</div>
+                      <img width="30px" height="30px" src="base/image/user_profile/'.$row["owner"].'.jpg" class="rounded-circle" alt="'.$row["owner"].'"> | '.$project_participants.'</div>
                       
                   </div>
                   </button>
