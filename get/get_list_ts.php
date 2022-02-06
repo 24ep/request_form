2bcb72483cf388cff4a71807ba360384fb0ca904
24ep <jaroonwit.pool@gmail.com>
2022-02-06 20:20:52 +0700

    debug

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -89,10 +89,10 @@ return $status;
                 ?>
 <li class="row shadow-sm rounded md-3 p-2 bg-white position-relative" style="<?php echo  $ri_style ?> " data-bs-toggle="offcanvas"
     data-bs-target="#detail_cr" aria-controls="offcanvasExample" onclick="cr_id_toggle(<?php echo $row['id'] ?>)">
-    <span class="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-primary"><ion-icon name="ticket-outline"></ion-icon> <span class="visually-hidden">unread messages</span></span>
+    
     <div class="col-9" data-bs-toggle="offcanvas" data-bs-target="#detail_cr" aria-controls="offcanvasExample"
         onclick="cr_id_toggle(<?php echo $row['id'];?>) " style="align-self: center;">
-        
+        <span class="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-primary"><ion-icon name="ticket-outline"></ion-icon> <span class="visually-hidden">unread messages</span></span>
         <?php echo "<strong>".$row["ticket_template"]."-".$row["id"]."</strong> ".$row["title"]; ?>
     </div>
     <div class="col-3" data-bs-toggle="offcanvas" data-bs-target="#detail_cr" aria-controls="offcanvasExample"
