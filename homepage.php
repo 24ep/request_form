15705a08629c1008adcf6039528793fe18dc3295
24ep <jaroonwit.pool@gmail.com>
2022-02-06 21:45:08 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -1427,7 +1427,7 @@
                             <div style="margin-bottom:10px">
                                 Select project you wnt to see , this option will show only project are not close
                             </div>
-                            <select class="form-select" size="35" multiple aria-label="multiple select example">
+                            <select class="form-select" size="25" multiple aria-label="multiple select example">
 
                                 <?php
                                
