8f20c70afb10f9239fcb200b5a56296fc68fe057
24ep <jaroonwit.pool@gmail.com>
2022-02-06 22:52:14 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -1436,7 +1436,7 @@
                                
                                     $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
                                     mysqli_query($con, "SET NAMES 'utf8' ");
-                                    $query = "SELECT * FROM project_bucket ORDER BY id asc" or die("Error:" . mysqli_error());
+                                    $query = "SELECT * FROM project_bucket where status <> 'Close' ORDER BY id asc" or die("Error:" . mysqli_error());
                                     $result = mysqli_query($con, $query);
                                     while($row = mysqli_fetch_array($result)) {
                                         if($row["sticky"]==1){
