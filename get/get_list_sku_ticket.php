a1e1ae9343cd78227b2a62c6552549fc4a838159
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2022-02-13 15:54:40 +0700

    Update get_list_sku_ticket.php

--- get/get_list_sku_ticket.php
+++ get/get_list_sku_ticket.php
@@ -2,27 +2,41 @@
   date_default_timezone_set("Asia/Bangkok");
   $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
   mysqli_query($con, "SET NAMES 'utf8' ");
-  $query = "SELECT * FROM sku_list where csg_id = ".$_POST['id']." ORDER BY id DESC " or die("Error:" . mysqli_error());
+  $query = "SELECT sl.sku,sl.create_by,sl.create_date,itm.jda,itm.sbc,itm.department,itm.sub_department,itm.product_name,itm.catalogue,itm.vendor,itm.vendornm,itm.retail FROM sku_list as sl left join itm_datalake as itm on itm.pid = sl.sku   where csg_id = ".$_POST['id']." ORDER BY id DESC " or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
     echo 
     "<table class='table table-bordered'>
         <thead>
             <tr>";
     echo "<th scope='row'>sku</th></th>";  
-    echo "<th>status</th>";
-    echo "<th>update_by</th>";
+    echo "<th scope='row'>itm_jda</th></th>";  
+    echo "<th scope='row'>itm_sbc</th></th>";  
+    echo "<th scope='row'>tm_dept</th></th>";  
+    echo "<th>itm_sdept</th>";
+    echo "<th>itm_name</th>";
+    echo "<th>itm_catalogue</th>";
+    echo "<th>itm_vendor</th>";
+    echo "<th>itm_vendornm</th>";
+    echo "<th>itm_retail</th>";
+    echo "<th>create_by</th>";  
     echo "<th>create_date</th>";  
-    echo "<th>update_date</th>";
     echo  "</tr>
     </thead>
      <tbody>";
      while($row = mysqli_fetch_array($result)) {
         echo "<tr>";
         echo "<th scope='row' style='background: #ededed;'>".$row["sku"]."</th>";
-        echo "<td style='background: #ededed;'>".$row["status"]."</dh>";
+       echo "<td style='background: #ededed;'>".$row["jda"]."</td>"; 
+       echo "<td style='background: #ededed;'>".$row["sbc"]."</td>"; 
+       echo "<td style='background: #ededed;'>".$row["deoartment"]."</td>"; 
+       echo "<td style='background: #ededed;'>".$row["sub_department"]."</td>"; 
+        echo "<td style='background: #ededed;'>".$row["product_name"]."</td>"; 
+       echo "<td style='background: #ededed;'>".$row["catalogue"]."</td>"; 
+        echo "<td style='background: #ededed;'>".$row["vendor"]."</td>"; 
+        echo "<td style='background: #ededed;'>".$row["vendornm"]."</td>"; 
+        echo "<td style='background: #ededed;'>".$row["retail"]."</td>"; 
         echo "<td style='background: #ededed;'>".$row["create_by"]."</td>";  
         echo "<td style='background: #ededed;'>".$row["create_date"]."</td>";
-        echo "<td style='background: #ededed;'>".$row["updated_date"]."</td>";
         echo "</tr>";
         $pass = true;
     } 
