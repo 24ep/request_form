2a566d5f753a033f3248e9d6ff042842cf95bab3
24ep <jaroonwit.pool@gmail.com>
2022-03-03 02:02:40 +0700

    debug

--- get/get_quick_link.php
+++ get/get_quick_link.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("Asia/Bangkok");
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
-$query_bk = "SELECT DISTINCT  bucket FROM all_in_one_project.quick_link;" or die("Error:" . mysqli_error());
+$query_bk = "SELECT DISTINCT  bucket FROM all_in_one_project.quick_link" or die("Error:" . mysqli_error());
 $result_bk = mysqli_query($con, $query_bk);
 while($row_bk = mysqli_fetch_array($result_bk)) {
 $bk_list .= '<button class="nav-link" id="v-pills-'.$row_bk["bucket"].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-'.$row_bk["bucket"].'" type="button" role="tab" aria-controls="v-pills-'.$row_bk["bucket"].'" aria-selected="false">'.$row_bk["bucket"].'</button>';
@@ -11,7 +11,7 @@ $query = "SELECT * from quick_link where bucket = '".$row_bk["bucket"]."'" or di
 $result = mysqli_query($con, $query);
 
 while($row = mysqli_fetch_array($result)) {
-    $list_detail = '
+    $list_detail = .'
     <div class="card">
         <div class="card-body">
         <a href="'.$row["link"].'" target="_blank" ><h5 class="card-header">'.$row["label"].'</h5></a>
