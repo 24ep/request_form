52da5250cbaee457e9ead909be48039e9e9d77ad
unknown <poojaroonwit@central.co.th>
2022-03-07 15:00:41 +0700

    debug

--- get/get_setting.php
+++ get/get_setting.php
@@ -1,13 +1,14 @@
 <?php
  $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
  mysqli_query($con, "SET NAMES 'utf8' ");
- $query = "SELECT value,icon FROM content_service_gate.setting_menu" or die("Error:" . mysqli_error());
+ $query = "SELECT value,icon,code FROM content_service_gate.setting_menu" or die("Error:" . mysqli_error());
  $result = mysqli_query($con, $query);
  while($row = mysqli_fetch_array($result)) {
    
     
     $value.= '<button style="text-align: left;font-weight: 600;" class="nav-link" id="v-pills-setting_'.$row["code"].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-setting_'.$row["code"].'" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><ion-icon style="margin-right: 8px" name="'.$row["icon"].'"></ion-icon>'.$row["value"].'</button>';
-    $detail.= '<div class="tab-pane fade" id="v-pills-setting_'.$row["code"].'" role="tabpanel" aria-labelledby="v-pills-setting_'.$row["code"].'-tab"><h3>'.$row["value"].'</h3>';
+    $detail.= '<div class="tab-pane fade" id="v-pills-setting_'.$row["code"].'" role="tabpanel" aria-labelledby="v-pills-setting_'.$row["code"].'-tab">
+    <h3>'.$row["value"].'</h3>';
          if($row["code"]=="account"){
             include("get_page_account.php");
             $function_account = get_page_account();
