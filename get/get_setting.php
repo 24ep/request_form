d8f76e64d04f41e60b5f75e19dc44ff7f0e360b9
unknown <poojaroonwit@central.co.th>
2022-03-07 14:43:03 +0700

    debug

--- get/get_setting.php
+++ get/get_setting.php
@@ -4,9 +4,10 @@
  $query = "SELECT value,icon FROM content_service_gate.setting_menu" or die("Error:" . mysqli_error());
  $result = mysqli_query($con, $query);
  while($row = mysqli_fetch_array($result)) {
+    include("get_page_".$row["code"].".php");
     $value.= '<button style="text-align: left;font-weight: 600;" class="nav-link" id="v-pills-setting_'.$row["code"].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-setting_'.$row["code"].'" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><ion-icon style="margin-right: 8px" name="'.$row["icon"].'"></ion-icon>'.$row["value"].'</button>';
     $detail.= '<div class="tab-pane fade" id="v-pills-setting_'.$row["code"].'" role="tabpanel" aria-labelledby="v-pills-setting_'.$row["code"].'-tab"><h3>'.$row["value"].'</h3>';
-    // $detail.=  include("get_page_".$row["code"].".php");      
+    $detail.=  get_page_account();     
     $detail.='</div>';
 }
 
