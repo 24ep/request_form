3d5767aa9a312d8899f4a37c7ec015f8f47fa0a6
unknown <poojaroonwit@central.co.th>
2022-03-07 14:29:11 +0700

    debug

---
+++ get/get_page_account.php
@@ -0,0 +1,40 @@
+<?php
+ $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+ mysqli_query($con, "SET NAMES 'utf8' ");
+ $query = "SELECT value,icon FROM all_in_one_project.account" or die("Error:" . mysqli_error());
+ $result = mysqli_query($con, $query);
+ while($row = mysqli_fetch_array($result)) {
+    $value.= '<td>$row["'.$row["id"].'"]</td>';
+    $value.= '<td>$row["'.$row["username"].'"]</td>';
+    $value.= '<td>$row["'.$row["work_email"].'"]</td>';
+    $value.= '<td>$row["'.$row["status"].'"]</td>';
+    $value.= '<td>$row["'.$row["department"].'"]</td>';
+    $value.= '<td>$row["'.$row["register_type"].'"]</td>';
+    $value.= '<td>IP : 45.55.55.4 T : 14 Feb 2022 12:10 PM</td>';
+    $value.= '<td><ion-icon name="ellipsis-vertical-outline"></ion-icon></td>';
+}
+
+?>
+
+<table class="table table-borderless">
+<thead>
+    <tr>
+      <th scope="col">Account ID</th>
+      <th scope="col">Username</th>
+      <th scope="col">Mail</th>
+      <th scope="col">status</th>
+      <th scope="col">Department</th>
+      <th scope="col">Register type</th>
+      <th scope="col">Last login</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
--- get/get_setting.php
+++ get/get_setting.php
@@ -5,7 +5,9 @@
  $result = mysqli_query($con, $query);
  while($row = mysqli_fetch_array($result)) {
     $value.= '<button style="text-align: left;font-weight: 600;" class="nav-link" id="v-pills-setting_'.$row["code"].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-setting_'.$row["code"].'" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><ion-icon style="margin-right: 8px" name="'.$row["icon"].'"></ion-icon>'.$row["value"].'</button>';
-    $detail.= '<div class="tab-pane fade" id="v-pills-setting_'.$row["code"].'" role="tabpanel" aria-labelledby="v-pills-setting_'.$row["code"].'-tab">Coming Soon</div>';
+    $detail.= '<div class="tab-pane fade" id="v-pills-setting_'.$row["code"].'" role="tabpanel" aria-labelledby="v-pills-setting_'.$row["code"].'-tab"><h3>'.$row["value"].'</h3>';
+              include("get_page_".$row["code"]);      
+    $detail.='</div>';
 }
 
 ?>
