2ac7dfa68368d0e9e86cae75ff1ce5d2ab937428
unknown <poojaroonwit@central.co.th>
2022-03-07 12:54:14 +0700

    debug

--- get/get_setting.php
+++ get/get_setting.php
@@ -4,7 +4,7 @@
  $query = "SELECT value,icon FROM content_service_gate.setting_menu" or die("Error:" . mysqli_error());
  $result = mysqli_query($con, $query);
  while($row = mysqli_fetch_array($result)) {
-    $value.= '<button style="text-align: left;font-weight: 600;" class="nav-link" id="v-pills-setting_'.$row["value"].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-setting_'.$row["value"].'" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><ion-icon style="margin-right: 5px" name="'.$row["icon"].'"></ion-icon>'.$row["value"].'</button>';
+    $value.= '<button style="text-align: left;font-weight: 600;" class="nav-link" id="v-pills-setting_'.$row["value"].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-setting_'.$row["value"].'" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"><ion-icon style="margin-right: 8px" name="'.$row["icon"].'"></ion-icon>'.$row["value"].'</button>';
     $detail.= '<div class="tab-pane fade" id="v-pills-setting_'.$row["value"].'" role="tabpanel" aria-labelledby="v-pills-setting_'.$row["value"].'-tab">Coming Soon</div>';
 }
 
