1d698b60b25856bd3a0d881bbc80cfa3327d814f
unknown <poojaroonwit@central.co.th>
2022-03-30 14:10:55 +0700

    debug

--- get/get_page_account.php
+++ get/get_page_account.php
@@ -6,7 +6,7 @@
     clear: both;
     background: white;
     padding: 50px;
-    border-radius: 40px;
+    border-radius: 30px;
     box-shadow: 0 .3rem 1rem rgba(0,0,0,.15)!important;
 }
 </style>
