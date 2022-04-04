d35b5078da43d43aec162eb255ceaa0fdc701ae9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-04-04 12:44:28 +0700

    debug

--- login_page.php
+++ login_page.php
@@ -79,28 +79,28 @@
 
                 <?php
                    
-                $con= mysqli_connect("localhost","cdse_content","@aA417528639","u749625779_cdscontent") or die("Error: " . mysqli_error($con));
-                mysqli_query($con, "SET NAMES 'utf8' ");
+                // $con= mysqli_connect("localhost","cdse_content","@aA417528639","u749625779_cdscontent") or die("Error: " . mysqli_error($con));
+                // mysqli_query($con, "SET NAMES 'utf8' ");
             
-                    $id = $_POST["id"];
-                    $sql="SELECT * FROM file_manage WHERE job_number='LACF-INDEX' and file_type='Linesheet' and upload_status='Active'";
+                //     $id = $_POST["id"];
+                //     $sql="SELECT * FROM file_manage WHERE job_number='LACF-INDEX' and file_type='Linesheet' and upload_status='Active'";
 
-                    $results = mysqli_query($con,$sql);
-                    if(mysqli_num_rows($results)==1){
+                //     $results = mysqli_query($con,$sql);
+                //     if(mysqli_num_rows($results)==1){
 
-                        $row = mysqli_fetch_array($results);
-                    }
+                //         $row = mysqli_fetch_array($results);
+                //     }
 
-                    echo '
-                    <h3 style="margin-bottom: px; ">'.$row["file_name"].'</h3>
-                    <div class="log_file" style="font-size: 13px!important; width: 300px;">
-                    <p >'.$row["remark"].'</p>
-                    <p style="font-size: 12px;"> <strong>Last Update '.date("Y-m-d H:i:s", strtotime($row["update_at"])).'</strong> </p>
-                    </div>
-                    <a  href="https://cdse-commercecontent.com/base/'.$row["file_path"].$row["file_name"].'" >
-                        <button type="button" class="btn btn-outline-dark">Download</button>
-                    </a>
-                        ';
+                //     echo '
+                //     <h3 style="margin-bottom: px; ">'.$row["file_name"].'</h3>
+                //     <div class="log_file" style="font-size: 13px!important; width: 300px;">
+                //     <p >'.$row["remark"].'</p>
+                //     <p style="font-size: 12px;"> <strong>Last Update '.date("Y-m-d H:i:s", strtotime($row["update_at"])).'</strong> </p>
+                //     </div>
+                //     <a  href="https://cdse-commercecontent.com/base/'.$row["file_path"].$row["file_name"].'" >
+                //         <button type="button" class="btn btn-outline-dark">Download</button>
+                //     </a>
+                //         ';
 
                 ?>
                 <!-- end -->
