1aa7a4a88c2eba73b213e759cd89c300117695de
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-04-05 17:59:53 +0700

    debug

--- script/traffic_mail_open_job.php
+++ script/traffic_mail_open_job.php
@@ -12,6 +12,55 @@ $conversation_id = $_POST["conversation_id"];
 $email_from = $_POST["email_from"];
 $contact = $_POST["contact"];
 
+function check_account(){
+    global $email_from;
+    date_default_timezone_set("Asia/Bangkok");
+    $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+    mysqli_query($con, "SET NAMES 'utf8' ");
+    $sql = "SELECT username from account where status='Enabled' and work_email = ".$email_from;
+    $result =  mysqli_query($con, $sql);
+    while($row = mysqli_fetch_array($result)) {
+        $exist_user =  $row["username"];
+    }
+    if($exist_user == null or $exist_user = ""){
+        #create a new user first
+        date_default_timezone_set("Asia/Bangkok");
+        $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+        mysqli_query($con, "SET NAMES 'utf8' ");
+        $username_replace = replace($email_from,"@central.co.th","");
+        $sql = "INSERT INTO account
+         (firstname,
+         lastname,
+         nickname,
+         username,
+         password,
+         department,
+         work_email,
+         office_tell,
+         register_type) 
+         VALUES 
+         ('".$username_replace."',
+         '".$username_replace."',
+         '".$username_replace."',
+         '".$email_from."',
+         '378af2140b1f3aa30a6c5790454fab97',
+         'Buyer Offline',
+         '".$email_from."',
+         '',
+         'AUTO_MAIL'
+         )
+         ";
+        $query = mysqli_query($con,$sql);
+        return $username_replace;
+
+    }else{
+        return $exist_user;
+    }
+
+}
+
+$username = check_account();
+
 #check separate subject email
 function check_separate_subject_mail(){
     global $subject;
@@ -29,25 +78,43 @@ function check_separate_subject_mail(){
         exit("Error : not found separent value");
     }
 }
-function mapping_department(){
 
-}
 
 $separant_value =  check_separate_subject_mail();
 $attributes_form_email = explode($separant_value,$subject);
 $attributes = array(
-    "department"=>$attributes_form_email[0],
+    "department"=>mapping_department($attributes_form_email[0]),
     "brand"=>$attributes_form_email[1],
     "total_sku"=>$attributes_form_email[2],
     "offline_runing_number"=>$attributes_form_email[3],
     "link_info"=>"link_info mockup",
-    "username"=>"poojaroonwit",
+    "username"=>$username,
     "request_important"=>$important,
     "production_type"=>"No info",
     "contact"=>$contact,
-    "participant"=>"noti_follow_team,poojaroonwit"
+    "participant"=>"noti_follow_team,".$username
     
 );
+
+function mapping_department($department){
+    if(strpos(strtolower($department), "mom") !== false){
+        return "MOM&KIDS";
+    }elseif(strpos(strtolower($department), "kid") !== false){
+        return "MOM&KIDS";
+    }elseif(strpos(strtolower($department), "home") !== false){
+        return "HOME";
+    }elseif(strpos(strtolower($department), "tech") !== false){
+        return "HOME";
+    }elseif(strpos(strtolower($department), "sport") !== false){
+        return "HOME";
+    }elseif(strpos(strtolower($department), "women") !== false){
+        return "WOMEN";
+    }elseif(strpos(strtolower($department), "men") !== false){
+        return "MEN";
+    }elseif(strpos(strtolower($department), "beauty") !== false){
+        return "BEAUTY";
+    }
+}
 
 function create_ticket_csg(){
     global $subject;
