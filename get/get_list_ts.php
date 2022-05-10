ed0709f96982e2bedc5028c2de28aaf6ef37148b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-10 14:52:24 +0700

    debug

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -2,17 +2,10 @@
 session_start();
 if($_POST["summary_filter"]<>"" and $_POST["summary_filter"]<>null ){
   $_SESSION["ts_query_input"] = $_POST["summary_filter"];
-}else{
-  if($_SESSION["ts_query_input"]<>""){
-    $_SESSION["ts_query_input"]=$_SESSION["ts_query_input"];
-  }else{
-    $_SESSION["ts_query_input"]="";
-  }
-
 }
-// if($_SESSION["ts_query_input"]=="" or $_SESSION["ts_query_input"] == null ){
-//   $_SESSION["ts_query_input"] = "ticket.participant like  '%".$_SESSION["username"]."%'";
-// }
+if($_SESSION["ts_query_input"]=="" or $_SESSION["ts_query_input"] == null ){
+  $_SESSION["ts_query_input"] = "";
+}
 if($_POST["ts_command_limit"]<>""){
   $ts_command_limit = $_POST["ts_command_limit"];
 }else{
