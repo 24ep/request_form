603f55d5e8840af065c96567e3001d4cf8285134
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-10 14:44:37 +0700

    dbeu

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -2,6 +2,8 @@
 session_start();
 if($_POST["summary_filter"]<>"" and $_POST["summary_filter"]<>null ){
   $_SESSION["ts_query_input"] = $_POST["summary_filter"];
+}else{
+  $_SESSION["ts_query_input"] = "";
 }
 // if($_SESSION["ts_query_input"]=="" or $_SESSION["ts_query_input"] == null ){
 //   $_SESSION["ts_query_input"] = "ticket.participant like  '%".$_SESSION["username"]."%'";
