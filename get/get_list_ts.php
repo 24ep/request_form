93eb79c5c0c330359b612f8d76975bfff6a338b0
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-10 14:39:13 +0700

    dbeug

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -12,10 +12,10 @@ if($_POST["ts_command_limit"]<>""){
   $ts_command_limit= 100;
 }
 
-$filter = '';
-$filter .= 'ticket.id "%'.$_SESSION['ts_query_input'].'%" or ';
-$filter .= 'ticket.title like "%'.$_SESSION['ts_query_input'].'%" or ';
-$filter .= 'ticket.description like "%'.$_SESSION['ts_query_input'].'%" ';
+$filter = "";
+$filter .= "ticket.id like '%".$_SESSION["ts_query_input"]."%' or ";
+$filter .= "ticket.title like '%".$_SESSION["ts_query_input"]."%' or ";
+$filter .= "ticket.description like '%".$_SESSION["ts_query_input"]."%' ";
     function badge_status_cr($status){
       if($status=="Pending"){
         $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: #a9a9a94f;color:#8f8f8f;border:#8f8f8f">pending</button>';
