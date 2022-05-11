a2dfa5a71998566ad701db1a539251b9baff3393
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-11 22:19:56 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -175,8 +175,8 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
             integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
         </script>
-        <link rel="stylesheet" href="https://content-service-gate.cdse-commercecontent.com/base/css-thean/light.css">
-        <link rel="stylesheet" href="https://content-service-gate.cdse-commercecontent.com/base/css-thean/<?php echo $pftheam; ?>.css">
+        <link rel="stylesheet" href="https://content-service-gate.cdse-commercecontent.com/base/css-theam/light.css">
+        <link rel="stylesheet" href="https://content-service-gate.cdse-commercecontent.com/base/css-theam/<?php echo $pftheam; ?>.css">
         <style>
         
         /* add test */
