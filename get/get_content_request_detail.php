7627df29284bf9230d1a45fefb7d2a1f9104cdbf
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-12 13:31:10 +0700

    debug

--- get/get_content_request_detail.php
+++ get/get_content_request_detail.php
@@ -544,9 +544,9 @@ function comment_cr_id_with_file(id) {
 
     var myIFrame = document.getElementById("comment_input_cr_ifr");
     var comment = myIFrame.contentWindow.document.body.innerHTML;
-
+    document.getElementById("comment_input_cr_ifr").innerHTML = '';
     
-    document.getElementById('comment_input_cr').value = ''; //clear value
+    // document.getElementById('comment_input_cr').value = ''; //clear value
     // var files = document.getElementById('actual-btn').files;
     var ins = document.getElementById('actual-btn').files.length;
     for (var x = 0; x < ins; x++) {
