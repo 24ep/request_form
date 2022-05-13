4bd4fc16735c78003a493331befdb7e20a4f4889
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-13 12:25:16 +0700

    debug

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -104,7 +104,7 @@ $filter .= "lower(ticket.description) like lower('%".$_SESSION["ts_query_input"]
                         echo  badge_due_date($row["effective_date"]);
                         echo '</div>';
                     }else{
-                        echo '<div class="col" style="max-width: fit-content;padding-top:3px;padding-right: 0px;">';
+                        echo '<div class="col" style="max-width: fit-content;padding-top:3px;padding-right: 15px;">';
                         echo $image_profile;
                         echo '</div>';
                         echo '<div class="col" style="padding-left: 5px;padding-top: 5px;">';
