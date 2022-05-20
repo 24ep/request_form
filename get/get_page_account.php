4e9638d9c2e08921349e2f2c359670a436b57a51
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-20 19:53:27 +0700

    dbeug

--- get/get_page_account.php
+++ get/get_page_account.php
@@ -26,9 +26,8 @@ function get_page_account(){
    }
     $value_account .='<tr style="text-align-last: center;border: solid #dee2e6 1px;background-color: transparent;">';
     $value_account .= '<td>'.$row_account["id"].'</td>';
-    $value_account .= '<td>'.$row_account["username"].'</td>';
-    $value_account .= '<td><strong>'.$row_account["firstname"]." ".$row_account["lastname"].'<strong><br><small>Username : '.$row_account["username"].'</small></td>';
-    $value_account .= '<td>'.$row_account["nickname"].'</td>';
+    $value_account .= '<td style="text-align-last: left;"><strong>'.$row_account["firstname"]." ".$row_account["lastname"].'('.$row_account["nickname"].')</strong><br><small>Username : '.$row_account["username"].'</small></td>';
+    $value_account .= '<td></td>';
     $value_account .= '<td>'.$status.'</td>';
     $value_account .= '<td>'.$row_account["department"].'</td>';
     $value_account .= '<td>'.$row_account["register_type"].'</td>';
@@ -42,8 +41,7 @@ function get_page_account(){
  $table =  '<table class="table" id="st_account_tb"  name="st_account_tb" table table-bordered">
 <thead>
     <tr style="text-align-last: center;border: solid #dee2e6 1px;background-color: transparent;">
-      <th scope="col">Account ID</th>
-      <th scope="col">Username</th>
+      <th style="text-align-last: left;" scope="col">Account ID</th>
       <th scope="col">Firstname - Lastname</th>
       <th scope="col">Nickname</th>
       <th scope="col">status</th>
