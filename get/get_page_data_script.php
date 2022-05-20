fdff2170036d391e3199429efde30f25b43ab066
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-20 20:40:12 +0700

    dbeug

--- get/get_page_data_script.php
+++ get/get_page_data_script.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.0/css/dataTables.bootstrap5.min.css">
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.0/css/dataTables.bootstrap5.min.css"></script>
 <style>
 .dataTables_wrapper {
     position: relative;
