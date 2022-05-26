f8b0fbe44819473b74492055057d6a0093267ae9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-05-26 20:21:46 +0700

    debug

--- get/get_content_request_detail.php
+++ get/get_content_request_detail.php
@@ -75,6 +75,23 @@ if($id=="" or $id == null){
   <link rel="stylesheet" type="text/css" href="https://content-service-gate.cdse-commercecontent.com/base/css-theam/light.css">
   <link rel="stylesheet" type="text/css" href="https://content-service-gate.cdse-commercecontent.com/base/css-theam/light-modern.css">
   <link rel="stylesheet" type="text/css" href="https://content-service-gate.cdse-commercecontent.com/base/css-theam/tree-ticket.css">
+  <style>
+  
+#overlay {
+  position: fixed; /* Sit on top of the page content */
+  display: none; /* Hidden by default */
+  width: 100%; /* Full width (cover the whole page) */
+  height: 100%; /* Full height (cover the whole page) */
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+  z-index: 2; /* Specify a stack order in case youre using a different order for other elements */
+  cursor: pointer; /* Add a pointer on hover */
+}
+  </style>
+  
   </head>
   <body onload="'.$overlay.'">
   <div id="overlay"></div>
