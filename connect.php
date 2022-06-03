50a37c8de8abcafff170c61e6cc489256f8098e3
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-04 00:02:33 +0700

    debug

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
  ?>
