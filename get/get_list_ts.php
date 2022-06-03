5d47ce9740263b2ede83ebd81ace0f4671507467
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-03 20:57:23 +0700

    debug

--- get/get_list_ts.php
+++ get/get_list_ts.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
-include_once("get_function_badge.php");
-include_once("get_default_profile_image.php");
+if(!@include_once("get_function_badge.php")) {
+  include_once("get_function_badge.php");
+  include_once("get_default_profile_image.php");
+}
+
 
 //query limit
 if($_POST["ts_command_limit"]<>""){
