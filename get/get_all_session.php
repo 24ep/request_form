5f620d7c602ce9abbb6a385c825426c7370c3c50
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-25 15:42:09 +0700

    debug

--- get/get_all_session.php
+++ get/get_all_session.php
@@ -3,5 +3,5 @@
     echo "<h3> PHP List All Session Variables</h3>";
     echo $_SERVER['HTTP_HOST'];
     foreach ($_SESSION as $key=>$val)
-    echo $key." ".$val."<br/>";
+    echo $key." =  ".$val."<br/>";
 ?>
