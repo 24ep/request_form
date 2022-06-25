fc5d159b81c00f9aeca585efae35e823fd73f144
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-25 23:10:49 +0700

    debug

--- get/get_list_bucket.php
+++ get/get_list_bucket.php
@@ -23,30 +23,30 @@
   date_default_timezone_set("Asia/Bangkok");
   $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
   mysqli_query($con, "SET NAMES 'utf8' ");
-  $query = "SELECT pb.id,pb.project_name,
-  pb.description,
-  pb.owner,
-  pb.sticky,
-  pb.status,
-  pb.prefix,
-  pb.color_project
-  FROM all_in_one_project.project_bucket as pb order by pb.sticky DESC" or die("Error:" . mysqli_error($con));
+  $query = "SELECT pb.prefix,pb.project_name,count(cr.id) as count_ticket
+  FROM all_in_one_project.project_bucket as pb
+  left join all_in_one_project.content_request as cr
+  on cr.ticket_template = pb.prefix and lower(cr.status) not in ('close','cancel') 
+  group by pb.prefix , pb.project_name
+  order by pb.sticky DESC" or die("Error:" . mysqli_error($con));
   $result = mysqli_query($con, $query);
 
-//   echo '
-//         <input type="checkbox" class="btn-check" name="bucket_checking" id="all" autocomplete="off">
-//         <label onclick="update_project_sticky_badge()" class="btn btn-outline-primary btn-sm bk-cr" for="all">Show all</label>
-//     ';
   while($row = mysqli_fetch_array($result)) {
     if(strpos( $_SESSION["prefix_project_sticky"],$row['prefix'])!==false){
         echo '
         <input type="checkbox" class="btn-check"  value="'.$row["prefix"].'"  name="bucket_checking" id="'.$row["prefix"].'" autocomplete="off" checked>
-        <label  onclick="update_project_sticky_badge(&#39;'.$row["prefix"].'&#39;);" class="btn btn-outline-primary btn-sm bk-cr shadow-sm" for="'.$row["prefix"].'" >'.$row["project_name"].'</label>
+        <label  onclick="update_project_sticky_badge(&#39;'.$row["prefix"].'&#39;);" class="btn btn-outline-primary btn-sm bk-cr shadow-sm" for="'.$row["prefix"].'" >
+        '.$row["project_name"].'
+        <span class="badge bg-danger">'.$row["count_ticket"].'</span>
+        </label>
     ';
     }else{
         echo '
         <input type="checkbox" class="btn-check"  value="'.$row["prefix"].'" name="bucket_checking" id="'.$row["prefix"].'" autocomplete="off">
-        <label  onclick="update_project_sticky_badge(&#39;'.$row["prefix"].'&#39;);" class="btn btn-outline-primary btn-sm bk-cr shadow-sm" for="'.$row["prefix"].'">'.$row["project_name"].'</label>
+        <label  onclick="update_project_sticky_badge(&#39;'.$row["prefix"].'&#39;);" class="btn btn-outline-primary btn-sm bk-cr shadow-sm" for="'.$row["prefix"].'">
+        '.$row["project_name"].'
+        <span class="badge bg-danger">'.$row["count_ticket"].'</span>
+        </label>
     ';
     }
     
