877d136022fe05f01273a787fd2f97249a2d135b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-25 13:58:58 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -377,14 +377,7 @@
                                         </form>
                                     </nav>
                                     <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-                                        <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
-                                        <label class="btn btn-outline-primary btn-sm bk-cr" for="btncheck1">Checkbox 1</label>
-
-                                        <input type="checkbox" class="btn-check" id="btncheck2" autocomplete="off">
-                                        <label class="btn btn-outline-primary btn-sm bk-cr" for="btncheck2">Checkbox 2</label>
-
-                                        <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
-                                        <label class="btn btn-outline-primary btn-sm bk-cr" for="btncheck3">Checkbox 3</label>
+                                        <?php include('get/get_list_bucket.php'); ?>
                                     </div>
                                     <div class="row" id="get_ts_admin_console">
                                     </div>
