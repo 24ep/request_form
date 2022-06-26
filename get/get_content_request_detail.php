1c0e9b1b76fcfa80be80487a667938f429ccaf06
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-26 21:53:31 +0700

    debug

--- get/get_content_request_detail.php
+++ get/get_content_request_detail.php
@@ -451,19 +451,19 @@ echo "<script>console.log('".$_SESSION["department"]."');</script>";
             //   </div>
             // </div>';
 
-            // echo '
-            // <div class="row" >
-            //   <div class="col" style=" padding-left: 25px;text-align-last: left;"><strong>'.$sj.' Owner</strong></div>|
-            //   <div class="col " style=" padding-left: 25px;text-align-last: right;">
-            //   <div class="directorist-select directorist-select-multi" id="multiSelect" data-isSearch="true" 
-            //   data-multiSelect="['.$username_op_set.']" data-max="9">
-            //     <select class="form-select form-select-sm" data-live-search="true"  id="cr_edit_case_officer" 
-            //      name="cr_edit_case_officer" onchange="update_cr_detail('.$id.','.$cr_edit_case_officer.')" 
-            //      style="border: 0px;font-weight: bold;background-color: transparent;" aria-label=".form-select-lg example">
-            //     '.$username_op .'
-            //     </select>
-            //   </div>
-            // </div>';
+            echo '
+            <div class="row" >
+              <div class="col" style=" padding-left: 25px;text-align-last: left;"><strong>'.$sj.' Owner</strong></div>|
+              <div class="col " style=" padding-left: 25px;text-align-last: right;">
+              <div class="directorist-select directorist-select-multi" id="multiSelect" data-isSearch="true" 
+              data-multiSelect="['.$username_op_set.']" data-max="9">
+                <select  data-live-search="true"  id="cr_edit_case_officer" 
+                 name="cr_edit_case_officer" onchange="update_cr_detail('.$id.','.$cr_edit_case_officer.')" 
+                 style="border: 0px;font-weight: bold;background-color: transparent;" aria-label=".form-select-lg example">
+                '.$username_op .'
+                </select>
+              </div>
+            </div>';
 
             echo '
             <div class="row" >
@@ -812,7 +812,9 @@ function off_overlay() {
 baguetteBox.run('.baguetteBoxFour'); //preview image
 // pureScriptSelect('#multiSelect'); //multi select
 new SlimSelect({
-  select: '#cr_edit_case_officer'
+  select: '#cr_edit_case_officer',
+  closeOnSelect: false,
+  addToBody: true
 })
 </script>
 
