98324d7450da9dbbd05d1f6431a04ce7d967fde9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-26 19:16:42 +0700

    debug

--- get/get_content_request_detail.php
+++ get/get_content_request_detail.php
@@ -427,7 +427,7 @@ echo "<script>console.log('".$_SESSION["department"]."');</script>";
               <div class="col" style=" padding-left: 25px;text-align-last: left;"><strong>'.$sj.' Owner</strong></div>|
               <div class="col " style=" padding-left: 25px;text-align-last: right;">
               <div class="directorist-select directorist-select-multi" id="multiSelect" data-isSearch="true">
-                <select class="form-select form-select-sm" data-live-search="true"  id="cr_edit_case_officer"  name="cr_edit_case_officer" onchange="update_cr_detail('.$id.','.$cr_edit_case_officer.')"  style="border: 0px;font-weight: bold;background-color: transparent;" aria-label=".form-select-lg example">
+                <select class="form-select form-select-sm" data-live-search="true"  id="cr_edit_case_officer"  name="cr_edit_case_officer" onselect="update_cr_detail('.$id.','.$cr_edit_case_officer.')"  style="border: 0px;font-weight: bold;background-color: transparent;" aria-label=".form-select-lg example">
                 '.$username_op .'
                 </select>
               </div>
