02ddc4fba8069c7b336d482e77cd90819a1348f6
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-06-26 17:19:34 +0700

    dbeug

--- get/get_comment_ticket.php
+++ get/get_comment_ticket.php
@@ -14,7 +14,7 @@ function get_comment($id){
         while($row_attach = mysqli_fetch_array($result_attach)) {
             $herf = str_replace("../..",'../..',$row_attach['file_path'].$row_attach['file_name']);
             if($row_attach['is_image']==1){
-                $list_image .= '<a target= "_blank" href="'.$herf.'"><img src="'.$row_attach['file_path'].$row_attach['file_name'].'"  class="img-thumbnail" alt="'.$row_attach['file_name'].'" style="object-fit:cover;width:180px;height:180px;margin-right:5px"></a>';
+                $list_image .= '<div class="baguetteBoxFour gallery"><a target= "_blank" href="'.$herf.'"><img src="'.$row_attach['file_path'].$row_attach['file_name'].'"  class="img-thumbnail" alt="'.$row_attach['file_name'].'" style="object-fit:cover;width:180px;height:180px;margin-right:5px"></a><div>';
             }else{
                 $list_file .= '<a type="button" href="'.$herf.'" style="margin-right:0px;margin-bottom:5px;font-size: 12px;border-color: #7ec1a2;"class="btn btn-outline-success btn-sm "><ion-icon name="document-outline" style="font-size: 12px;"></ion-icon>'.$row_attach['file_name'].'</a>';
             }
