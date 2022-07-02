12b1070ceae0c40a32dd47ae468365c6eaa0ac2a
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-02 22:57:59 +0700

    debug

--- get/get_list_job_update.php
+++ get/get_list_job_update.php
@@ -8,11 +8,11 @@
  lg.id,
  lg.`action`,
  lg.action_date, lg.action_by, lg.action_table, lg.action_data_id, lg.nt_readable, lg.nt_readed , ac.firstname,ac.lastname  ,
- case when lg.nt_readed like '%".$row['username']."%' then 'readed' end as readed  
+ case when lg.nt_readed like '%".$_SESSION['username']."%' then 'readed' end as readed  
  FROM all_in_one_project.log  as lg
  left join all_in_one_project.account as ac 
  on ac.username = lg.action_by
- where lg.nt_readable like '%".$row['username']."%'  and lg.action_by <> '".$row['username']."'
+ where lg.nt_readable like '%".$_SESSION['username']."%'  and lg.action_by <> '".$_SESSION['username']."'
  order by lg.id desc
  limit 100
  ";
