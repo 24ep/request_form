e1bf7e14c8c018ba750c8626e7e169a68f08a086
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-02 21:42:37 +0700

    debug

---
+++ get/get_count_nt_unread.php
@@ -0,0 +1,14 @@
+<?php
+ session_start();
+ date_default_timezone_set("Asia/Bangkok");
+ $con = mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+ mysqli_query($con, "SET NAMES 'utf8' ");
+ $query = "
+ SELECT count(id) as count_id FROM all_in_one_project.log where nt_readable like '%".$_SESSION["username"]."%'
+ ";
+ $result = mysqli_query($con, $query);
+ while($row = mysqli_fetch_array($result)) {
+    echo $row['count_id'];
+  }
+  mysqli_close($con);
+  ?>
