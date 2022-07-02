70c7654a23a18582f1a8e152a3932c339cff654c
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-02 15:30:00 +0700

    debug

--- action/action_update_cr_detail.php
+++ action/action_update_cr_detail.php
@@ -149,7 +149,7 @@ session_start();
          toast.show();</script>';
           if($value_name=='status' and $value_change<>'close'){
             echo '<script>
-             var child_c = document.getElementById("#crid_".'.$_POST["id"].');
+             var child_c = document.getElementById("#crid_'.$_POST["id"].'");
              var parent_c = document.getElementById("ul_".$value_change);
             child_c.appendTo(parent_c);	
             </script>';
