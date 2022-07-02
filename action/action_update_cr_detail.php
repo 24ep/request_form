e7c6533c727e7e59aa96e3c3709dc79cec25f93f
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-02 15:35:32 +0700

    dbeug

--- action/action_update_cr_detail.php
+++ action/action_update_cr_detail.php
@@ -149,9 +149,8 @@ session_start();
          toast.show();</script>';
           if($value_name=='status' and $value_change<>'close'){
             echo '<script>
-             var child_c = document.getElementById("#crid_'.$_POST["id"].'");
-             var parent_c = document.getElementById("ul_'.$value_change.'");
-            child_c.appendTo(parent_c);	
+            $("#crid_'.$_POST["id"].'").appendTo("ul_'.$value_change.'");
+        	
             </script>';
           }
 	}else{
