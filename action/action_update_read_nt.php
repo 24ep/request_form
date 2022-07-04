455dc9239ac0774fdaa548c8d5432c782dc175db
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-04 23:38:57 +0700

    debug

--- action/action_update_read_nt.php
+++ action/action_update_read_nt.php
@@ -5,8 +5,7 @@
     $id = $_POST["id"];
     $sql = "UPDATE all_in_one_project.log
 	SET  nt_readed= concat(nt_readed,',','".$_SESSION['username']."')
-	WHERE (log.nt_readable like '%".$_SESSION['username']."%' and   log.action_by <> '".$_SESSION['username']."' ) 
-    and log.action_by <> '".$_SESSION['username']."'";
+	WHERE (nt_readable like '%".$_SESSION['username']."%' and  action_by <> '".$_SESSION['username']."' )";
     $query_time_zone = mysqli_query($con,"SET time_zone = 'Asia/Bangkok';");
     $query = mysqli_query($con,$sql);
     mysqli_close($con);
