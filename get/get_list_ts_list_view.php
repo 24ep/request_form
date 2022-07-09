ebd47c95fcc0ce4b3ea001a14e1b2e2078612323
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-09 11:39:05 +0700

    dbeug

--- get/get_list_ts_list_view.php
+++ get/get_list_ts_list_view.php
@@ -68,74 +68,27 @@ $filter .= "lower(ticket.description) like lower('%".$_SESSION["ts_query_input"]
         where ".$ts_filter."
         GROUP BY  ticket.id order by ".$sort_de_status."  limit ".$ts_command_limit;
         $result = mysqli_query($con, $query);
-        echo "<ul id='ul_".$status."' style='padding:15px'>";
+        echo " <table class='table table-hover'>";
+        echo "  <tr>
+                    <th>Id</th>
+                    <td>Title</td>
+                    <td>STATUS</td>
+                    <td>Request for</td>
+                    <td>Due date</td>
+                </th>";
           while( $row = mysqli_fetch_array($result)) {
                 ?>
             <!-- ui -->
-            <li class="row shadow-sm rounded md-3 p-2 bg-white position-relative npd-card-bording-priority-<?php echo strtolower($row['piority']); ?>"
-                onclick="cr_id_toggle(<?php echo $row['id'];?>) " data-bs-toggle="offcanvas" data-bs-target="#detail_cr"
-                data-bucket="<?php echo $row['prefix'];?>" data-cr-id="<?php echo $row['id'];?>"
-                data-cr-participant="<?php echo strtolower($row['participant']);?>" id="crid_<?php echo $row['id'];?>"
-                data-cr-title="<?php echo strtolower($row['title']);?>" aria-controls="offcanvasExample">
-                <div class="row" style="padding-right: 0px;">
-                    <div class="col-10" style="padding-right: 0px;" onclick="cr_id_toggle(<?php echo $row['id'];?>) "
-                        data-bs-toggle="offcanvas" data-bs-target="#detail_cr" aria-controls="offcanvasExample">
-                        <?php echo "<strong style='color: ".$row["color_project"].";'>".$row["ticket_template"]."-".$row["id"]."</strong> ".$row["title"]; ?>
-                    </div>
-                    <div class="col-2" style="padding-right: 0px;" onclick="cr_id_toggle(<?php echo $row['id'];?>) "
-                        data-bs-toggle="offcanvas" data-bs-target="#detail_cr" aria-controls="offcanvasExample">
-                        <?php
-                            if($row["contain_content"] == 'Yes'){
-                                echo '<ion-icon style="color:#41baf0!important" name="pencil-outline"></ion-icon>';
-                            }
-                            if($row["contain_studio"] == 'Yes'){
-                                echo '<ion-icon style="color:#CC0000!important" name="image-outline"></ion-icon>';
-                            }
-                            if($row["contain_datapump"] == 'Yes'){
-                                echo '<ion-icon style="color:#e126ec!important" name="server-outline"></ion-icon>';
-                            }
-                            if($row["contain_data"] == 'Yes'){
-                                echo '<ion-icon style="color:#41baf0!important" name="receipt-outline"></ion-icon>';
-                            }
-                            ?>
-                    </div>
-                </div>
-                <hr style="margin: 5px;color: #6c757d8c;">
-                <div class="row" style="margin-bottom: 0px;" onclick="cr_id_toggle(<?php echo $row['id'];?>) "
-                    data-bs-toggle="offcanvas" data-bs-target="#detail_cr">
-                    <?php
-                                    $ef_badge = "";
-                                    $image_profile = "";
-                                    if($row['case_officer']==null or $row['case_officer']=="" or $row['case_officer']=="unassign"){
-                                        echo '<div class="col card-unassin-bt" >';
-                                        echo  '<a type="button" class="btn btn-sm btn-outline-secondary" style="border-radius: 15px;">Unassign</a>';
-                                        echo '</div>';
-                                        echo '<div class="col card-unassin-eft" >';
-                                        echo  badge_due_date($row["effective_date"]);
-                                        echo '</div>';
-                                    }else{
-                                    $ef_badge = "";
-                                    $image_profile = "";
-                                    $officer_display =  explode(",",$row['case_officer']);
-                                    foreach ($officer_display as $officer){
-                                    $image_profile = profile_image($officer,$row['department'],25,$officer,1);
-                                        echo '<div class="badge-profile">';
-                                        echo '<div class="col">';
-                                        echo $image_profile;
-                                        echo '</div>';
-                                        echo '<div class="col card-assign-name">';
-                                        echo ucwords($officer);
-                                        echo '</div>';
-                                        echo '</div>';
-                                    }
-                                    echo '<div class="col card-assigned-eft">';
-                                        echo  badge_due_date($row["effective_date"]);
-                                        echo '</div>';
-                                    }
-                    ?>
-                </div>
-            </li>
+            <tr>
+                <th><?php echo $row['id']; ?></th>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['status']; ?></td>
+                <td><?php echo $row['ticket_type']; ?></td>
+                <td><?php echo $row['effective_fate']; ?></td>
+            </tr>
+            <!-- ui -->
         <?php $i++; }
+        echo '</table>';
                       echo "</ul>";
                     mysqli_close($con);
         }
