0a65218ee86f18081535f5df97159ab87577a2ed
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-09 10:50:03 +0700

    dbeug

--- homepage.php
+++ homepage.php
@@ -386,16 +386,16 @@ if (!$_SESSION["login_csg"]){
                                             </datalist>
                                         </form>
                                     </nav>
-                                    <div class="btn-group btn-group-sm" role="group"
+                                    <div class="btn-group btn-group-sm" style="position: inherit;" role="group"
                                         aria-label="Basic checkbox toggle button group">
                                         <?php include('get/get_list_bucket.php'); ?>
 
-                                        <ul class="nav nav-pills mb-3" id="pills-tab" style="right: 0;position: fixed;padding: 10px 40px;" role="tablist">
+                                        <ul class="nav nav-pills mb-3" id="pills-tab" style="right: 0;position: absolute;padding: 10px 40px;" role="tablist">
                                             <li class="nav-item" role="presentation">
-                                                <button class="nav-link active" id="pills-list_view_ts-tab" data-bs-toggle="pill" data-bs-target="#pills-list_view_ts" type="button" role="tab" aria-controls="pills-list_view_ts" aria-selected="true"><ion-icon name="list-outline"></ion-icon></button>
+                                                <button class="nav-link ts-view active" id="pills-list_view_ts-tab" data-bs-toggle="pill" data-bs-target="#pills-list_view_ts" type="button" role="tab" aria-controls="pills-list_view_ts" aria-selected="true"><ion-icon name="list-outline"></ion-icon></button>
                                             </li>
                                             <li class="nav-item" role="presentation">
-                                                <button class="nav-link" id="pills-board_view_ts-tab" data-bs-toggle="pill" data-bs-target="#pills-board_view_ts" type="button" role="tab" aria-controls="pills-board_view_ts" aria-selected="false"><ion-icon name="grid-outline"></ion-icon></button>
+                                                <button class="nav-link ts-view" id="pills-board_view_ts-tab" data-bs-toggle="pill" data-bs-target="#pills-board_view_ts" type="button" role="tab" aria-controls="pills-board_view_ts" aria-selected="false"><ion-icon name="grid-outline"></ion-icon></button>
                                             </li>
                                         </ul>
                                         <div class="tab-content" id="pills-tabContent">
