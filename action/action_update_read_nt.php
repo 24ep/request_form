2e6ee744f5991393cefe781aa26044bdfd4da9c1
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-17 14:48:39 +0700

    debug

--- action/action_update_read_nt.php
+++ action/action_update_read_nt.php
@@ -4,9 +4,10 @@
     date_default_timezone_set("Asia/Bangkok");
     $sql = "UPDATE all_in_one_project.log
 	SET  nt_readed= concat(nt_readed,',','".$_SESSION['username']."')
-	WHERE (nt_readable like '%".$_SESSION['username']."%' and  action_by <> '".$_SESSION['username']."' ) and (nt_readed not like '%".$_SESSION['username']."%' or nt_readed is null)";
+	where nt_readable like '%".$_SESSION["username"]."%' and (nt_readed not like '%".$_SESSION['username']."%' or nt_readed is null) and action_by <> '".$_SESSION["username"]."'";
     $query_time_zone = mysqli_query($con,"SET time_zone = 'Asia/Bangkok';");
     $query = mysqli_query($con,$sql);
     mysqli_close($con);
     //header( "location: https://cdsecommercecontent.ga/base/homepage.php");
     ?>
+
