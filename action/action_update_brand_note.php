bbcc6f61ccfb2ecbbd324c3a4abbfff831a6ba9d
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-17 16:06:55 +0700

    debug

--- action/action_update_brand_note.php
+++ action/action_update_brand_note.php
@@ -4,7 +4,7 @@
     $dataoutput = $_POST['dataoutput'];
     $brand = $_POST['brand'];
     date_default_timezone_set("Asia/Bangkok");
-    $sql = "INSERT INTO all_in_one_project.brand_editor (body,version,brand) VALUES ('".$dataoutput."',1,'".$brand."') ON DUPLICATE KEY UPDATE body='".$brand_content."',version = version+1";
+    $sql = "INSERT INTO all_in_one_project.brand_editor (body,version,brand) VALUES ('".$dataoutput."',1,'".$brand."') ON DUPLICATE KEY UPDATE body='".$dataoutput."',version = version+1";
     $query_time_zone = mysqli_query($con,"SET time_zone = 'Asia/Bangkok';");
     $query = mysqli_query($con,$sql);
     mysqli_close($con);
