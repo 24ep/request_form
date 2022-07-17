d567d046f7f347d6450732d060e1138c179f5fb4
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-17 20:09:39 +0700

    debug

---
+++ action/action_endpoint_uploadfiles.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Get the name of the uploaded file */
+$filename = $_FILES['file']['name'];
+
+/* Choose where to save the uploaded file */
+$location = "../../attachment/brand_editor/".$filename;
+
+/* Save the uploaded file to the local filesystem */
+if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
+    echo '{
+        "success" : 1,
+        "file": {
+            "url" : "https://content-service-gate.cdse-commercecontent.com/attachment/brand_editor/'.$filename.'",
+        }
+    }';
+} else { 
+  echo 'Failure'; 
+}
+
+?>
