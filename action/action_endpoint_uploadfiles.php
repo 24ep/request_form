3e0b9226f7557803b4238c48f9e6d7fd25714aec
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-17 20:56:23 +0700

    debug

--- action/action_endpoint_uploadfiles.php
+++ action/action_endpoint_uploadfiles.php
@@ -27,7 +27,7 @@ if (empty($errors)) {
                 "url" : "https://content-service-gate.cdse-commercecontent.com/attachment/brand_editor/'.$filename.'",
             }
         }';
-        echo '<script>'.JSON.parse($arr).'</script>';
+        echo '<script>JSON.parse('.$arr.')</script>';
     } else {
         echo "An error occurred. Try again or contact your system administrator.";
     }
