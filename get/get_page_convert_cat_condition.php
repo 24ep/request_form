a04e84648d0cf732dd10972a65accb777db533e7
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-22 23:35:58 +0700

    debug

---
+++ get/get_page_convert_cat_condition.php
@@ -0,0 +1,65 @@
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+<style>
+.dataTables_wrapper {
+    position: relative;
+    clear: both;
+    background: white;
+    padding: 50px;
+    border-radius: 10px;
+    box-shadow: 0 .3rem 1rem rgba(0,0,0,.15)!important;
+}
+</style>
+<?php
+ session_start();
+ include_once("get_default_profile_image.php");
+function get_page_convert_cat_condition(){
+ $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+ $query = "SELECT * FROM u749625779_cdscontent.pim_cate_convert_condition" or die("Error:" . mysqli_error($con));
+ $result = mysqli_query($con, $query);
+ 
+ while($row = mysqli_fetch_array($result)) {
+    $value .= '<tr>';
+    $value .= '<td scope="col">'.$row['id'].'</td>';
+    $value .= '<td scope="col"><ul>
+                <li> dept :'.$row['department'].'</li>
+                <li> sdept :'.$row['sub_department'].'</li>
+                </ul>
+               </td>';
+    $value .= '<td scope="col">Action</td>';
+    $value .= '</tr>';
+}
+
+
+?>
+<?php
+ $table =  '<table class="table" id="st_convert_cate_mapping"  name="st_convert_cate_mapping" >
+<thead>
+    <tr style="text-align-last: center;border: solid #dee2e6 1px;background-color: transparent;">
+      <th style="text-align-last: center;" scope="col">id</th>
+      <th scope="col" style="width: 30%;text-align-last: left;">Condition</th>
+      <th scope="col">categories_path_id/th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    '.$value.'
+  </tbody>
+</table>';
+
+return $table;
+}
+?>
+
+<script>
+  $(document).ready( function () {
+  $('#st_convert_cate_mapping').DataTable({
+        lengthMenu: [
+            [10,20, 50, 100, -1],
+            [10, 20,50, 100, 'All'],
+        ],
+    });
+
+} );
+
+</script>
