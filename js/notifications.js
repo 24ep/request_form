0a2915cf0f9202d940c470c4ff25ddfde51a70ab
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-30 22:10:21 +0700

    debug

--- js/notifications.js
+++ js/notifications.js
@@ -1,17 +1,17 @@
 function get_count_read_nt() {
-    $.post("base/get/get_count_nt_unread.php", {}, function(data) {
+    $.post("get/get_count_nt_unread.php", {}, function(data) {
         $('#get_count_nt_unread').html(data);
     });
 }
 
 function update_readed_nt() {
-    $.post("base/action/action_update_read_nt.php", {}, function(data) {
+    $.post("action/action_update_read_nt.php", {}, function(data) {
         // $('#project_bucket').html(data);
     });
 }
 
 function get_list_update_job() {
-    $.post("base/get/get_list_job_update.php", {}, function(data) {
+    $.post("get/get_list_job_update.php", {}, function(data) {
         $('#get_list_job_update').html(data);
         timeago().render(document.querySelectorAll('.timeago'));
         update_readed_nt();
