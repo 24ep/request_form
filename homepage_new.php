0f370b3dc8e5542fae40ae9337f3be1686259cb4
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-30 16:56:07 +0700

    debu

--- homepage_new.php
+++ homepage_new.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css-theam/light-new.css">
-    <script src="js/notificatios.js"></script>
+    <script src="js/notifications.js"></script>
 </head>
 <body>
         <div class="col-2 list_bra shadow">
