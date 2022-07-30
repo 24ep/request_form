a64b1181d7ef19725333f1c69f62e92192c16c00
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-31 02:42:29 +0700

    debug

--- get/get_list_new_job_new.php
+++ get/get_list_new_job_new.php
@@ -7,7 +7,7 @@ function badge_status($status){
   }elseif($status=="checking"  ){
     $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: transparent;color:#ffff7e;border:#ffff7e solid 1px;width: 100%;">checking</button>';
   }elseif( $status=="on-production"){
-    $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: transparent;color:ff9a59;border:#ff9a59 solid 1px;width: 100%;">on-production</button>';
+    $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: transparent;color:#ff9a59;border:#ff9a59 solid 1px;width: 100%;">on-production</button>';
   }elseif($status=="accepted" or $status=="approved"){
     $status = '<button type="button" class="btn btn-secondary btn-sm shadow-sm" style="background: transparent;color:#7befb2;border:#7befb2 solid 1px;width: 100%;">'.$status.'</button>';
   }elseif($status=="waiting confirm"){
