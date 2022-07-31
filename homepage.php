5f4761a8446f97b5ec7be08c9b5a55464085f655
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-31 14:00:58 +0700

    debug

--- homepage.php
+++ homepage.php
@@ -1,9 +1,13 @@
 <?php 
 session_start();
+if (!$_SESSION["login_csg"]){ 
+    Header("Location: login");
+}else{
 if($_SESSION="poojaroonwit"){
     include('homepage_new.php');
 }else{
     include('homepage_old.php');
+}
 }
 
  ?>
