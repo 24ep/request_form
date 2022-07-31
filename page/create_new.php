3a293a55c0dadac96a453476942e8c333e835e89
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-31 11:57:30 +0700

    debug

--- page/create_new.php
+++ page/create_new.php
@@ -19,7 +19,7 @@
                     <?php //echo $username_op;?>
                 </datalist> -->
             </div>
-            <div class="col-auto">
+            <div class="col-2">
               
                
                 <div class="input-group input-group-sm mb-3">
@@ -29,7 +29,7 @@
                 </select>
             </div>
             </div>
-            <div class="col-auto">
+            <div class="col-4">
             <div class="input-group input-group-sm mb-3" style=" width: 500px;">
                 <span class="input-group-text" id="inputGroup-sizing-sm">Search</span>
                 <input type="text" class="form-control" id="brand_filter" onchange="filter_update();" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Dept , Sub Dept , Brand , ID">
