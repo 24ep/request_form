044aa7afa2e24039ac51bb17f1faf00dc4cf0b05
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-31 12:01:02 +0700

    debug

--- page/create_new.php
+++ page/create_new.php
@@ -2,15 +2,15 @@
     <div class="tab-content" id="myTabContent">
         <div class="row align-items-center mt-3 mb-3">
             <div class="col-auto">
-            <div class="input-group input-group-sm mb-3 flex-nowrap">
-                <span class="input-group-text " id="addon-wrapping">Username</span>
-                <input value="<?php echo $_SESSION["user_filter"];?>" class="form-control"
-                    list="datalistOptionsuser" id="user_filter" onchange="filter_update();"
-                    placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
-                <datalist id="datalistOptionsuser">
-                    <?php echo $username_op;?>
-                </datalist>
-            </div>
+                <div class="input-group input-group-sm mb-3 flex-nowrap">
+                    <span class="input-group-text " id="addon-wrapping">Username</span>
+                    <input value="<?php echo $_SESSION["user_filter"];?>" class="form-control"
+                        list="datalistOptionsuser" id="user_filter" onchange="filter_update();" placeholder="Username"
+                        aria-label="Username" aria-describedby="addon-wrapping">
+                    <datalist id="datalistOptionsuser">
+                        <?php echo $username_op;?>
+                    </datalist>
+                </div>
                 <!-- <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Username</label>
                 <input value="<?php //echo $_SESSION["user_filter"];?>" class="selection_filter"
                     list="datalistOptionsuser" id="user_filter" onchange="filter_update();"
@@ -20,39 +20,48 @@
                 </datalist> -->
             </div>
             <div class="col-2">
-              
-               
+
+
                 <div class="input-group input-group-sm mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Status</span>
-                <select class="form-select" id="status_filter" onchange="filter_update();">
-                    <?php echo $request_new_status_op;?>
-                </select>
-            </div>
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Status</span>
+                    <select class="form-select" id="status_filter" onchange="filter_update();">
+                        <?php echo $request_new_status_op;?>
+                    </select>
+                </div>
             </div>
             <div class="col-5">
-            <div class="input-group input-group-sm mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Search</span>
-                <input type="text" class="form-control" id="brand_filter" onchange="filter_update();" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Dept , Sub Dept , Brand , ID">
-            </div>
-             
-            </div>
-            <div class="col-1">
-            <div class="input-group input-group-sm mb-3">
-                <span class="input-group-text" id="inputGroup-sizing-sm">Page</span>
-                <input type="number" class="form-control" id="brand_filter" onchange="filter_update();" id="pagenation_input" min=1
-                    <?php if($_SESSION["total_page_rnj"]<>""){echo "max=".$_SESSION["total_page_rnj"];}?>
-                    value="<?php echo $_SESSION["pagenation"];?>" onchange="filter_update();" placeholder="" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Dept , Sub Dept , Brand , ID">
-                    <span class="input-group-text" id="inputGroup-sizing-sm"><div id="total_page_nj"></div></span>
+                <div class="input-group input-group-sm mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Search</span>
+                    <input type="text" class="form-control" id="brand_filter" onchange="filter_update();"
+                        aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                        placeholder="Dept , Sub Dept , Brand , ID">
                 </div>
 
             </div>
-       
+            <div class="col-1">
+                <div class="input-group input-group-sm mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">Page</span>
+                    <input type="number" class="form-control" id="brand_filter" onchange="filter_update();"
+                        id="pagenation_input" min=1
+                        <?php if($_SESSION["total_page_rnj"]<>""){echo "max=".$_SESSION["total_page_rnj"];}?>
+                        value="<?php echo $_SESSION["pagenation"];?>" onchange="filter_update();" placeholder=""
+                        aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                        placeholder="Dept , Sub Dept , Brand , ID">
+                    <span class="input-group-text" id="inputGroup-sizing-sm">
+                        <div id="total_page_nj"></div>
+                    </span>
+                </div>
+
+            </div>
+
             <div class="col-auto">
-                <button class="btn btn-primary btn-sm" style="margin-left:10px" type="button" data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-                    <ion-icon size="small" name="add-outline"></ion-icon>
-                    New Request
-                </button>
+                <div class="input-group input-group-sm mb-3">
+                    <button class="btn btn-dark btn-sm" style="margin-left:10px" type="button"
+                        data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                        <ion-icon size="small" name="add-outline"></ion-icon>
+                        Create New
+                    </button>
+                </div>
             </div>
             <!-- </div> -->
             <div class="col-auto" style="right: 20px;position: absolute;margin-top: 15px;">
@@ -86,22 +95,22 @@
             </nav>
         </div>
     </div>
-    
-            <li class="row mb-3" style="color: #b3b3b3;font-weight: 600;text-align-last: center;">
-                <div class="col" scope="col">Ticket ID</div>
-                <div class="col" scope="col">Department</div>
-                <div class="col" scope="col">Brand</div>
-                <div class="col" scope="col">SKU</div>
-                <div class="col" scope="col">Production request</div>
-                <div class="col" scope="col">Project-type</div>
-                <div class="col" scope="col">launch date</div>
-                <div class="col" scope="col">Badge</div>
-                <div class="col" scope="col">Status</div>
-                <div class="col" scope="col">Action</div>
-</li>
-      
-        
-            <?php include('../get/get_list_new_job_new.php'); ?>
-     
-    
+
+    <li class="row mb-3" style="color: #b3b3b3;font-weight: 600;text-align-last: center;">
+        <div class="col" scope="col">Ticket ID</div>
+        <div class="col" scope="col">Department</div>
+        <div class="col" scope="col">Brand</div>
+        <div class="col" scope="col">SKU</div>
+        <div class="col" scope="col">Production request</div>
+        <div class="col" scope="col">Project-type</div>
+        <div class="col" scope="col">launch date</div>
+        <div class="col" scope="col">Badge</div>
+        <div class="col" scope="col">Status</div>
+        <div class="col" scope="col">Action</div>
+    </li>
+
+
+    <?php include('../get/get_list_new_job_new.php'); ?>
+
+
 </div>
