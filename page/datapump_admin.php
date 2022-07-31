1a3bf56f177689cda7ce905b01fdd6162058b8e1
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-07-31 19:04:54 +0700

    debug

--- page/datapump_admin.php
+++ page/datapump_admin.php
@@ -1,4 +1,21 @@
+<span style="
+    position: absolute;
+    color: white;
+    background: black;
+    padding: 5px 20px;
+    border-radius: 0px 0px 10px 0px;
+" class="shadow-sm span-ifram">Ifram</span>
 <iframe src="https://eor-admin.central.tech/upload"
-    style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"
+    style="
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+    margin: 0;
+    width: 84%;
+    overflow: hidden;
+    z-index: 1;"
     title="cto_datapump">
 </iframe>
