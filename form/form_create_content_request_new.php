9b0738fafa7436b2a0341c65f083e01bcb60918b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-03 15:09:25 +0700

    debug

--- form/form_create_content_request_new.php
+++ form/form_create_content_request_new.php
@@ -291,9 +291,9 @@ function submit_cr_form(id) {
 <script>
 function attaction_alert_cr(id) {
     //check requirt value
-    var cr_ticket_type = document.getElementById("cr_ticket_type").value;
+    var cr_ticket_type = document.getElementById("cr_ticket_type");
     var is_valid = [];
-    if (cr_ticket_type == '') {
+    if (cr_ticket_type.value == '') {
         cr_ticket_type.className += "is-invalid";
         is_valid[0] = true;
     } else {
@@ -304,10 +304,10 @@ function attaction_alert_cr(id) {
 
 
         if (cr_ticket_type == "Datapump Add Source" || cr_ticket_type == "Datapump Delete Source") {
-            var cr_dp_reason = document.getElementById("cr_dp_reason").value;
-            var cr_brand = document.getElementById("cr_brand").value;
+            var cr_dp_reason = document.getElementById("cr_dp_reason");
+            var cr_brand = document.getElementById("cr_brand");
             //reason
-            if (cr_dp_reason == '') {
+            if (cr_dp_reason.value == '') {
                 cr_dp_reason.className += "is-invalid";
                 is_valid[1] = true;
             } else {
@@ -317,7 +317,7 @@ function attaction_alert_cr(id) {
                 }
             }
             //brand
-            if (cr_brand == '') {
+            if (cr_brand.value == '') {
 
                 cr_brand.className += "is-invalid";
                 is_valid[2] = true;
