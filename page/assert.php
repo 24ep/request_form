4429982feb24178103b933d2358b28d7fbb10371
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-07 16:20:16 +0700

    debug

--- page/assert.php
+++ page/assert.php
@@ -11,18 +11,7 @@
   </div>
   <div class="container-fluid">
   <ul class="list-group">
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            A list item
-            <span class="badge bg-primary rounded-pill">14</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            A second list item
-            <span class="badge bg-primary rounded-pill">2</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            A third list item
-            <span class="badge bg-primary rounded-pill">1</span>
-        </li>
+        <?php include('base/get/get_list_assert_dri.php'); ?>
     </ul>
   </div>
 </nav>
