0f51371ba0e69daee8e7d2d622be4d6f30b0516d
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-07 16:42:22 +0700

    dbeug

--- page/assert.php
+++ page/assert.php
@@ -4,15 +4,28 @@
     <a class="navbar-brand">Asserts</a>
     <form class="d-flex" role="search">
       <input class="form-control me-2 form-control-sm" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-warning btn-sm" type="button">Create folder</button>
+      <button class="btn btn-warning btn-sm me-2" type="button">Create folder</button>
       <button class="btn btn-dark btn-sm" type="button">Create Block</button>
     </form>
     <!-- list -->
   </div>
-  <div class="container-fluid">
+  <div class="container-fluid m-3">
   <ul class="list-group">
+      <div id=list_asserts>
         <?php include('../get/get_list_assert_dri.php'); ?>
+      </div>
     </ul>
   </div>
 </nav>
 </div>
+
+<script>
+  function goto_dri(dri_id){
+    $.post("../get/get_list_assert_dri.php", {
+        dri_id: dri_id
+    }, function(data) {
+         $('#list_asserts').html(data);
+    });
+  }
+ 
+</script>
