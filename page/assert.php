8908086bc04cbd3304476d60dc737590c4e94771
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-07 11:46:39 +0700

    debug

--- page/assert.php
+++ page/assert.php
@@ -4,9 +4,25 @@
     <a class="navbar-brand">Asserts</a>
     <form class="d-flex" role="search">
       <input class="form-control me-2 form-control-sm" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-warning btn-sm" type="submit">Create folder</button>
-      <button class="btn btn-dark btn-sm" type="submit">Create Block</button>
+      <button class="btn btn-warning btn-sm" type="button">Create folder</button>
+      <button class="btn btn-dark btn-sm" type="button">Create Block</button>
     </form>
+    <!-- list -->
+    <ul class="list-group">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            A list item
+            <span class="badge bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            A second list item
+            <span class="badge bg-primary rounded-pill">2</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            A third list item
+            <span class="badge bg-primary rounded-pill">1</span>
+        </li>
+    </ul>
+
   </div>
 </nav>
 </div>
