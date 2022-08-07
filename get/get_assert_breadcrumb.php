88fa454746736f19784bbf5e6c8fac40fca05827
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-07 17:43:39 +0700

    dbeug

--- get/get_assert_breadcrumb.php
+++ get/get_assert_breadcrumb.php
@@ -21,8 +21,7 @@
   }
   $path_id_arr = explode("/",$path_id);
   
-  foreach( $dri_id_in_path as $path_id_arr){
-    echo $dri_id_in_path ;
+  foreach($path_id_arr as $dri_id_in_path ){
     $query = "SELECT dri.id, dri.code, dri.label, dri.parent
     FROM all_in_one_project.assets_directories as dri
     where  dri.id =   ".$dri_id_in_path."
