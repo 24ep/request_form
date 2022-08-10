0d3c1b7b31cca4061b2d34625e614f905937e091
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-10 19:27:51 +0700

    debug

---
+++ page/report.php
@@ -0,0 +1,22 @@
+<!-- <span style="
+    position: fixed;
+    color: white;
+    background: black;
+    padding: 5px 20px;
+    border-radius: 0px 0px 10px 0px;
+    z-index: 1;"
+class="shadow-sm span-ifram">https://eor-admin.central.tech/upload</span> -->
+    <iframe src="https://datastudio.google.com/u/0/navigation/reporting"
+    style="
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+    margin: 0;
+    width: 84%;
+    overflow: hidden;
+    z-index: -1;"
+    title="cto_datapump">
+</iframe>
