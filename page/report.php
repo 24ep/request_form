102354fcbce185a35397bb2f1f58c9ebce7adcd8
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-10 19:34:30 +0700

    debug

--- page/report.php
+++ page/report.php
@@ -6,7 +6,7 @@
     border-radius: 0px 0px 10px 0px;
     z-index: 1;"
 class="shadow-sm span-ifram">https://eor-admin.central.tech/upload</span> -->
-    <iframe src="https://datastudio.google.com/u/0/navigation/reporting"
+    <iframe src="https://datastudio.google.com/u/0/reporting/e0db1ece-dfb6-4b1f-9b5d-28a282350a51/page/IWuzC/edit"
     style="
     position: absolute;
     top: 0;
