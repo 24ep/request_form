9de75bcd1c99bf0cdcdafdd803e0121789ea2e02
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-10 19:14:49 +0700

    debug

--- homepage_new.php
+++ homepage_new.php
@@ -168,7 +168,7 @@ if (!$_SESSION["login_csg"]){
                         </a>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" data-bs-toggle="pill" type="button" role="tab" aria-selected="false"
+                        <a class="nav-link" data-bs-toggle="pill" type="button" role="tab" aria-selected="false" onclick="get_page('report');"
                             onclick="get_page('update_content');">
                             <ion-icon name="ticket"></ion-icon>Other Requests
                         </a>
