e95247010dbce47d49e0a7410ba3bae8f6cf8325
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-10 19:12:41 +0700

    debug

---
+++ page/notion.php
@@ -0,0 +1,22 @@
+<!-- <span style="
+    position: fixed;
+    color: white;
+    background: black;
+    padding: 5px 20px;
+    border-radius: 0px 0px 10px 0px;
+    z-index: 1;"
+class="shadow-sm span-ifram">https://eor-admin.central.tech/upload</span> -->
+<iframe src="https://www.notion.so/d82b34d77d634fe08f614f6fa6e23c7f?v=89dac92da27b4357b2ea7992c095b905"
+    style="
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+    margin: 0;
+    width: 84%;
+    overflow: hidden;
+    z-index: -1;"
+    title="cto_datapump">
+</iframe>
