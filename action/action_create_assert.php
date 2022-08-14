7381cc1fb91c60998fa5e831a1e56f694d14c963
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-14 12:15:55 +0700

    debug

--- action/action_create_assert.php
+++ action/action_create_assert.php
@@ -26,8 +26,32 @@ function insert_folder($parent,$label,$code,$path_id){
 	$query = mysqli_query($con,$sql);
     mysqli_close($con);
 }
+function insert_block($parent,$label,$code,$path_id){
+    date_default_timezone_set("Asia/Bangkok");
+    $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+    mysqli_query($con, "SET NAMES 'utf8' ");
+      $sql = "INSERT INTO brand_editor (
+      brand,
+      update_by,
+      dri_id
+      )
+      VALUES (
+      '".$label."',
+      '".$_SESSION['username']."',
+      '".$path_id."'
+      )";
+      $query = mysqli_query($con,$sql);
+      mysqli_close($con);
+  }
 
-insert_folder($parent,$label,$code,$path_id)
+if($create_type=='folder'){
+    insert_folder($parent,$label,$code,$path_id);
+}
+elseif($create_type=='block'){
+    insert_block($parent,$label,$code,$path_id);
+}
+
+
 
 
 ?>
