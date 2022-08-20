d0530dc9142b5f12eac18342c3be6257ddb69ab3
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-08-20 19:09:23 +0700

    debug

--- get/get_internal_console_nj_new.php
+++ get/get_internal_console_nj_new.php
@@ -26,7 +26,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Assign or Take owner
+      <ion-icon name="person-add-outline"></ion-icon> Assign or Take owner
       </button>
     </h2>
     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
@@ -73,7 +73,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Stamp Email send
+      <ion-icon name="mail-outline"></ion-icon> Stamp Email send
       </button>
     </h2>
     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
@@ -108,7 +108,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-       Update Contact
+      <ion-icon name="call-outline"></ion-icon> Update Contact
       </button>
     </h2>
     <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
@@ -138,7 +138,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Update contact
+      <ion-icon name="call-outline"></ion-icon> Update contact
       </button>
     </h2>
     <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
@@ -172,7 +172,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-heading4">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse4" aria-expanded="false" aria-controls="flush-collapse4">
-        Cancel ticket
+      <ion-icon name="call-outline"></ion-icon> Cancel ticket
       </button>
     </h2>
     <div id="flush-collapse4" class="accordion-collapse collapse" aria-labelledby="flush-heading4" data-bs-parent="#accordionFlushExample">
@@ -236,7 +236,7 @@
 <div class="accordion-item">
     <h2 class="accordion-header" id="flush-heading5">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse5" aria-expanded="false" aria-controls="flush-collapse5">
-      Create Sub-Ticket
+      <ion-icon name="cut-outline"></ion-icon> Create Sub-Ticket
       </button>
     </h2>
     <div id="flush-collapse5" class="accordion-collapse collapse" aria-labelledby="flush-heading5" data-bs-parent="#accordionFlushExample">
@@ -273,7 +273,7 @@
         <div class="accordion-item">
     <h2 class="accordion-header" id="flush-heading6">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse6" aria-expanded="false" aria-controls="flush-collapse6">
-        Stamp Start working
+      <ion-icon name="bonfire-outline"></ion-icon> Stamp Start working
       </button>
     </h2>
     <div id="flush-collapse6" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
@@ -303,7 +303,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-heading7">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse7" aria-expanded="false" aria-controls="flush-collapse7">
-        Stamp complete job
+      <ion-icon name="checkmark-done-circle-outline"></ion-icon> Stamp complete job
       </button>
     </h2>
     <div id="flush-collapse7" class="accordion-collapse collapse" aria-labelledby="flush-heading7" data-bs-parent="#accordionFlushExample">
