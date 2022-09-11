b9dcbf0dc2dcaa4fe3d94f5b04427b27e01e8ce4
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-11 12:37:14 +0700

    debug

---
+++ get/get_sub_manu_activity.php
@@ -0,0 +1,11 @@
+<div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">
+                    <ion-icon name="notifications-outline"></ion-icon> <strong>Activity</strong>
+                </h5>
+                <button type="button" class="btn-close" onclick="show_sub_manu();" ></button>
+            </div>
+            <div class="offcanvas-body">
+                <div id="get_list_job_update">
+                    <?php include('get_list_job_update.php'); ?>
+                </div>
+            </div>
---
+++ get/get_sub_manu_product_mantain.php
@@ -0,0 +1,60 @@
+<div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasRightLabel">
+                            <ion-icon name="storefront-outline"></ion-icon> <strong>Product mantain</strong>
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <small class="header_manu_bra">Datapump</small>
+                        <li><a id="nav_datapump_convert" onclick="get_page('datapump_convert');"
+                                class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>Datapump Convert
+                            </a></li>
+                        <li><a id="nav_datapump_admin" onclick="get_page('datapump_admin');"
+                                class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>Datapump Admin
+                            </a></li>
+                        <hr class="hr_manu_bra_in">
+                        <small class="header_manu_bra">Backend (PROD)</small>
+                        <li><a href="https://cto-pim.cloud.akeneo.com/" target="_blank" class="dropdown-item dp-link"
+                                type="button">
+                                <ion-icon name="shapes"></ion-icon>PIM
+                            </a></li>
+                        <li><a href="https://backend.central.co.th/gutentag/admin/index/index/key/36f79751536eaff794291a09bb5d1831d1f4453a2819d0d731cde2da72b95ffb/"
+                                target="_blank" class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>MDC
+                            </a></li>
+                        <hr class="hr_manu_bra_in">
+                        <small class="header_manu_bra">Backend (Stagging)</small>
+                        <li><a href="https://uat.central.co.th/th" target="_blank" class="dropdown-item dp-link"
+                                type="button">
+                                <ion-icon name="shapes"></ion-icon>FE
+                            </a></li>
+                        <li><a href="https://eor-admin-uat.central.tech/upload" target="_blank"
+                                class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>DATAPUMP Admin
+                            </a></li>
+                        <li><a href="https://cto-pim-uat.cloud.akeneo.com/user/login" target="_blank"
+                                class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>PIM
+                            </a></li>
+                        <li><a href="https://staging-mdc.central.co.th/admin/admin/index/index/key/84db97aa4de2e2c911fef6db4bab589d9eab2b47811d1d714d7193c2029e299f/"
+                                target="_blank" class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>MDC
+                            </a></li>
+                        <hr class="hr_manu_bra_in">
+                        <small class="header_manu_bra">Ability</small>
+                        <li><a id="nav_writer-auto-assign-console" onclick="get_page('writer-auto-assign-console');"
+                                class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>writer auto-assign console
+                            </a></li>
+                        <li><a id="nav_convert_mapping" onclick="get_page('convert_mapping');"
+                                class="dropdown-item dp-link" type="button">
+                                <ion-icon name="shapes"></ion-icon>Convert Mapping
+                            </a></li>
+                        <li><a id="nav_powerautomate" onclick="get_page('powerautomate');" class="dropdown-item dp-link"
+                                type="button">
+                                <ion-icon name="shapes"></ion-icon>Power automate
+                            </a></li>
+
+                    </div>
