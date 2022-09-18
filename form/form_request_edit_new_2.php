9561907688516ff78d5ba859889cab584d20512d
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-18 13:07:55 +0700

    debug

--- form/form_request_edit_new_2.php
+++ form/form_request_edit_new_2.php
@@ -1,6 +1,4 @@
 <?php
-
- session_start();
  $id = $_POST['id'];
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
 function return_input_box($att_name,$site_element,$current_value,$code_element,$enable_edit,$id){
