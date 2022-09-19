5ba3b77904f2b48b4ca0caa8754c219cd0d256a9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-19 18:36:22 +0700

    debug

--- form/form_production.php
+++ form/form_production.php
@@ -151,9 +151,11 @@ while($row = mysqli_fetch_array($result)) {
           }elseif($row["attribute_type"]=="textarea"){
           $element .= return_textarea_box($row["attribute_label"],"textarea",${"jc_".$row["attribute_code"]},"jc_edit_".$row["attribute_code"],$allow_in_edit,$id);
           }elseif($row["attribute_type"]=="single_select"){
-          $element .= return_s_select_box($row["attribute_label"],"single_select",${"jc_".$row["attribute_code"]},"jc_edit_".$row["attribute_code"],$row["attribute_id"],$allow_in_edit,$id);
+            $element .= return_input_box($row["attribute_label"],"text",${"jc_".$row["attribute_code"]},"jc_edit_".$row["attribute_code"],$allow_in_edit,$id);
+        //   $element .= return_s_select_box($row["attribute_label"],"single_select",${"jc_".$row["attribute_code"]},"jc_edit_".$row["attribute_code"],$row["attribute_id"],$allow_in_edit,$id);
           }elseif($row["attribute_type"]=="multi_select"){
-          $element .= return_m_select_box($row["attribute_label"],"multi_select",${"jc_".$row["attribute_code"]},"jc_edit_".$row["attribute_code"],$row["attribute_id"],$allow_in_edit,$id);
+            $element .= return_input_box($row["attribute_label"],"text",${"jc_".$row["attribute_code"]},"jc_edit_".$row["attribute_code"],$allow_in_edit,$id);
+        //   $element .= return_m_select_box($row["attribute_label"],"multi_select",${"jc_".$row["attribute_code"]},"jc_edit_".$row["attribute_code"],$row["attribute_id"],$allow_in_edit,$id);
           }
     }
     return $element;
@@ -177,7 +179,7 @@ function get_attribute_section($attribute_set){
 }
 
 //get attribute set manu
-$query = "SELECT distinct attribute_set FROM u749625779_cdscontent.job_attribute where allow_display=1" or die("Error:" . mysqli_error($con));
+$query = "SELECT distinct attribute_set FROM u749625779_cdscontent.job_attribute where allow_display=1 order by sort_attribute_set" or die("Error:" . mysqli_error($con));
   $result = mysqli_query($con, $query);
   $d_attribute_set="";
   $d_attribute_section="";
