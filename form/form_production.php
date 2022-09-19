dc52ec2d7da0a4e80e63e0b6ce289b2b7c6649dc
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-19 18:04:44 +0700

    debug

--- form/form_production.php
+++ form/form_production.php
@@ -1,6 +1,20 @@
 <?php
 $id=$_POST['id'];
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+//jet data 24ep
+$query = "SELECT  * FROM u749625779_cdscontent.job_cms where csg_request_new_id = ".$id or die("Error:" . mysqli_error($con));
+    $result = mysqli_query($con, $query);
+    while($row = mysqli_fetch_array($result)) {
+        $query_column = "SELECT `COLUMN_NAME` 
+        FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+        WHERE `TABLE_SCHEMA`='u749625779_cdscontent' 
+            AND `TABLE_NAME`='job_cms';" or die("Error:" . mysqli_error($con));
+        $result = mysqli_query($con, $query_column);
+        while($row_column = mysqli_fetch_array($result_column)) {
+            ${$row_column['COLUMN_NAME']} = $row[$row_column['COLUMN_NAME']];
+        }
+     
+    }
 function return_input_box($att_name,$site_element,$current_value,$code_element,$enable_edit,$id){
     if($site_element=='datetime-local'){
       $current_value = str_replace(" ","T",$current_value);
