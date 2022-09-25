a7d987a8a757ece2a5012a19d749623116a16977
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-25 20:53:28 +0700

    debug

--- homepage_new.php
+++ homepage_new.php
@@ -148,16 +148,16 @@ if (!$_SESSION["login_csg"]){
         <div id="calloffcanvas_cr">
         </div>
     </div>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="notificationcanvas" aria-labelledby="offcanvasRightLabel"
+    <!-- <div class="offcanvas offcanvas-end" tabindex="-1" id="notificationcanvas" aria-labelledby="offcanvasRightLabel"
         style="position: absolute;">
         <div class="offcanvas-header">
             <h5 id="offcanvasRightLabel">Activity</h5>
             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            <?php include("get/get_log.php"); ?>
+            <?php// include("get/get_log.php"); ?>
         </div>
-    </div>
+    </div> -->
     <div class="offcanvas offcanvas-start" style="width:70%" tabindex="-1" id="content_request_canvas"
         aria-labelledby="offcanvasExampleLabel">
         <div class="offcanvas-header" style="    background: #313131;color: white;">
