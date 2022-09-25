e39239b0885e0a558a1bd4b1766cf2f43a1c0cce
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-25 20:08:25 +0700

    debug

--- homepage_new.php
+++ homepage_new.php
@@ -350,7 +350,7 @@ if (!$_SESSION["login_csg"]){
                         src="https://ui-avatars.com/api/?name=<?php echo $ac_firstname.'+'.$ac_lastname; ?>&background=<?php echo bg_dept($ac_department); ?>&color=fff&rounded=true&size=30">
                     <a class="navbar-brand ms-1" style="" href="#"><?php echo $ac_firstname." ".$ac_lastname; ?></a>
                     <small href="#"><?php echo $ac_department; ?> </small> | <?php echo $ac_role; ?>
-                    <ion-icon onclick="call_model_edit_account('<?php echo $ac_username; ?>')" name="open-outline">
+                    <ion-icon type="button"  class="btn btn-outline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"onclick="call_model_edit_account('<?php echo $ac_username; ?>')" name="open-outline">
                     </ion-icon>
                 </div>
             </nav>
