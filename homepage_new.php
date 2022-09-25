b03019caa72ebfcd460403b5dfbf5339f07dd50b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-25 13:56:49 +0700

    debug

--- homepage_new.php
+++ homepage_new.php
@@ -304,6 +304,29 @@ if (!$_SESSION["login_csg"]){
         <div class="col-2 shadow sub_manu hide p-3 bg-white bg-gradient" id="sub_manu">
         </div>
         <div class="col-10 col_detail_main" style="font-size: 14px;padding: 0px;">
+        <?php
+            session_start();
+
+            $database = 'all_in_one_project';
+            $table = 'account';
+            $primary_key_id = 'username';
+            $id=$_SESSION['username'];
+            $prefix_table = 'ac';
+            $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+
+                $query = "SELECT  * FROM ".$database.".".$table." where ".$primary_key_id." = '".$id."'" or die("Error:" . mysqli_error($con));
+                $result = mysqli_query($con, $query);
+                while($row = mysqli_fetch_array($result)) {
+                    $query_column = "SELECT `COLUMN_NAME` 
+                    FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+                    WHERE `TABLE_SCHEMA`='".$database."' 
+                        AND `TABLE_NAME`='".$table."'" or die("Error:" . mysqli_error($con));
+                    $result_column = mysqli_query($con, $query_column);
+                    while($row_column = mysqli_fetch_array($result_column)) {
+                        ${$prefix_table."_".$row_column['COLUMN_NAME']} = $row[$row_column['COLUMN_NAME']];
+                    }
+                }
+            ?>
             <div id="col_detail">
             </div>
         </div>
--- page/panel.php
+++ page/panel.php
@@ -1,26 +1,4 @@
-<?php
-session_start();
-
-   $database = 'all_in_one_project';
-   $table = 'account';
-   $primary_key_id = 'username';
-   $id=$_SESSION['username'];
-   $prefix_table = 'ac';
-   $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
-
-      $query = "SELECT  * FROM ".$database.".".$table." where ".$primary_key_id." = '".$id."'" or die("Error:" . mysqli_error($con));
-      $result = mysqli_query($con, $query);
-      while($row = mysqli_fetch_array($result)) {
-          $query_column = "SELECT `COLUMN_NAME` 
-          FROM `INFORMATION_SCHEMA`.`COLUMNS` 
-          WHERE `TABLE_SCHEMA`='".$database."' 
-              AND `TABLE_NAME`='".$table."'" or die("Error:" . mysqli_error($con));
-          $result_column = mysqli_query($con, $query_column);
-          while($row_column = mysqli_fetch_array($result_column)) {
-              ${$prefix_table."_".$row_column['COLUMN_NAME']} = $row[$row_column['COLUMN_NAME']];
-          }
-      }
-?>
+<!--  -->
 
 <nav class="p-3 bg-white shadow-sm border-bottom">
     <div class="container-fluid">
