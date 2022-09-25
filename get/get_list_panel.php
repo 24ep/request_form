a58ff9c69977517e8cf42c71c111dcee104f4e38
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-25 12:44:49 +0700

    debug

--- get/get_list_panel.php
+++ get/get_list_panel.php
@@ -11,14 +11,48 @@ function get_panel_card($database,$table,$primary_key_id,$id,$title,$prefix,$end
     while($row = mysqli_fetch_array($result)) {
         
          ?>
-            <div class="" style="width: 18rem;">
-                <ul class="list-group list-group-flush">
-                    <li class="" style="list-style: none"><strong><?php echo $prefix.$row[$title];?><strong></li>
-                    <li class="" style="list-style: none"><?php echo $row['brand']." ".$row['sku'];?></li>
-                    <li class=""style="list-style: none" ><span><?php echo $row[$status_key];?></span></li>
-                </ul>
-            </div>
-            <?php
+<div class="p-3 border-bottom">
+    <div class="row">
+        <div class="col">
+            <ul class="list-group list-group-flush">
+                <li class="" style="list-style: none"><strong><?php echo $prefix.$row[$title];?></strong></li>
+                <li class="" style="list-style: none"><?php echo $row['brand']." ".$row['sku'];?> SKUs</li>
+                <li class="" style="list-style: none"><span><?php echo $row[$status_key];?></span></li>
+            </ul>
+        </div>
+        <div class="col">
+            <?php 
+                            //priority_badge
+                                $current_day = date("Y-m-d");
+                                $p_badge="";
+                                $create_date = date_create($row["create_date"]);
+                                $create_date = date_format($create_date,"Y-m-d");
+                                // -1 create date > 5
+                                $create_date_diff = (strtotime($current_day) - strtotime($create_date))/  ( 60 * 60 * 24 );
+                                if($create_date_diff>=10){
+                                $p_badge .= '<span class="badge bg-danger bg-gradient p-2 mb-1" style="margin-left:5px;background-color: #46088f!important;"><ion-icon name="warning-outline"></ion-icon>Age > '.$create_date_diff.' Days</span>';
+                                }elseif($create_date_diff>=5){
+                                $p_badge .= '<span class="badge bg-danger bg-gradient p-2 mb-1" style="margin-left:5px"> <ion-icon name="warning-outline" style="margin"></ion-icon>Age > '.$create_date_diff.' Days</span>';
+                                }elseif($create_date_diff>=3){
+                                $p_badge .= '<span class="badge bg-warning bg-gradient p-2 mb-1" style="margin-left:5px">Age > '.$create_date_diff.' Days</span>';
+                                }
+                                //  launch date
+                                if($row["launch_date"] <> null){
+                                $launch_date_c = date_create($row["launch_date"]);
+                                $launch_date_c = date_format($launch_date_c,"Y-m-d");
+                                $launch_date_diff = (strtotime($launch_date_c)-strtotime($current_day))/  ( 60 * 60 * 24 );
+                                    if($launch_date_diff<=0){
+                                    $p_badge .= '<span class="badge bg-danger bg-gradient  p-2 mb-1" style="margin-left:5px;background-color: #46088f!important;"><ion-icon name="warning-outline"></ion-icon> Over Launch '.($launch_date_diff*(-1)).' days</span>';
+                                    }elseif($launch_date_diff<=5){
+                                        $p_badge .= '<span class="badge bg-danger bg-gradient  p-2 mb-1" style="margin-left:5px"><ion-icon name="warning-outline"></ion-icon>Launch in '.$launch_date_diff.' days</span>';
+                                    }
+                                }
+                                echo $p_badge;
+                        ?>
+        </div>
+    </div>
+</div>
+<?php
     }
 }
 if($ac_role=='follow'){
