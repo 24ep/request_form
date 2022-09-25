2baa37029fdfc46bfe35a69a8ebec369bfe850cf
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-25 12:32:54 +0700

    debug

--- get/get_list_panel.php
+++ get/get_list_panel.php
@@ -24,11 +24,11 @@ function get_panel_card($database,$table,$primary_key_id,$id,$title,$prefix,$end
 if($ac_role=='follow'){
     get_panel_card('all_in_one_project','add_new_job','follow_up_by',$ac_username ,'id','NS-','accepted_date');
 }elseif($ac_role=='writer'){
-    get_panel_card('u749625779_cdscontent','job_cms','content_assign_name',$ac_nicknam,'job_number','','content_complete_date');
+    get_panel_card('u749625779_cdscontent','job_cms','content_assign_name',$ac_nickname,'job_number','','content_complete_date');
 }elseif($ac_role=='shooter'){
-    get_panel_card('u749625779_cdscontent','job_cms','shoot_assign_name',$ac_nicknam,'job_number','','shoot_complete_date');
+    get_panel_card('u749625779_cdscontent','job_cms','shoot_assign_name',$ac_nickname,'job_number','','shoot_complete_date');
 }elseif($ac_role=='retoucher'){
-    get_panel_card('u749625779_cdscontent','job_cms','retouch_assign_name',$ac_nicknam,'job_number','','retouch_complete_date');
+    get_panel_card('u749625779_cdscontent','job_cms','retouch_assign_name',$ac_nickname,'job_number','','retouch_complete_date');
 }elseif($ac_role=='product_executive'){
     get_panel_card('all_in_one_project','add_new_job','follow_up_by',$ac_username ,'id','NS-','accepted_date');
 }
