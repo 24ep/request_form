7396b580d137928bfb775c21fa2bb06a1b762366
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-25 13:03:15 +0700

    dbeug

--- get/get_list_panel.php
+++ get/get_list_panel.php
@@ -5,7 +5,7 @@ $ac_username = $_POST['ac_username'];
 $ac_nickname = $_POST['ac_nickname'];
 function get_panel_card($database,$table,$primary_key_id,$id,$title,$prefix,$end_key,$status_key,$create_key){
     $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
-    $query = "SELECT * FROM ".$database.".".$table." where ".$primary_key_id." = '".$id."' and ".$end_key."
+    $query = "SELECT ".$title.",brand,sku,id,".$status_key.",".$create_key." FROM ".$database.".".$table." where ".$primary_key_id." = '".$id."' and ".$end_key."
     order by id ASC limit 10" or die("Error:" . mysqli_error($con));
     $result = mysqli_query($con, $query);
     while($row = mysqli_fetch_array($result)) {
@@ -57,15 +57,15 @@ function get_panel_card($database,$table,$primary_key_id,$id,$title,$prefix,$end
 }
 if($status=='inprogress'){
     if($ac_role=='follow'){
-        get_panel_card('all_in_one_project','add_new_job','follow_up_by',$ac_username ,'id','NS-','accepted_date is null','status','recive_mail_date');
+        get_panel_card('all_in_one_project','add_new_job','follow_up_by',$ac_username ,'id','NS-','accepted_date is null ','status','recive_mail_date');
     }elseif($ac_role=='writer'){
-        get_panel_card('u749625779_cdscontent','job_cms','content_assign_name',$ac_nickname,'job_number','','content_complete_date is null','job_status_filter','recive_mail_date');
+        get_panel_card('u749625779_cdscontent','job_cms','content_assign_name',$ac_nickname,'job_number','','content_complete_date is null ','job_status_filter','recive_mail_date');
     }elseif($ac_role=='shooter'){
-        get_panel_card('u749625779_cdscontent','job_cms','shoot_assign_name',$ac_nickname,'job_number','','shoot_complete_date is null','job_status_filter','recive_mail_date');
+        get_panel_card('u749625779_cdscontent','job_cms','shoot_assign_name',$ac_nickname,'job_number','','shoot_complete_date is null ','job_status_filter','recive_mail_date');
     }elseif($ac_role=='retoucher'){
-        get_panel_card('u749625779_cdscontent','job_cms','retouch_assign_name',$ac_nickname,'job_number','','retouch_complete_date is null','job_status_filter','recive_mail_date');
+        get_panel_card('u749625779_cdscontent','job_cms','retouch_assign_name',$ac_nickname,'job_number','','retouch_complete_date is null ','job_status_filter','recive_mail_date');
     }elseif($ac_role=='product_executive'){
-        get_panel_card('all_in_one_project','add_new_job','follow_up_by',$ac_username ,'id','NS-','accepted_date is null','status','recive_mail_date');
+        get_panel_card('all_in_one_project','add_new_job','follow_up_by',$ac_username ,'id','NS-','accepted_date is null' ,'status','recive_mail_date');
     }
 }elseif($status=='pending'){
 
