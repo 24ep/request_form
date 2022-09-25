16df1abe6b6a1ddc61675b694d141758982943af
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-09-25 21:42:48 +0700

    debug

--- page/dashboard.php
+++ page/dashboard.php
@@ -29,7 +29,7 @@ function get_list_status($username,$status){
 }
 ?>
 <div class="container overflow-auto" style="padding:20px 20px 0px 20px">
-<h5 style="font-weight: 700;margin-bottom: 20px;margin-left: 5px;"><ion-icon name="hand-left-outline"></ion-icon> Hi <?php echo $_SESSION['firstname']?> :)</h5>
+<!-- <h5 style="font-weight: 700;margin-bottom: 20px;margin-left: 5px;"><ion-icon name="hand-left-outline"></ion-icon> Hi <?php //echo $_SESSION['firstname']?> :)</h5> -->
 <h6 style="font-weight: 700;margin-bottom: 20px;margin-left: 5px;"><ion-icon name="storefront-outline"></ion-icon>Product creations</h6>
 <!-- <?php //include("../get/linesheet_download_alert_bra.php"); ?> -->
     <div class="card-group" style="position: absolute;z-index: 1;width: inherit;">
