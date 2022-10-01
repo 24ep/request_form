5c4849c53dbc8123a70570d21000205fa771f603
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 21:31:51 +0700

    debug

--- get/get_file_control.php
+++ get/get_file_control.php
@@ -2,7 +2,7 @@
 The classic file input element we'll enhance
 to a file pond, configured with attributes
 -->
-<span>Please select file type<span>
+<!-- <span>Please select file type<span>
 <select class="form-select" aria-label="file_type">
   <option value="original" selected>Original</option>
   <option value="cleaned">Cleaned</option>
@@ -12,7 +12,7 @@ to a file pond, configured with attributes
 <div class="form-floating">
   <textarea class="form-control" placeholder="Leave a remark here" id="floatingTextarea2" style="height: 100px"></textarea>
   <label for="floatingTextarea2">Remark</label>
-</div>
+</div> -->
 <input type="file" class="filepond"  name="filepond">
 <script>
 FilePond.parse(document.body);
@@ -26,8 +26,8 @@ FilePond.setOptions({
     maxFiles: 5,
     maxFiles: '2MB',
     server: {
-        url: 'http://content-service-gate.cdse-commercecontent.com',
-        process: 'base/action/action_upload_files.php',
+        url: 'http://content-service-gate.cdse-commercecontent.com/',
+        process: 'base/action/action_upload_files.php?id='+,
         // revert: './revert.php',
         // restore: './restore.php?id=',
         // fetch: './fetch.php?data=',
