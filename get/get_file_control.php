c4f8c2abcd9b065dd29b2f0db8e70b7306ec748b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 20:04:37 +0700

    debug

--- get/get_file_control.php
+++ get/get_file_control.php
@@ -2,26 +2,9 @@
 The classic file input element we'll enhance
 to a file pond, configured with attributes
 -->
-<!-- <input type="file" class="filepond" name="filepond" multiple data-allow-reorder="true" data-max-file-size="2MB"
-    data-max-files="5"> -->
+<input type="file" class="filepond" name="filepond" multiple data-allow-reorder="true" data-max-file-size="2MB"
+    data-max-files="5">
 
-    <!-- <script>
+    <script>
   FilePond.parse(document.body);
-//   FilePond.registerPlugin(
-//   FilePondPluginImagePreview,
-//   FilePondPluginImageExifOrientation,
-//   FilePondPluginFileValidateSize,
-//   FilePondPluginImageEdit
-// );
-
-
-  </script> -->
-  <input type="file" />
-
-<script>
-    // Get a reference to the file input element
-    const inputElement = document.querySelector('input[type="file"]');
-
-    // Create a FilePond instance
-    const pond = FilePond.create(inputElement);
-</script>
+  </script>
