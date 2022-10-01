3024216208f5fa2e4f121a67a32ac58c9cab15cb
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 17:29:18 +0700

    debug

--- get/get_search_result.php
+++ get/get_search_result.php
@@ -4,20 +4,21 @@ $input = $_POST['input'];
 date_default_timezone_set("Asia/Bangkok");
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
-$query = "SELECT anj.id , jc.job_number , jc.job_status_filter , anj.status
-from all_in_one_project.add_new_job anj 
-left join u749625779_cdscontent.job_cms jc 
-on jc.csg_request_new_id = anj.id 
-where anj.id='".$input."' or jc.job_number like '%".$input."%' 
-group by anj.id , jc.job_number , jc.job_status_filter , anj.status" or die("Error:" . mysqli_error($con));
+//---
+$query = "SELECT anj.id  from all_in_one_project.add_new_job anj where anj.id='".$input."'" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
+echo "<ul>";
 while($row = mysqli_fetch_array($result)) {
-    echo "<ul>";
     echo "<li>NS-ID : ".$row['id']."</li>";
-    echo "<li>job_number : ".$row['job_number']."</li>";
-    echo "</ul>";
-
 }
+//---
+$query = "SELECT jc.job_number from u749625779_cdscontent.job_cms jc where jc.job_number like '%".$input."%'" or die("Error:" . mysqli_error($con));
+$result = mysqli_query($con, $query);
+echo "<ul>";
+while($row = mysqli_fetch_array($result)) {
+    echo "<li>Job Number : ".$row['job_number']."</li>";
+}
+echo "</ul>";
 
 
 ?>
