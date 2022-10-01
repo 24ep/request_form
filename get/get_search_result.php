d6b1d0675358459b5841b81875bca9884daf9dd4
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 17:18:39 +0700

    debug

--- get/get_search_result.php
+++ get/get_search_result.php
@@ -4,13 +4,12 @@ $input = $_POST['input'];
 date_default_timezone_set("Asia/Bangkok");
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
-$query = "SELECT  sl.sku , anj.id , jc.job_number , jc.job_status_filter , anj.status
-FROM all_in_one_project.sku_list as sl
-left join all_in_one_project.add_new_job anj 
-on sl.csg_id = anj.id 
+$query = "SELECT anj.id , jc.job_number , jc.job_status_filter , anj.status
+from all_in_one_project.add_new_job anj 
 left join u749625779_cdscontent.job_cms jc 
 on jc.csg_request_new_id = anj.id 
-where anj.id='".$input."' or jc.job_number like '%".$input."%' or sl.sku like '%".$input."%'" or die("Error:" . mysqli_error($con));
+where anj.id='".$input."' or jc.job_number like '%".$input."%' 
+group by anj.id , jc.job_number , jc.job_status_filter , anj.status" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
     echo "<ul>";
