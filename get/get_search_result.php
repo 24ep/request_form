6541152fbaf676bf7cae6c1f6a473a5b6e132b3c
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 17:50:34 +0700

    debug

--- get/get_search_result.php
+++ get/get_search_result.php
@@ -9,19 +9,25 @@ $query = "SELECT anj.id  from all_in_one_project.add_new_job anj where anj.id='"
 $result = mysqli_query($con, $query);
 echo "<ul>";
 while($row = mysqli_fetch_array($result)) {
-    echo "<li>NS-ID : ".$row['id']."</li>";
+    echo "<li>NS-ID : ".$row['id']."  <ion-icon type='button'  class='btn btn-outline-dark border-0 btn-sm' data-bs-toggle='modal' data-bs-target='#exampleModal' onclick='call_edit_add_new_modal('".$row['id']."')' name='open-outline'>  </li>";
 }
 //---
 $query = "SELECT jc.job_number from u749625779_cdscontent.job_cms jc where jc.job_number like '%".$input."%'" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
-    echo "<li>Job Number : ".$row['job_number']."</li>";
+    echo "<li>Job Number : ".$row['job_number']." <ion-icon name='open-outline'></ion-icon> </li>";
 }
 //---
 $query = "SELECT cr.id from all_in_one_project.content_request cr where cr.id = '".$input."'" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
-    echo "<li>CR-ID : ".$row['id']."</li>";
+    echo "<li>CR-ID : ".$row['id']." <ion-icon name='open-outline'></ion-icon> </li>";
+}
+//---
+$query = "SELECT sl.sku from all_in_one_project.sku_list sl where sl.sku like '%".$input."%'" or die("Error:" . mysqli_error($con));
+$result = mysqli_query($con, $query);
+while($row = mysqli_fetch_array($result)) {
+    echo "<li>SKU : ".$row['sku']." <ion-icon name='open-outline'></ion-icon> </li>";
 }
 echo "</ul>";
 
