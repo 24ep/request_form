9562bc6479109b31a01a64f806cea626f707d6c8
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 21:38:59 +0700

    debug

---
+++ action/action_upload_files.php
@@ -0,0 +1,88 @@
+<?php
+ 
+  function is_image($path)
+  {
+      $a = getimagesize($path);
+      $image_type = $a[2];
+      if(in_array($image_type , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP)))
+      {
+          return true;
+      }
+      return false;
+  }
+session_start();
+include('action_send_line_api.php');
+include('action_add_participant.php');
+include('action_insert_log.php');
+// include("connect.php");
+$id = $_POST["id"];
+$ticket_type = "content_request";
+$comment = str_replace("'","''",$_POST["comment"]);
+$dt= new DateTime();
+$dt_formate = $dt->format('Y-m-d H:i:s');
+$nickname = $_SESSION["username"];
+$send_type= $_POST["send_type"];
+//check size file
+
+date_default_timezone_set("Asia/Bangkok");
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
+
+  //add comment
+  	$sql = "INSERT INTO comment (
+    ticket_id,
+      comment,
+      ticket_type,
+      comment_by
+    )
+      VALUES(
+        ".$id.",
+        '".$comment."',
+        '".$ticket_type."',
+        '".$nickname."'
+      )";
+      $query = mysqli_query($con,$sql);
+          //create forder
+          $last_id = $con->insert_id;
+          $fullpath = '../../attachment/csg/attachments/ns/'.$last_id."/";
+          if (!file_exists($fullpath)) {
+            mkdir($fullpath, 0777, true);
+          }
+         
+          // upload image
+          foreach($_FILES['files']['tmp_name'] as $key => $val){
+            $file_name = $_FILES['files']['name'][$key];
+            $file_size =$_FILES['files']['size'][$key];
+            $file_tmp =$_FILES['files']['tmp_name'][$key];
+            $file_type=$_FILES['files']['type'][$key];
+            $is_image = is_image($file_tmp);
+            if(isset($_FILES["files"])){
+                if ($file_name <> ""  ) { 
+                $sql = "INSERT INTO attachment (
+                    file_name,
+                    file_path,
+                    file_size,
+                    file_type,
+                    is_image,
+                    file_owner,
+                    ticket_id,
+                    ticket_type
+                    )
+                    VALUES(
+                    '".$file_name."',
+                    '".$fullpath."',
+                    '".$file_size."',
+                    '".$file_type."',
+                    '".$is_image."',
+                    '".$_SESSION["username"]."',
+                    '".$last_id."',
+                    'ticket_files'
+                    )";
+                $query = mysqli_query($con,$sql);
+                move_uploaded_file($file_tmp,$fullpath.$file_name);
+                    }
+                }
+            }
+      add_participant($id,"add_new_job");
+  mysqli_close($con);
+?>
