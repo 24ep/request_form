50f5d2e5d8e18aaaff3cdb9cf08b4837391bf02d
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 20:16:38 +0700

    debug

--- get/get_file_control.php
+++ get/get_file_control.php
@@ -2,7 +2,7 @@
 The classic file input element we'll enhance
 to a file pond, configured with attributes
 -->
-<input type="file" class="filepond" name="filepond" multiple data-allow-reorder="true" data-max-file-size="2MB"
+<input type="file" class="filepond" name="filepond" multiple data-allow-reorder="true" allowProcess="true" allowRemove="true" data-max-file-size="2MB"
     data-max-files="5">
 
     <script>
