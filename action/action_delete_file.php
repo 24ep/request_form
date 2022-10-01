91658f77a38109bafd934946f5d276bbe5f2141b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-02 01:27:43 +0700

    debug

--- action/action_delete_file.php
+++ action/action_delete_file.php
@@ -6,13 +6,14 @@
     $file_path = $_POST["path"];
 
     $file_name = $_POST["name"];
-    $file = 'public_html/'.$file_path.$file_name ;
+    $file = $file_path.$file_name ;
 //remove form FTP
 // ftp://admin_base_csg@156.67.217.3/public_html/attachment/csg/attachments/ns/15359
 $ftp_server = "156.67.217.3";
 $ftp_conn = ftp_connect($ftp_server) or die("Error : 1Could not connect to $ftp_server");
 $login = ftp_login($ftp_conn, 'admin_base_csg', '@aA417528639');
 // try to delete file
+
 if (ftp_delete($ftp_conn, $file))
   {
     $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
