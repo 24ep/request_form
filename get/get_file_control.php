77c32b22220fcdfcb6c4b468ae488ed867c13871
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 19:44:03 +0700

    debug

--- get/get_file_control.php
+++ get/get_file_control.php
@@ -5,10 +5,6 @@ to a file pond, configured with attributes
 <input type="file" class="filepond" name="filepond" multiple data-allow-reorder="true" data-max-file-size="3MB"
     data-max-files="3">
 
-<script>
-// Get a reference to the file input element
-const inputElement = document.querySelector('input[type="file"]');
-
-// Create a FilePond instance
-const pond = FilePond.create(inputElement);
-</script>
+    <script>
+  FilePond.parse(document.body);
+  </script>
