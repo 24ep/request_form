41749761a09611372ebe43cbf4583d093037e26a
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-01 20:35:31 +0700

    debug

--- get/get_file_control.php
+++ get/get_file_control.php
@@ -2,8 +2,24 @@
 The classic file input element we'll enhance
 to a file pond, configured with attributes
 -->
-<input type="file" class="filepond" name="filepond" multiple data-allow-reorder="true" data-allow-process="true" data-max-file-size="2MB"
-    data-max-files="5">
+<input type="file" class="filepond" name="filepond"  >
     <script>
   FilePond.parse(document.body);
+  FilePond.setOptions({
+    allowDrop: false,
+    allowReplace: false,
+    instantUpload: false,
+    allowReoder:true,
+    allowMultiple:true,
+    allowRemove:true,
+    maxFiles:5,
+    maxFiles:'2MB',
+    server: {
+        url: 'http://156.67.217.3',
+        process: './action_upload_files.php',
+        // revert: './revert.php',
+        // restore: './restore.php?id=',
+        // fetch: './fetch.php?data=',
+    },
+});
   </script>
