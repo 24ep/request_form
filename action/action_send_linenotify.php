47099d80332c754ca23bae324f4ed433f5cafe55
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-09 00:09:39 +0700

    debug

--- action/action_send_linenotify.php
+++ action/action_send_linenotify.php
@@ -96,7 +96,7 @@ function sendline($id,$value_name,$value_change,$prefix){
          }
       }
 
-      
+      echo $list_user_push;
       $list_user_push = substr($list_user_push,1);
       webpush($list_user_push,$text_update,$id,$callback_url);
       
