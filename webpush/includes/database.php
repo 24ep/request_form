8d052f27f1182b82c2210e429ffd1f7eb73c37a0
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-08 20:13:48 +0700

    debug

--- webpush/includes/database.php
+++ webpush/includes/database.php
@@ -1,6 +1,7 @@
 <?php
 
-$con = mysqli_connect('localhost', 'root', '', 'test_temp');
+// $con = mysqli_connect('localhost', 'root', '', 'test_temp');
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
 $con->set_charset('utf8mb4');
 
 if(mysqli_connect_errno()){
