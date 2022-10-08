1824a146b72a47d25201822aa1196ed2d228d61b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-09 00:05:39 +0700

    debug

--- action/action_send_linenotify.php
+++ action/action_send_linenotify.php
@@ -57,7 +57,7 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 
 curl_close($curl);
-// echo $response;
+echo $response;
 
 }
 function sendline($id,$value_name,$value_change,$prefix){
