0a746f82881d5b2cac97b78686214cf69bed18ca
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-08 17:14:04 +0700

    debug

---
+++ action/action_data_snapshot.php
@@ -0,0 +1,39 @@
+<?php
+$database = $_POST['database'];
+$table = $_POST['table'];
+$primary_key_id = $_POST['primary_key_id'];
+$id = $_POST['id'];
+$ticket_type = $_POST['ticket_type'];
+$nickname = "System";
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+
+$query = "SELECT  * FROM ".$database.".".$table." where ".$primary_key_id." = '".$id."'" or die("Error:" . mysqli_error($con));
+$result = mysqli_query($con, $query);
+while($row = mysqli_fetch_array($result)) {
+    $query_column = "SELECT `COLUMN_NAME` 
+    FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+    WHERE `TABLE_SCHEMA`='".$database."' 
+        AND `TABLE_NAME`='".$table."'" or die("Error:" . mysqli_error($con));
+    $result_column = mysqli_query($con, $query_column);
+    while($row_column = mysqli_fetch_array($result_column)) {
+        $comment .= "{snapshot}".$row_column['COLUMN_NAME']." = ".$row[$row_column['COLUMN_NAME']]."</br>";
+    }
+}
+
+//insert to comment
+$sql = "INSERT INTO comment (
+      ticket_id,
+      comment,
+      ticket_type,
+      comment_by
+    )
+      VALUES(
+        ".$id.",
+        '".$comment."',
+        '".$ticket_type."',
+        '".$nickname."'
+      )";
+      $query = mysqli_query($con,$sql);
+
+      mysqli_close($con);
+?>
