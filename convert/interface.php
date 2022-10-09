01b48c18f170cf67cd3432a78387584b136ee92c
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-09 17:08:39 +0700

    debug

--- convert/interface.php
+++ convert/interface.php
@@ -5,9 +5,9 @@ $job_number = $_POST["job_number"];
 $launch_date = $_POST["launch_date"];
 $content_assign_name = $_POST["content_assign_name "];
 function get_value($crid,$col_re,$db){
-    $con_cr= mysqli_connect("localhost","cdse_admin","@aA417528639","u749625779_cdscontent") or die("Error: " . mysqli_error($con_cr));
+    $con_cr= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con_cr));
     mysqli_query($con_cr, "SET NAMES 'utf8' ");
-    $query_cr = "SELECT * FROM ".$db." ORDER BY id DESC" or die("Error:" . mysqli_error($con_cr));
+    $query_cr = "SELECT * FROM u749625779_cdscontent.".$db." ORDER BY id DESC" or die("Error:" . mysqli_error($con_cr));
     $result_cr = mysqli_query($con_cr, $query_cr);
     while($row_cr = mysqli_fetch_array($result_cr)) {
             if($crid==$row_cr["id"]){
@@ -18,9 +18,9 @@ function get_value($crid,$col_re,$db){
     return $current_cr;
 }
 // get fill setting convert from pim db
-  $con_pim= mysqli_connect("localhost","cdse_admin","@aA417528639","u749625779_cdscontent") or die("Error: " . mysqli_error($con_pim));
+  $con_pim= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con_pim));
   mysqli_query($con_pim, "SET NAMES 'utf8' ");
-  $query_pim = "SELECT * FROM pim_attribute_convert_im_form " or die("Error:" . mysqli_error( $con_pim));
+  $query_pim = "SELECT * FROM u749625779_cdscontent.pim_attribute_convert_im_form " or die("Error:" . mysqli_error( $con_pim));
   $result_pim = mysqli_query($con_pim, $query_pim);
   while($row_pim= mysqli_fetch_array($result_pim)) {
     if($row_pim["linesheet_type"]=="convert_setting" and $row_pim["is_create_first"]=="Yes"){
@@ -71,100 +71,106 @@ function get_value($crid,$col_re,$db){
   //-end getting------------------------------------------------------
     ?>
 
-    <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Convert IM Form to <strong style="color:#663399">Akeneo</strong> Template <strong><?php echo $job_number; ?></strong> ID <strong><?php echo $id; ?></strong> | Writer by <strong> <?php echo $content_assign_name; ?> </strong> </h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
+
+<h6 class="modal-title" id="staticBackdropLabel">Convert <strong style="color:#dc3545"> IM Form </strong> to <strong style="color:#663399">PIM</strong>
+    Template <strong><?php echo $job_number; ?></strong> ID <strong><?php echo $id; ?></strong> | Writer by <strong>
+<?php echo $content_assign_name; ?> </strong> 
+</h6>
+
     <input type="hidden" id="id" name="id" value="<?php echo $id; ?>">
     <input type="hidden" id="job_number" name="job_number" value="<?php echo $job_number; ?>">
-        <div class="row">
-          <div class="col-2">
-            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Attribute setting</a>
-              <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Function</a>
-            </div>
-          </div>
-          <div class="col-10">
-            <div class="tab-content" id="v-pills-tabContent">
-              <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"> 
-                <div class="row">
-                  <div class="col-5">
-                  <?php echo $get_setting_pim_convert; ?>
-                  </div>
-                  <div class="col-7">
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">skutype</label>
-                        <input style="width:250px" type="text"  class="form-control  form-control-sm mx-sm-3" aria-describedby="typeHelpInline" id="skutype" name="skutype" value="GR" oninput="changed_parent()">
-                      </div>
-                    </div>
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">BU Create</label>
-                        <input style="width:250px" type="text"  class="form-control  form-control-sm mx-sm-3" aria-describedby="typeHelpInline" id="bu_create" name="bu_create" value="CDS"  oninput="changed_parent()">
-                      </div>
-                    </div>
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">User ID</label>
-                        <input style="width:250px" type="text"  class="form-control  form-control-sm mx-sm-3" aria-describedby="typeHelpInline" id="ug_id" name="ug_id" value="<?php echo sprintf("%02d", $_SESSION["group_id"]); ?>"  oninput="changed_parent()">
-                      </div>
-                    </div>
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">Year</label>
-                        <input style="width:250px" type="text"  class="form-control  form-control-sm mx-sm-3" aria-describedby="typeHelpInline" id="g_year" name="g_year" value="<?php echo date("y"); ?>"  oninput="changed_parent()">
-                      </div>
-                    </div>
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">Month</label>
-                        <input style="width:250px" type="text"  class="form-control  form-control-sm mx-sm-3" aria-describedby="typeHelpInline" id="g_month" name="g_month" value="<?php echo date("m"); ?>"  oninput="changed_parent()">
-                      </div>
-                    </div>
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">Running (4 DG)</label>
-                        <input style="width:250px" type="text"  class="form-control  form-control-sm mx-sm-3" aria-describedby="typeHelpInline" id="g_running" name="g_running" value="" placeholder="XXXX"  oninput="changed_parent()">
-                      </div>
-                    </div>
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">Parent : </label>
-                        <input  style="width:250px;border: 0px;font-weight: bold;"  readonly type="text" class="form-control  form-control-sm mx-sm-3" id="parent" name="parent" value="" >
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="form-inline" style="margin-bottom: 5px;">
-                      <div class="form-group">
-                        <label style="width:100px;justify-content:left;">Sheet Name</label>
-                        <input style="width:250px" type="text"  class="form-control  form-control-sm mx-sm-3" aria-describedby="typeHelpInline" id="sheet_name" name="sheet_name" value="IM FORM" placeholder="work sheet name in excel"  >
-                      </div>
-                    </div>
-                    <hr>
-                    <small>Have a good day, :> </small>
-                  </div>
+    <div class="row">
+        <div class="col-5">
+            <?php echo $get_setting_pim_convert; ?>
+        </div>
+        <div class="col-7">
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">skutype</label>
+                    <input style="width:250px" type="text" class="form-control  form-control-sm mx-sm-3"
+                        aria-describedby="typeHelpInline" id="skutype" name="skutype" value="GR"
+                        oninput="changed_parent()">
                 </div>
-               </div>
-              <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-              <?php echo $get_setting_pim_convert_function; ?>
-              </div>
             </div>
-          </div>
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">BU Create</label>
+                    <input style="width:250px" type="text" class="form-control  form-control-sm mx-sm-3"
+                        aria-describedby="typeHelpInline" id="bu_create" name="bu_create" value="CDS"
+                        oninput="changed_parent()">
+                </div>
+            </div>
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">User ID</label>
+                    <input style="width:250px" type="text" class="form-control  form-control-sm mx-sm-3"
+                        aria-describedby="typeHelpInline" id="ug_id" name="ug_id"
+                        value="<?php echo sprintf("%02d", $_SESSION["group_id"]); ?>" oninput="changed_parent()">
+                </div>
+            </div>
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">Year</label>
+                    <input style="width:250px" type="text" class="form-control  form-control-sm mx-sm-3"
+                        aria-describedby="typeHelpInline" id="g_year" name="g_year" value="<?php echo date("y"); ?>"
+                        oninput="changed_parent()">
+                </div>
+            </div>
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">Month</label>
+                    <input style="width:250px" type="text" class="form-control  form-control-sm mx-sm-3"
+                        aria-describedby="typeHelpInline" id="g_month" name="g_month" value="<?php echo date("m"); ?>"
+                        oninput="changed_parent()">
+                </div>
+            </div>
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">Running (4 DG)</label>
+                    <input style="width:250px" type="text" class="form-control  form-control-sm mx-sm-3"
+                        aria-describedby="typeHelpInline" id="g_running" name="g_running" value="" placeholder="XXXX"
+                        oninput="changed_parent()">
+                </div>
+            </div>
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">Parent : </label>
+                    <input style="width:250px;border: 0px;font-weight: bold;" readonly type="text"
+                        class="form-control  form-control-sm mx-sm-3" id="parent" name="parent" value="">
+                </div>
+            </div>
+            <hr>
+            <div class="form-inline" style="margin-bottom: 5px;">
+                <div class="form-group">
+                    <label style="width:100px;justify-content:left;">Sheet Name</label>
+                    <input style="width:250px" type="text" class="form-control  form-control-sm mx-sm-3"
+                        aria-describedby="typeHelpInline" id="sheet_name" name="sheet_name" value="IM FORM"
+                        placeholder="work sheet name in excel">
+                </div>
+            </div>
+            <hr>
+            <?php echo $get_setting_pim_convert_function; ?>
+            <hr>
+            <small>Have a good day, :> </small>
+
         </div>
-        </div>
-          <div class="modal-footer">
-          <div class="custom-file">
-  			    <input type="file" class="custom-file-input" id="linesheet_akeneo_file" name="linesheet_akeneo_file" aria-describedby="linesheet_akeneo_file" style="opacity: 1;width:80%">
-  			  </div>
-            	<button type="submit" class="btn btn btn-success"  style="position: absolute;background:#FF0000;border:none">Convert to Template</button>
-          </div>
- <?php mysqli_close($con_pim); ?>
+
+
+
+  
+</div>
+
+    <div class="custom-file">
+        <input type="file" class="custom-file-input" id="linesheet_akeneo_file" name="linesheet_akeneo_file"
+            aria-describedby="linesheet_akeneo_file" style="opacity: 1;width:80%">
+    </div>
+    <button type="submit" class="btn btn btn-success" style="position: absolute;background:#FF0000;border:none">Convert
+        to Template</button>
+
+<?php mysqli_close($con_pim); ?>
 <script>
 changed_parent();
+
 function changed_parent() {
     var skutype = document.getElementById("skutype").value;
     var bu_create = document.getElementById("bu_create").value;
