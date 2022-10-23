8507fd0a1e1115f1d95d9b429694ce8a5ce9611e
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-23 22:47:35 +0700

    debug

--- verify_account/verify_by_email.php
+++ verify_account/verify_by_email.php
@@ -1,22 +1,18 @@
 <?php 
     session_start();
-$username = $_GET['username'];
 $verify_code = $_GET['verify_code'];
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
 $con_update= mysqli_connect("localhost","cdse_admin","@aA417528639","all_in_one_project") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
-$sql  = "SELECT username,verify_code from account WHERE username='".$username."'";
+$sql  = "SELECT username,verify_code from account WHERE verified_code='".$verify_code."'";
 $result = mysqli_query($con,$sql);
 while($row = mysqli_fetch_array($result)) {
-    if($row['verify_code']==$verify_code){
-        $sql_update  = "UPDATE account SET verify_code = 'verified' WHERE username='".$username."'";
+ 
+        $sql_update  = "UPDATE account SET verify_code = 'verified' WHERE verified_code='".$verify_code."'";
         $query_update = mysqli_query($con_update,$sql_update);
         $_SESSION['verify_code'] = 'verified';
         Header("Location: https://content-service-gate.cdse-commercecontent.com/");
-    }else{
-        echo  $con->error;
-        echo 'verify uncomplete';
-    }
+   
 }
 // echo  $con->error;
 // echo $verify_code;
