3be3b4ac5a953a93feab6f93c9b5b65cd074ef0c
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-23 11:09:09 +0700

    debug

--- verify_account/send_verify.php
+++ verify_account/send_verify.php
@@ -31,16 +31,25 @@ $email = $_GET['email'];
     if($query){
         $member_id = $con->insert_id;
           // 3. ส่ง verify code ไปทางอีเมล์ 
-        $to = "User <".$email.">" ; 
-        $subject = "Verify By Email" ; 
-        $headers = "MIME-Version: 1.0\r\n" ; 
-        $headers .= "Content-Type: text/html; charset=UTF-8 \r\n" ; 
-        $headers .= "From: CSG <noreply@cdse-commercecontent.com>\r\n" ; 
-        $body = "<a href=https://content-service-gate.cdse-commercecontent.com/base/verify_account/verify_by_email.php?
-                        username=".$username."&verify_code=".$member_verify_code.">
-                        คลิกเพื่อยันยัน</a>" ; 
+        // $to = $email ; 
+        // $subject = "Verify By Email" ; 
+        // $headers = "MIME-Version: 1.0\r\n" ; 
+        // $headers .= "Content-Type: text/html; charset=UTF-8 \r\n" ; 
+        // $headers .= "From: CSG <noreply@cdse-commercecontent.com>\r\n" ; 
+        // $body = "<a href=https://content-service-gate.cdse-commercecontent.com/base/verify_account/verify_by_email.php?
+        //                 username=".$username."&verify_code=".$member_verify_code.">
+        //                 คลิกเพื่อยันยัน</a>" ; 
         
-        @mail($to, $subject, $body,$headers) ; 
+        // mail($to, $subject, $body,$headers) ; 
+
+        $to      = $email;
+        $subject = 'the subject';
+        $message = 'hello';
+        $headers = 'From: webmaster@example.com'       . "\r\n" .
+                     'Reply-To: webmaster@example.com' . "\r\n" .
+                     'X-Mailer: PHP/' . phpversion();
+    
+        mail($to, $subject, $message, $headers);
     }
     
   
