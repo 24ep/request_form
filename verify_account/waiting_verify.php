6aaa61f1ccf103387cf29c029145db2f1f56518b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-23 18:19:00 +0700

    debug

--- verify_account/waiting_verify.php
+++ verify_account/waiting_verify.php
@@ -680,8 +680,8 @@ session_start();
                                                                 <tr>
                                                                     <td align="left"
                                                                         class="button-bg button-bg-secondary">
-                                                                        <button type="button" onclick="change_email_address();" class="button button-secondary">Change your
-                                                                            email address</button>
+                                                                        <a type="button" onclick="change_email_address();" class="button button-secondary">Change your
+                                                                            email address</a>
 
                                                                     </td>
                                                                 </tr>
