e7e9b741cb5be9e09a23cca08f20138888c1ccd6
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-24 14:06:18 +0700

    debug

--- form/form_ns_reject.php
+++ form/form_ns_reject.php
@@ -11,6 +11,7 @@ include('../get/get_attribute.php');
             echo return_textarea_box('approved_decline_remark','Reject Note',"textarea",'','rj_edit_approved_decline_remark','',$id,'jc','u749625779_cdscontent','job_cms','csg_request_new_id');
             echo return_s_select_box('approved_editing_status','Reject status',"single_select",'','rj_edit_approved_editing_status','',$id,'jc','u749625779_cdscontent','job_cms','csg_request_new_id');
             echo return_input_box('approved_decline_date','Reject date',"datetime-local",'','rj_approved_decline_date','',$id,'jc','u749625779_cdscontent','job_cms','csg_request_new_id');
+            echo return_input_box('approved_edited_date','Revised date',"datetime-local",'','rj_approved_edited_date','',$id,'jc','u749625779_cdscontent','job_cms','csg_request_new_id');
         ?>
     </ul>
 </div>
