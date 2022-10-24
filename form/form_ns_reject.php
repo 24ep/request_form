1231634f4f9317de71e8fa4aefba97c5637ba111
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-10-24 13:11:42 +0700

    debug

--- form/form_ns_reject.php
+++ form/form_ns_reject.php
@@ -1,4 +1,4 @@
-<?php
+-<?php
 $id = $_POST['id'];
 include('../get/get_attribute');
 ?>
@@ -10,13 +10,13 @@ include('../get/get_attribute');
 <?php
 echo return_s_select_box('approved_editing_status','reject status',"single_select",'rj_approved_editing_status','rj_edit_approved_editing_status','',$id,'jc','u749625779_cdscontent','job_cms','csg_request_new_id');
 ?>
-<input type="hidden" id="rj_edit_approved_edited_date" class="form-control" aria-describedby="approved_edited_date">
+<input type="hidden" id="rj_edit_approved_edited_date" class="form-control" value="CURRENT_TIMESTAMP"aria-describedby="approved_edited_date">
 
 <button type="button" onclick="ns_reject(<?php echo $id;?>)" class="btn btn-danger">Reject</button>
 
 <script>
 function ns_reject(id){
-    document.getElementById('approved_edited_date').value = Date.now();
+    // document.getElementById('approved_edited_date').value = Date.now();
     approved_decline_remark = document.getElementById('rj_edit_approved_decline_remark').value
     approved_editing_status = document.getElementById('rj_edit_approved_editing_status').value
     approved_edited_date = document.getElementById('rj_edit_approved_edited_date').value
