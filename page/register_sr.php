00e9bcf427db60569d3de562a7606dbdaac8291e
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-12-16 14:06:02 +0700

    debug

--- page/register_sr.php
+++ page/register_sr.php
@@ -1,4 +1,11 @@
 <?php
-echo shell_exec('wmic bios get serialnumber 2>&1');
+
+// Execute the wmic command to get the serial number of the client's BIOS
+$output = system('wmic BIOS get SerialNumber');
+
+// The serial number will be returned as a string in the output variable
+// You can then process the string as needed, such as by storing it in a database or displaying it on a web page
+
+echo "The serial number of the client's BIOS is: $output";
 
 ?>
