8458c62df9aaec0591cc1bc999ca37dd0c674321
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-12-16 14:07:36 +0700

    debug

--- page/register_sr.php
+++ page/register_sr.php
@@ -1,11 +1,11 @@
 <?php
 
-// Execute the wmic command to get the serial number of the client's BIOS
-$output = system('wmic BIOS get SerialNumber');
+// Execute the hdparm command to get the serial number of the client's hard drive
+$output = shell_exec('hdparm -i /dev/sda | grep SerialNo');
 
 // The serial number will be returned as a string in the output variable
 // You can then process the string as needed, such as by storing it in a database or displaying it on a web page
 
-echo "The serial number of the client's BIOS is: $output";
+echo "The serial number of the client's hard drive is: $output";
 
 ?>
