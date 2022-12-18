65c5d8d5f6ede739d726c7dd681aea624edba222
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-12-18 11:59:26 +0700

    debug

---
+++ page/data_table.php
@@ -0,0 +1,113 @@
+<?php
+$assigned_log_id = $_POST["id"];
+session_start();
+date_default_timezone_set("Asia/Bangkok");
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
+
+//get column 
+
+    $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+    mysqli_query($con, "SET NAMES 'utf8' ");
+    $table = 'job_attribute';
+    $query = "SELECT `COLUMN_NAME` 
+    FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+    WHERE `TABLE_SCHEMA`='u749625779_cdscontent' 
+        AND `TABLE_NAME`='".$table."';" or die("Error:" . mysqli_error());
+    $result =  mysqli_query($con, $query);
+    while($row = mysqli_fetch_array($result)) {
+        $th .= '<th scope="col">'.$row['COLUMN_NAME'].'</th>';
+    }
+
+
+//get table 1 
+
+//get table
+$query = "SELECT *
+FROM u749625779_cdscontent.".$table.";" or die("Error:" . mysqli_error());
+$result =  mysqli_query($con, $query);
+echo '
+                        <div style="margin-bottom: 20px;">
+
+                            <input type="text" id="filter0" placeholder="Filter By Name">
+                            <input type="text" id="filter1" placeholder="Filter By Email">
+                            <input type="text" id="filter2" placeholder="Filter By Status">
+
+                        </div>
+';
+echo '<table class="table table-bordered" id="filter name="st_attribute_config">
+<thead>
+    <tr>
+      '.$th.'
+    </tr>
+</thead>
+<tbody>';
+while($row = mysqli_fetch_array($result)) {
+   echo '<tr>';
+          
+
+   $query_col = "SELECT `COLUMN_NAME` 
+   FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+   WHERE `TABLE_SCHEMA`='u749625779_cdscontent' 
+       AND `TABLE_NAME`='".$table."';" or die("Error:" . mysqli_error());
+   $result_col =  mysqli_query($con, $query_col);
+   while($row_col = mysqli_fetch_array($result_col)) {
+    
+       echo '
+       <td>
+        <input class="form-control" 
+                type="text" 
+                id="'.$row_col['COLUMN_NAME'].'_'.$row['id'].'" 
+                name="'.$row_col['COLUMN_NAME'].'_'.$row['id'].'" 
+                value="'.$row[$row_col['COLUMN_NAME']].'">
+        </td>';
+    
+   }
+
+
+
+    echo'</tr>';
+}
+ echo ' </tbody>
+ </table>
+ 
+ ';
+
+mysqli_close($con);
+
+
+
+?>
+ <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script> -->
+    <!--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+<script> -->
+
+
+
+<!-- <script>
+  $(document).ready( function () {
+  $('#st_attribute_config').DataTable({
+        lengthMenu: [
+            [10,20, 50, 100, -1],
+            [10, 20,50, 100, 'All'],
+        ],
+    });
+
+} );
+
+
+
+</script> -->
+
+<script src="TableFilter.min.js" defer></script>
--- page/register_sr.php
+++ page/register_sr.php
@@ -1,55 +1,37 @@
-<!-- attribute_edit.php -->
+<?php
+session_start();
+date_default_timezone_set("Asia/Bangkok");
+$con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
 
-<!doctype html>
-<html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+$query = "SELECT attribute_code
+FROM u749625779_cdscontent.job_attribute;" or die("Error:" . mysqli_error());
+$result =  mysqli_query($con, $query);
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+while($row = mysqli_fetch_array($result)) {
+echo '
+    <tr>
+      <th scope="col">'.$row['attribute_code'].'</th>
+      <th scope="col">'.$row['attribute_label'].'</th>
+      <th scope="col">'.$row['description'].'</th>
+      <th scope="col">'.$row['Edit'].'</th>
+    </tr>
+';
+  
+}
 
-  <title>Attribute Edit</title>
-</head>
-<body>
-  <div class="container mt-5">
-    <h1>Edit Attribute</h1>
-    <!-- HTML form to edit the attribute -->
-    <form id="edit-form" action="attribute_update.php" method="post">
-      <div class="form-group">
-        <label for="attribute">Attribute:</label>
-        <input type="text" class="form-control" id="attribute" name="attribute">
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form> 
-  </div>
 
-  <!-- JavaScript to handle form submission and display a message when the attribute is updated -->
-  <script>
-    // Get the form element
-    const form = document.getElementById('edit-form');
+echo '<table class="table table-bordered" id="filter name="st_attribute_config">
+<thead>
+    <tr>
+      <th scope="col">Attribute Code</th>
+      <th scope="col">Attribute Label</th>
+      <th scope="col">Description</th>
+      <th scope="col">Edit Label</th>
+    </tr>
+</thead>
+<tbody>';
 
-    // Add an event listener to the form to handle submission
-    form.addEventListener('submit', (event) => {
-      // Prevent the default form submission behavior
-      event.preventDefault();
-
-      // Send an HTTP POST request to the server to update the attribute
-      fetch(form.action, {
-        method: 'POST',
-        body: new FormData(form),
-      })
-        .then((response) => {
-          // If the attribute was successfully updated, display a message
-          if (response.ok) {
-            alert('Attribute updated successfully');
-          }
-        });
-    });
-  </script>
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.16.6/dist/umd/popper.min.js" integrity="sha384-wHAi
+echo '</tbody>
+</table>';
+?>
