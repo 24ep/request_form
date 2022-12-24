b08ef54e6bf9ddc16eb8ea18bf364f8c9aa989b5
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-12-25 02:12:36 +0700

    debug

--- action/action_delete_record.php
+++ action/action_delete_record.php
@@ -6,7 +6,7 @@
     date_default_timezone_set("Asia/Bangkok");
     $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
     mysqli_query($con, "SET NAMES 'utf8' ");
-    $sql = "DELETE FROM ".$db.".".$table." where ".$primary_key_id." = ".$id."";
+    $sql = "DELETE FROM ".$db.".".$table." where ".$primary_key_id." = ".$id;
     $query = mysqli_query($con,$sql);
 
     // $last_id = $con->insert_id;
