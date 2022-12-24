a423835dba6f8ed058d1020d01918d45ae430a32
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-12-25 00:54:51 +0700

    debug

--- action/action_insert_new_record.php
+++ action/action_insert_new_record.php
@@ -4,7 +4,7 @@
     date_default_timezone_set("Asia/Bangkok");
     $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
     mysqli_query($con, "SET NAMES 'utf8' ");
-    $sql = "INSERT INTO ".$db.".".$table." VALUES (default)";
+    $sql = "INSERT INTO ".$db.".".$table." (id) VALUES (default)";
     $query = mysqli_query($con,$sql);
 
     $last_id = $con->insert_id;
