b473ad5fd6ce2bd9c77e959e0fc7a75ec6345248
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2022-12-31 13:37:03 +0700

    debug

--- get/get_board_update_content.php
+++ get/get_board_update_content.php
@@ -162,20 +162,20 @@ $result = mysqli_query($con, $query);
 $result_after_fetch =$result;
 echo'<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
 <li class="nav-item" role="presentation">
-  <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Buyer</button>
+  <button class="nav-link active" id="pills-waiting_buyer-tab" data-bs-toggle="pill" data-bs-target="#pills-waiting_buyer" type="button" role="tab" aria-controls="pills-waiting_buyer" aria-selected="true">Buyer</button>
 </li>
 <li class="nav-item" role="presentation">
-  <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Execution</button>
+  <button class="nav-link" id="pills-execution-tab" data-bs-toggle="pill" data-bs-target="#pills-execution" type="button" role="tab" aria-controls="pills-execution" aria-selected="false">Execution</button>
 </li>
 <li class="nav-item" role="presentation">
-  <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">CTO</button>
+  <button class="nav-link" id="pills-cto-tab" data-bs-toggle="pill" data-bs-target="#pills-cto" type="button" role="tab" aria-controls="pills-cto" aria-selected="false">CTO</button>
 </li>
 </ul>';
 
 echo'<div class="tab-content" id="pills-tabContent">
-<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">'.listing_ticket_card( $result_after_fetch,'Waiting Buyer').'</div>
-<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">'.listing_ticket_card( $result_after_fetch,'Waiting Execution').'</div>
-<div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">'.listing_ticket_card( $result_after_fetch,'Waiting CTO').'</div>
+<div class="tab-pane fade show active" id="pills-waiting_buyer" role="tabpanel" aria-labelledby="pills-waiting_buyer-tab" tabindex="0">'.listing_ticket_card( $result_after_fetch,'Waiting Buyer').'</div>
+<div class="tab-pane fade" id="pills-execution" role="tabpanel" aria-labelledby="pills-execution-tab" tabindex="0">'.listing_ticket_card( $result_after_fetch,'Waiting Execution').'</div>
+<div class="tab-pane fade" id="pills-cto" role="tabpanel" aria-labelledby="pills-cto-tab" tabindex="0">'.listing_ticket_card( $result_after_fetch,'Waiting CTO').'</div>
 </div>';
 
 echo '</div>';
