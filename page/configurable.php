96c3b5a62f87c2e20414b6629e56764eab494b61
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-04 23:16:59 +0700

    debug

--- page/configurable.php
+++ page/configurable.php
@@ -13,53 +13,53 @@ Table setting
 -->
 
 <?php
-$configurable_session_map = array("general_setting","table_setting");
+$configurable_session_map = array("General Settings","Table settings");
 //  session |icon | table | header | description
 $configurable_map = array (
     array(
-        'session'=>'general_setting',
+        'session'=>'General Settings',
         'icon'=>'people',
         'table'=>'account',
         'title'=>'ServiceGate accounts',
         'function'=>'get_page',
         'description'=>'Manage any register account in system'),
     array(
-        'session'=>'general_setting',
+        'session'=>'General Settings',
         'icon'=>'apps',
         'table'=>'app_version_control',
         'title'=>'LinesheetApp',
         'function'=>'get_page',
         'description'=>'Manage lineshetAPP'),
     array(
-        'session'=>'general_setting',
+        'session'=>'General Settings',
         'icon'=>'rocket',
         'table'=>'add_new_job',
         'title'=>'add_new_job',
         'function'=>'get_attribute_config',
         'description'=>'Manage attribute of new product creation (NS ticket)'),
     array(
-        'session'=>'general_setting',
+      'session'=>'General Settings',
         'icon'=>'grid',
         'table'=>'job_cms',
         'title'=>'job_cms',
         'function'=>'get_attribute_config',
         'description'=>'manage attribute of new product creation (production job)'),
     array(
-        'session'=>'general_setting',
+        'session'=>'General Settings',
         'icon'=>'ticket',
         'table'=>'content_request',
         'title'=>'content_request',
         'function'=>'get_attribute_config',
         'description'=>'Manage attribute of new product update (CR,DP ticket)'),
     array(
-        'session'=>'settings',
+        'session'=>'Table Settings',
         'icon'=>'settings',
         'table'=>'job_attribute',
         'title'=>'Attribute setting',
         'function'=>'get_attribute_config',
         'description'=>'Manage an attribute of config table'),
     array(
-        'session'=>'settings',
+        'session'=>'Table Settings',
         'icon'=>'settings',
         'table'=>'job_attribute_option',
         'title'=>'Attribute option setting',
@@ -91,69 +91,7 @@ for ($i=0; $i < $length_configurable_session_map; $i++) {
 
 ?>
 
-<!--
-<div class="row row-cols-1 row-cols-md-3 g-4 m-3">
 
-</div>
-
-<hr>
-<h4>Table setting</h4>
-<div class="row row-cols-1 row-cols-md-3 g-4 m-3">
-<div class="col" onclick="get_attribute_config('account')">
-<div class="card p-3">
-<ion-icon name="people" style="font-size: 50px;color: #ababab;"></ion-icon>
-<div class="card-body">
-<h5 class="card-title">Account table</h5>
-<p class="card-text">Manage account that register on the ServiceGate</p>
-</div>
-</div>
-</div>
-<div class="col" onclick="get_attribute_config('add_new_job')">
-<div class="card p-3">
-<ion-icon name="rocket" style="font-size: 50px;color: #ababab;"></ion-icon>
-<div class="card-body">
-<h5 class="card-title">Add new job table </h5>
-<p class="card-text">Manage the an attribute of ns-ticket</p>
-</div>
-</div>
-</div>
-<div class="col" onclick="get_attribute_config('content_request')">
-<div class="card p-3">
-<ion-icon name="ticket" style="font-size: 50px;color: #ababab;"></ion-icon>
-<div class="card-body">
-<h5 class="card-title">Content Request table </h5>
-<p class="card-text">CR-Ticket, DP-Ticket - Manage an attribute of content-request ticket</p>
-</div>
-</div>
-</div>
-<div class="col" onclick="get_attribute_config('job_cms')">
-<div class="card p-3">
-<ion-icon name="grid" style="font-size: 50px;color: #ababab;"></ion-icon>
-<div class="card-body">
-<h5 class="card-title">Job CMS table (24EP)</h5>
-<p class="card-text">Manage an attribute of writer job in 24EP system (old)</p>
-</div>
-</div>
-</div>
-<div class="col" onclick="get_attribute_config('job_attribute')">
-<div class="card p-3">
-<ion-icon name="settings" style="font-size: 50px;color: #ababab;"></ion-icon>
-<div class="card-body">
-<h5 class="card-title">Attribute config table</h5>
-<p class="card-text">Manage an attribute of config table</p>
-</div>
-</div>
-</div>
-<div class="col" onclick="get_attribute_config('job_attribute_option')">
-<div class="card p-3">
-<ion-icon name="settings" style="font-size: 50px;color: #ababab;"></ion-icon>
-<div class="card-body">
-<h5 class="card-title">Attribute option config table</h5>
-<p class="card-text">Manage an attribtue option of config table</p>
-</div>
-</div>
-</div>
-</div> -->
 
 <script>
 function get_attribute_config(table) {
