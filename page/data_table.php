2c49de2d0a9f0b86738798f12a73375deb8b9217
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-04 21:44:32 +0700

    debug

--- page/data_table.php
+++ page/data_table.php
@@ -1,45 +1,45 @@
 <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col p-5">
+  <div class="col mt-5 m-3">
     <div class="card p-3">
-    <ion-icon name="people" style="font-size: 100px;color: #ababab;"></ion-icon>
+    <ion-icon name="people" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
-        <h5 class="card-title">Account</h5>
+        <h5 class="card-title">Account table</h5>
         <p class="card-text">Manage account that register on the ServiceGate</p>
       </div>
     </div>
   </div>
-  <div class="col p-5">
+  <div class="col mt-5 m-3">
     <div class="card p-3">
-    <ion-icon name="rocket" style="font-size: 100px;color: #ababab;"></ion-icon>
+    <ion-icon name="rocket" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
-        <h5 class="card-title">Add new job (NS-Ticket)</h5>
+        <h5 class="card-title">Add new job table (NS-Ticket) </h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
-  <div class="col p-5">
+  <div class="col mt-5 m-3">
     <div class="card p-3">
-    <ion-icon name="ticket" style="font-size: 100px;color: #ababab;"></ion-icon>
+    <ion-icon name="ticket" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
-        <h5 class="card-title">Content Request (CR-Ticket, DP-Ticket)</h5>
+        <h5 class="card-title">Content Request table (CR-Ticket, DP-Ticket)</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
   </div>
-  <div class="col p-5">
+  <div class="col mt-5 m-3">
     <div class="card p-3">
-    <ion-icon name="grid" style="font-size: 100px;color: #ababab;"></ion-icon>
+    <ion-icon name="grid" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
-        <h5 class="card-title">Job CMS (24EP)</h5>
+        <h5 class="card-title">Job CMS table (24EP)</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
-  <div class="col p-5">
+  <div class="col mt-5 m-3">
     <div class="card p-3">
-    <ion-icon name="grid" style="font-size: 100px;color: #ababab;"></ion-icon>
+    <ion-icon name="settings" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
-        <h5 class="card-title">Attribute config</h5>
+        <h5 class="card-title">Attribute config table</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
