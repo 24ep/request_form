408195ee2ce6a1da5b61da43234e71c6943746c4
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-04 21:36:35 +0700

    debug

--- page/data_table.php
+++ page/data_table.php
@@ -1,43 +1,43 @@
 <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card">
-    <ion-icon name="people-outline"></ion-icon>
+  <div class="col p-5">
+    <div class="card p-3">
+    <ion-icon name="people" style="font-size: 100px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Account</h5>
         <p class="card-text">Manage account that register on the ServiceGate</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-    <ion-icon name="rocket-outline"></ion-icon>
+  <div class="col p-5">
+    <div class="card p-3">
+    <ion-icon name="rocket" style="font-size: 100px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Add new job (NS-Ticket)</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-    <ion-icon name="ticket-outline"></ion-icon>
+  <div class="col p-5">
+    <div class="card p-3">
+    <ion-icon name="ticket" style="font-size: 100px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Content Request (CR-Ticket, DP-Ticket)</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-    <ion-icon name="grid-outline"></ion-icon>
+  <div class="col p-5">
+    <div class="card p-3">
+    <ion-icon name="grid" style="font-size: 100px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Job CMS (24EP)</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-    <ion-icon name="grid-outline"></ion-icon>
+  <div class="col p-5">
+    <div class="card p-3">
+    <ion-icon name="grid" style="font-size: 100px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Attribute config</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
