287bd080dedf90f398bc1eda5ea0a375aa35ebe9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-04 21:50:29 +0700

    debug

--- page/data_table.php
+++ page/data_table.php
@@ -1,5 +1,5 @@
-<div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col mt-5 m-3">
+<div class="row row-cols-1 row-cols-md-3 g-4 m-3">
+  <div class="col">
     <div class="card p-3">
     <ion-icon name="people" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
@@ -8,39 +8,39 @@
       </div>
     </div>
   </div>
-  <div class="col mt-5 m-3">
+  <div class="col">
     <div class="card p-3">
     <ion-icon name="rocket" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
-        <h5 class="card-title">Add new job table (NS-Ticket) </h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Add new job table </h5>
+        <p class="card-text">Manage the an attribute of ns-ticket</p>
       </div>
     </div>
   </div>
-  <div class="col mt-5 m-3">
+  <div class="col">
     <div class="card p-3">
     <ion-icon name="ticket" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
-        <h5 class="card-title">Content Request table (CR-Ticket, DP-Ticket)</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">Content Request table (</h5>
+        <p class="card-text">CR-Ticket, DP-Ticket - Manage an attribtue of content-request ticket</p>
       </div>
     </div>
   </div>
-  <div class="col mt-5 m-3">
+  <div class="col">
     <div class="card p-3">
     <ion-icon name="grid" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Job CMS table (24EP)</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">Manage an attribute of writer job in 24EP system (old)</p>
       </div>
     </div>
   </div>
-  <div class="col mt-5 m-3">
+  <div class="col">
     <div class="card p-3">
     <ion-icon name="settings" style="font-size: 80px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Attribute config table</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">Manage an attribtue of config table</p>
       </div>
     </div>
   </div>
