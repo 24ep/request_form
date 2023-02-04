130cb72efb604cfbf28b86095ac0b44b07b72fb9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-04 22:02:15 +0700

    debug

--- page/data_table.php
+++ page/data_table.php
@@ -1,7 +1,7 @@
 <div class="row row-cols-1 row-cols-md-3 g-4 m-3">
   <div class="col" onclick="get_attribute_config('account')">
     <div class="card p-3">
-    <ion-icon name="people" style="font-size: 80px;color: #ababab;"></ion-icon>
+    <ion-icon name="people" style="font-size: 50px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Account table</h5>
         <p class="card-text">Manage account that register on the ServiceGate</p>
@@ -10,7 +10,7 @@
   </div>
   <div class="col" onclick="get_attribute_config('add_new_job')">
     <div class="card p-3">
-    <ion-icon name="rocket" style="font-size: 80px;color: #ababab;"></ion-icon>
+    <ion-icon name="rocket" style="font-size: 50px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Add new job table </h5>
         <p class="card-text">Manage the an attribute of ns-ticket</p>
@@ -19,7 +19,7 @@
   </div>
   <div class="col" onclick="get_attribute_config('content_request')">
     <div class="card p-3">
-    <ion-icon name="ticket" style="font-size: 80px;color: #ababab;"></ion-icon>
+    <ion-icon name="ticket" style="font-size: 50px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Content Request table </h5>
         <p class="card-text">CR-Ticket, DP-Ticket - Manage an attribute of content-request ticket</p>
@@ -28,7 +28,7 @@
   </div>
   <div class="col" onclick="get_attribute_config('job_cms')">
     <div class="card p-3">
-    <ion-icon name="grid" style="font-size: 80px;color: #ababab;"></ion-icon>
+    <ion-icon name="grid" style="font-size: 50px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Job CMS table (24EP)</h5>
         <p class="card-text">Manage an attribute of writer job in 24EP system (old)</p>
@@ -37,7 +37,7 @@
   </div>
   <div class="col" onclick="get_attribute_config('job_attribute')">
     <div class="card p-3">
-    <ion-icon name="settings" style="font-size: 80px;color: #ababab;"></ion-icon>
+    <ion-icon name="settings" style="font-size: 50px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Attribute config table</h5>
         <p class="card-text">Manage an attribute of config table</p>
@@ -46,7 +46,7 @@
   </div>
   <div class="col" onclick="get_attribute_config('job_attribute_option')">
     <div class="card p-3">
-    <ion-icon name="settings" style="font-size: 80px;color: #ababab;"></ion-icon>
+    <ion-icon name="settings" style="font-size: 50px;color: #ababab;"></ion-icon>
       <div class="card-body">
         <h5 class="card-title">Attribute option config table</h5>
         <p class="card-text">Manage an attribtue option of config table</p>
