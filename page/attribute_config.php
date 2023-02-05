33cc514ab6c68c5e05b844a685aa15d0946903e6
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-05 18:45:29 +0700

    debug

--- page/attribute_config.php
+++ page/attribute_config.php
@@ -91,20 +91,23 @@ echo '</tbody>
             });
   }
 function add_new_attribute(db,table,target_tb,target_db,target_prefix) {
+console.log(target_tb);
+console.log(target_db);
+console.log(target_prefix);
   $.post("base/action/action_insert_new_record.php", {
         table : table,
         db : db
       },
       function(data) {
           attribute_detail_page(data,'',table,'create');
-
-      });
-        document.getElementById('ja_edit_table_name').value = target_tb;
+          document.getElementById('ja_edit_table_name').value = target_tb;
           document.getElementById('ja_edit_db_name').value = target_db;
           document.getElementById('ja_edit_prefix').value = target_prefix;
           update_value_attribute(data, 'ja_edit_table_name' , 'ja' , 'u749625779_cdscontent' , 'job_attribute' , 'id');
           update_value_attribute(data, 'ja_edit_db_name' , 'ja' , 'u749625779_cdscontent' , 'job_attribute' , 'id');
           update_value_attribute(data, 'ja_edit_prefix' , 'ja' , 'u749625779_cdscontent' , 'job_attribute' , 'id');
+      });
+
 }
 function alter_delete_attribute(db,table,column) {
 $.post("base/action/action_alter_delete_column.php", {
