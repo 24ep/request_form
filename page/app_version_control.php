7712efeb83c6d4240e804e938d74c557cf7ac628
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-05 19:44:10 +0700

    debug

--- page/app_version_control.php
+++ page/app_version_control.php
@@ -15,7 +15,7 @@ FilePond.setOptions({
     maxFileSize:'2MB',
     server: {
         url: 'https://content-service-gate.cdse-commercecontent.com/',
-        process: 'base/action/action_upload_files.php?id='+<?php echo $_POST['id']; ?>
+        process: 'base/action/action_upload_files.php'
         // revert: './revert.php',
         // restore: './restore.php?id=',
         // fetch: './fetch.php?data=',
