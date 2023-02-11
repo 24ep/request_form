7ab5b4fe8d108bab9c812eadc469ff17ce0c8c37
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-11 22:50:50 +0700

    debug

--- get/get_attribute.php
+++ get/get_attribute.php
@@ -321,6 +321,6 @@ function update_value_attribute(id, attribute_code, prefix, database, table, pri
 
 <script>
    new SlimSelect({
-    select: 'document.querySelectorAll(".select-slim")'
+    select: 'document.querySelector(".select-slim")'
   })
 </script>
