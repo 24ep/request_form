445e751bddde75bab588c3fb3f39064c8be5ac4d
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-20 01:46:01 +0700

    debug

--- get/get_dynamic_filter.php
+++ get/get_dynamic_filter.php
@@ -105,8 +105,8 @@ function simple_select($attribute_code,$attribute_label,$type){
 $dynamic_filter = $_POST['dynamic_filter'];
 // $filter = explode(',', $dynamic_filter);
 // $filter = array("brand","id","status","launch_date","production_type","dept","subdept","request_username","follow_up_by");
-$filter_string = implode("','",$dynamic_filter);
-$filter_where =  $filter_string;
+// $filter_string = implode("','",$dynamic_filter);
+$filter_where =  $dynamic_filter;
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
 $query = "SELECT * FROM u749625779_cdscontent.job_attribute where table_name = 'add_new_job' and attribute_code in (".$filter_where.")" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
