12928b5c8dc4ab6dfc39ad1db4b067bfaa17a147
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-19 19:22:12 +0700

    debug

--- get/get_dynamic_filter.php
+++ get/get_dynamic_filter.php
@@ -2,7 +2,7 @@
 function text($attribute_code,$attribute_label){
     $input = '
     <div class="form-floating col-md-4">
-        <input type="text" class="form-control" attribute_code="" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
+        <input type="text" class="form-control" attribute_code="'.$attribute_code.'" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
         <label for="floatingInputValue">'.$attribute_label.'</label>
     </div>
     ';
@@ -12,7 +12,7 @@ function text($attribute_code,$attribute_label){
 function number($attribute_code,$attribute_label){
     $input = '
     <div class="form-floating col-md-4">
-        <input type="number" class="form-control" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
+        <input type="number" class="form-control" attribute_code="'.$attribute_code.'" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
         <label for="floatingInputValue">'.$attribute_label.'</label>
     </div>
     ';
@@ -41,7 +41,7 @@ function simple_select($attribute_code,$attribute_label){
     }
     $input = '
     <div class="form-floating col-md-4">
-        <select class="form-select" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
+        <select class="form-select" attribute_code="'.$attribute_code.'" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
         '.$option_element.'
         </select>
         <label for="floatingInputValue">'.$attribute_label.'</label>
