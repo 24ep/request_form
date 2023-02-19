7173592c50aa1679109fd27fcc2d46d8450428a8
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-19 17:27:03 +0700

    debug

--- get/get_dynamic_filter.php
+++ get/get_dynamic_filter.php
@@ -9,6 +9,16 @@ function text($attribute_code,$attribute_label){
 
     return $input;
 }
+function number($attribute_code,$attribute_label){
+    $input = '
+    <div class="form-floating col-md-4">
+        <input type="number" class="form-control" id="filter_'.$attribute_code.'">
+        <label for="floatingInputValue">'.$attribute_label.'</label>
+    </div>
+    ';
+
+    return $input;
+}
 
 function simple_select($attribute_code,$attribute_label){
     $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
@@ -50,8 +60,12 @@ $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
     if($row['attribute_type']=='single_select'){
        echo  simple_select($attribute_code,$attribute_label);
-    }else{}
-    echo text($row['attribute_code'],$row['attribute_label']);
+    }elseif(){
+       echo  number($attribute_code,$attribute_label);
+    }else{
+        echo text($row['attribute_code'],$row['attribute_label']);
+    }
+
 }
 
 
