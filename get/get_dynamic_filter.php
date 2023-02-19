3358945bf42a3d1d1f7d9259577ce30e702df8e9
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-19 17:07:34 +0700

    debug

--- get/get_dynamic_filter.php
+++ get/get_dynamic_filter.php
@@ -2,7 +2,7 @@
 function text($attribute_code,$attribute_label){
     $input = '
     <div class="form-floating col-md-4">
-        <input type="email" class="form-control" id="filter_'.$attribute_code.'">
+        <input type="text" class="form-control" id="filter_'.$attribute_code.'">
         <label for="floatingInputValue">'.$attribute_label.'</label>
     </div>
     ';
@@ -15,7 +15,7 @@ $filter = array("brand","id","status","accepted_date");
 $filter_string = implode("','",$filter);
 $filter_where =  "'".$filter_string."'";
 $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
-$query = "SELECT * FROM u749625779_cdscontent.job_attribute where table_name = 'add_new_job' attribute_code in (".$filter_where.")" or die("Error:" . mysqli_error($con));
+$query = "SELECT * FROM u749625779_cdscontent.job_attribute where table_name = 'add_new_job' and attribute_code in (".$filter_where.")" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
     echo text($row['attribute_code'],$row['attribute_label']);
