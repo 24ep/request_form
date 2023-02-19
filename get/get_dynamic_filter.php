a3fb02f6762a7a3d2ba25b045cd1a34383b8e25b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-19 17:25:45 +0700

    debug

--- get/get_dynamic_filter.php
+++ get/get_dynamic_filter.php
@@ -1,8 +1,38 @@
 <?php
 function text($attribute_code,$attribute_label){
     $input = '
-    <div class="form-floating col">
+    <div class="form-floating col-md-4">
         <input type="text" class="form-control" id="filter_'.$attribute_code.'">
+        <label for="floatingInputValue">'.$attribute_label.'</label>
+    </div>
+    ';
+
+    return $input;
+}
+
+function simple_select($attribute_code,$attribute_label){
+    $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+    $query_op = "SELECT * FROM u749625779_cdscontent.job_attribute_option
+    WHERE attribute_code = '".$attribute_code."' and attribute_table = 'add_new_job' ORDER BY id ASC" or die("Error:" . mysqli_error($con));
+    $result_op = mysqli_query($con, $query_op);
+    $i=0;
+    while($option = mysqli_fetch_array($result_op)) {
+        if($option["attribute_option_code"]==$current_value){
+        $selected = 'selected';
+        }else{
+        $selected = '';
+        }
+        if($option["attribute_option_code"]<>"" and $i==0){
+        $i++;
+        $option_element .= "<option ".$selected ." value=''></option>";
+        }
+        $option_element .= "<option ".$selected ." value='".$option["attribute_option_code"]."'>".$option["attribute_option_label"]."</option>";
+    }
+    $input = '
+    <div class="form-floating col-md-4">
+        <select class="form-select" id="'.$attribute_code.'">
+        '.$option_element.'
+        </select>
         <label for="floatingInputValue">'.$attribute_label.'</label>
     </div>
     ';
@@ -18,6 +48,9 @@ $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " .
 $query = "SELECT * FROM u749625779_cdscontent.job_attribute where table_name = 'add_new_job' and attribute_code in (".$filter_where.")" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
+    if($row['attribute_type']=='single_select'){
+       echo  simple_select($attribute_code,$attribute_label);
+    }else{}
     echo text($row['attribute_code'],$row['attribute_label']);
 }
 
