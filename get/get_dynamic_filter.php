0ed9e6f39dbe470761b0307169c9e07ec9baee8b
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-19 17:35:52 +0700

    debug

--- get/get_dynamic_filter.php
+++ get/get_dynamic_filter.php
@@ -60,8 +60,8 @@ $result = mysqli_query($con, $query);
 while($row = mysqli_fetch_array($result)) {
     if($row['attribute_type']=='single_select'){
        echo  simple_select($attribute_code,$attribute_label);
-    }elseif($row['attribute_type']=='single_select'){
-       echo  number($attribute_code,$attribute_label);
+    }elseif($row['attribute_type']=='number'){
+       echo  number($row['attribute_code'],$row['attribute_label']);
     }else{
         echo text($row['attribute_code'],$row['attribute_label']);
     }
