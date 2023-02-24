0bf2788bd5fb1811225e78c9d9db9a20aef6c03a
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-24 17:14:42 +0700

    debug

--- get/get_dynamic_filter.php
+++ get/get_dynamic_filter.php
@@ -2,7 +2,7 @@
 session_start();
 function datepick($attribute_code,$attribute_label){
     $input = '
-    <div class="col-md-3">
+    <div class="col-md-2">
     <label for="floatingInputValue">'.$attribute_label.'</label>
         <input  class="form-control form-control-sm" attribute_type="date" attribute_code="'.$attribute_code.'" id="filter_'.$attribute_code.'" onfocusout="getFilterInputValues()">
     </div>
@@ -41,7 +41,7 @@ function datepick($attribute_code,$attribute_label){
 }
 function text($attribute_code,$attribute_label){
     $input = '
-    <div class="col-md-3">
+    <div class="col-md-2">
     <label for="floatingInputValue">'.$attribute_label.'</label>
         <input type="text" class="form-control form-control-sm" attribute_code="'.$attribute_code.'" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
 
@@ -52,7 +52,7 @@ function text($attribute_code,$attribute_label){
 }
 function number($attribute_code,$attribute_label){
     $input = '
-    <div class="col-md-3">
+    <div class="col-md-2">
         <label for="floatingInputValue">'.$attribute_label.'</label>
         <input type="number" class="form-control form-control-sm" attribute_code="'.$attribute_code.'" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
     </div>
@@ -81,7 +81,7 @@ function simple_select($attribute_code,$attribute_label,$type){
         $option_element .= "<option ".$selected ." value='".$option["attribute_option_code"]."'>".$option["attribute_option_label"]."</option>";
     }
     $input = '
-    <div class="col-md-3">
+    <div class="col-md-2">
     <label for="floatingInputValue">'.$attribute_label.'</label>
         <select '.$type.' attribute_code="'.$attribute_code.'" id="filter_'.$attribute_code.'" onchange="getFilterInputValues()">
         '.$option_element.'
