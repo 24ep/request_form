57da9ef94555ea71bedbd50c010ec47e8e672684
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-24 17:21:41 +0700

    debug

--- page/ns_list.php
+++ page/ns_list.php
@@ -160,13 +160,26 @@ Create New
 // //add new filter
 
 // }
+function clearParams() {
+  const queryParams = new URLSearchParams(window.location.search);
+  queryParams.delete("page");
 
+  const urlWithoutParams = window.location.origin + window.location.pathname;
+  if (queryParams.toString()) {
+    urlWithoutParams += '?' + queryParams.toString();
+  }
+
+  window.history.replaceState({}, document.title, urlWithoutParams);
+}
+
+// Example usage: clear all parameters except for "page"
 
 function getFilterInputValues() {
   var pagenation_input = document.getElementById("pagenation_input").value
   var filterPrefix = "filter_";
   var inputs = document.querySelectorAll(`input[id^="${filterPrefix}"], select[id^="${filterPrefix}"]`);
   var inputValues = {};
+  clearParams();
 
   inputs.forEach(input => {
     var name = input.getAttribute("attribute_code");
