433eaa647b5b0512ba8d7ddb99fd25fee293b504
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-26 23:43:38 +0700

    debug

--- page/job_on_hand.php
+++ page/job_on_hand.php
@@ -34,6 +34,22 @@ $role_op = get_option("role",$_SESSION["role"],"single");
         <li role="presentation" class="nav-item nav-task" onclick="get_list_job_on_hand('waiting for other stage');">Waiting for other stage<span id="c_waiting for other stage" class="badge text-bg-dark ms-2"></span></a>
         <li role="presentation" class="nav-item nav-task" onclick="get_list_job_on_hand('rejected');">Rejected<span id="c_rejected" class="badge text-bg-dark ms-2"></span></li>
     </ul>
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+  </li>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
+  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+</div>
     <div class="position-absolute end-0 me-3 mt-1" style="margin-right: 120px!important;">
         <select  id="username_on_hand" onchange="get_list_job_on_hand('current_status');">
             <?php echo $username_op;?>
