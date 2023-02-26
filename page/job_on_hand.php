213d16f2c5d8af4a248fbdb0f39e512ac547a599
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-26 23:53:34 +0700

    debug

--- page/job_on_hand.php
+++ page/job_on_hand.php
@@ -28,36 +28,37 @@ $role_op = get_option("role",$_SESSION["role"],"single");
 
 <nav class="nav p-2 bg-light shadow" style="border-bottom: 1px solid #e3e3e3;">
     <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-        <li role="presentation" class="nav-item nav-task" onclick="get_list_job_on_hand('pending');">Pending<span id="c_pending" class="badge text-bg-dark ms-2"></span></li>
-        <li role="presentation" class="nav-item nav-task" onclick="get_list_job_on_hand('inprogress');">Inprogress<span id="c_inprogress" class="badge text-bg-dark ms-2"></span></a>
-        <li role="presentation" class="nav-item nav-task" onclick="get_list_job_on_hand('waiting');">Waiting information<span id="c_waiting" class="badge text-bg-dark ms-2"></span></li>
-        <li role="presentation" class="nav-item nav-task" onclick="get_list_job_on_hand('waiting for other stage');">Waiting for other stage<span id="c_waiting for other stage" class="badge text-bg-dark ms-2"></span></a>
-        <li role="presentation" class="nav-item nav-task" onclick="get_list_job_on_hand('rejected');">Rejected<span id="c_rejected" class="badge text-bg-dark ms-2"></span></li>
+        <li role="presentation" class="nav-task" onclick="get_list_job_on_hand('pending');">
+            <button class="nav-link active" data-bs-toggle="pill" type="button" role="tab" aria-selected="true">Pending
+                <span id="c_pending" class="badge text-bg-dark ms-2"></span>
+            </button>
+        </li>
+        <li role="presentation" class="nav-task" onclick="get_list_job_on_hand('inprogress');">
+            <button class="nav-link active" data-bs-toggle="pill" type="button" role="tab"
+                aria-selected="true">Inprogress<span id="c_inprogress" class="badge text-bg-dark ms-2"></span> </button>
+        </li>
+        <li role="presentation" class="nav-task" onclick="get_list_job_on_hand('waiting');">
+            <button class="nav-link active" data-bs-toggle="pill" type="button" role="tab" aria-selected="true">Waiting
+                information<span id="c_waiting" class="badge text-bg-dark ms-2"></span> </button>
+        </li>
+        <li role="presentation" class="nav-task" onclick="get_list_job_on_hand('waiting for other stage');">
+            <button class="nav-link active" data-bs-toggle="pill" type="button" role="tab" aria-selected="true">Waiting
+                for other stage<span id="c_waiting for other stage" class="badge text-bg-dark ms-2"></span> </button>
+        </li>
+        <li role="presentation" class="nav-task" onclick="get_list_job_on_hand('rejected');">
+            <button class="nav-link active" data-bs-toggle="pill" type="button" role="tab"
+                aria-selected="true">Rejected<span id="c_rejected" class="badge text-bg-dark ms-2"></span> </button>
+        </li>
     </ul>
-    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
-  </li>
-</ul>
-<div class="tab-content" id="pills-tabContent">
-  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
-</div>
+
     <div class="position-absolute end-0 me-3 mt-1" style="margin-right: 120px!important;">
-        <select  id="username_on_hand" onchange="get_list_job_on_hand('current_status');">
+        <select id="username_on_hand" onchange="get_list_job_on_hand('current_status');">
             <?php echo $username_op;?>
         </select>
     </div>
     <div class="position-absolute end-0 me-3 mt-1">
         <select id="role_on_hand" onchange="get_list_job_on_hand('current_status');">
-        <?php echo $role_op;?>
+            <?php echo $role_op;?>
         </select>
     </div>
 </nav>
@@ -81,8 +82,8 @@ $role_op = get_option("role",$_SESSION["role"],"single");
 // }
 
 function get_list_job_on_hand(status) {
-    if(status=='current_status'){
-        status = document.getElementById('selected_status').value ;
+    if (status == 'current_status') {
+        status = document.getElementById('selected_status').value;
     }
     ac_username = document.getElementById('username_on_hand').value;
     ac_role = document.getElementById('role_on_hand').value;
@@ -132,12 +133,11 @@ function call_edit_add_new_panel(id, brand) {
     }
 }
 
-  new SlimSelect({
+new SlimSelect({
     select: "#username_on_hand"
-  })
+})
 
-  new SlimSelect({
+new SlimSelect({
     select: "#role_on_hand"
-  })
-  </script>
-
+})
+</script>
