345723b25dc447026f9e8fd14aba6f109bf88b3d
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-27 13:39:09 +0700

    debug

--- get/get_list_ns.php
+++ get/get_list_ns.php
@@ -107,7 +107,7 @@ $limit="LIMIT 30";
 $query = "SELECT * FROM add_new_job as anj where (((".$filter.") or id in (".$parent_filter."))
  and anj.parent is null )  ORDER BY anj.id DESC  ".$limit." OFFSET ".$start_item
 or die("Error:" . mysqli_error($con));
-echo "<script>console.log(&#39;".$query."&#39;)</script>";
+echo "<script>console.log(#39;".$query."#39;)</script>";
 date_default_timezone_set("Asia/Bangkok");
 mysqli_query($con, "SET NAMES 'utf8' ");
 $result = mysqli_query($con, $query);
@@ -201,20 +201,6 @@ while($row = mysqli_fetch_array($result)) {
                     $sub_ticket .= "<div class='col'>". "<button type='button' id='ns_ticket_".$row_child['id']."' class='badge rounded bg-gradient bg-dark p-2 ps-3 pe-3'  onclick='call_edit_add_new_modal(".$row_child["id"].")' >
                     Detail </button></div >";
                     $i++;
-                }else{
-                        //data row
-                if(!isset($sub_ticket)){$sub_ticket ="";}
-                if(!isset($tr_class)){$tr_class="";}
-                $sub_ticket .= "<li ".$tr_class.">";
-                $sub_ticket .= "<div scope='row' ".$th_class." style='min-width: 380px;'><span class='tree_label'>NS-".$row["id"]."-".$i." (".$row_child["id"].") ".$row_child["sku"]." SKUs</span></div>";
-                $sub_ticket .= "<div class='col'></div>";
-                $sub_ticket .= "<div class='col'></div>";
-                $sub_ticket .= "<div class='col'></div>";
-                $sub_ticket .= "<div class='col'></div>";
-                $sub_ticket .= "<div class='col' >".$status."</div>";
-                $sub_ticket .= "<div class='col'>". "<button type='button' id='ns_ticket_".$row_child['id']."' class='badge rounded bg-gradient bg-dark p-2 ps-3 pe-3' onclick='call_edit_add_new_modal(".$row_child["id"].")' >
-                Detail </button></div>";
-                $i++;
                 }
             }else{
                 //data row
