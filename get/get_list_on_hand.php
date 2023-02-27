b715bce040261ae66964030f7082d2eb3d0d0334
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-02-27 10:37:14 +0700

    debug

--- get/get_list_on_hand.php
+++ get/get_list_on_hand.php
@@ -256,7 +256,14 @@ $configurable_map = array (
     array(
         'ac_role'=>'approver',
         'status'=>'pending',
-        'filter'=>'jc.approved_date is null and jc.approved_editing_status ="correct" and anj.status = "on-productions"',
+        'filter'=>'jc.approved_date is null and jc.approved_editing_status ="correct"
+                   and anj.status = "on-productions"
+                   and (
+                    (anj.production_type = "Data only" and anj.content_complete_date is not null ) or
+                    (anj.production_type <> "Data only" and anj.content_complete_date is not null and
+                    jc.upload_image_date is not null and jc.content_complete_date is not null)
+                   )
+                     ',
         'key_stage'=>'jc.approved_by',
         'key_name'=>'null'
     ),
