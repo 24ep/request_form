459aca52b890e1d7c359c2810989809dd99d52a7
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-03-05 01:45:46 +0700

    debug

--- page/cr_list.php
+++ page/cr_list.php
@@ -111,7 +111,8 @@
                                 $query = "SELECT id, project_name, prefix , color_project FROM all_in_one_project.project_bucket;" or die("Error:" . mysqli_error($con));
                                 $result = mysqli_query($con, $query);
                                 echo '<div class="tab-pane fade show active" id="v-pills-all" role="tabpanel" aria-labelledby="v-pills-all-tab" tabindex="0">';
-                                include("../get/get_list_update_content.php?bucket=all");
+                                echo include("../get/get_list_update_content.php?bucket=all");
+
                                 echo'</div>';
                                 while($row = mysqli_fetch_array($result)) {
                                     echo'<div class="tab-pane fade" id="v-pills-'.$row['prefix'].'" role="tabpanel" aria-labelledby="v-pills-'.$row['prefix'].'-tab" tabindex="0">';
