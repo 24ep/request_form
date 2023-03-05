00ab7745ec57b0a1f30999b4fb4bb2f2c4112811
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-03-05 21:38:45 +0700

    debug

--- page/cr_list.php
+++ page/cr_list.php
@@ -65,12 +65,12 @@
                                 where pb.status = 'Open'
                                 group by pb.id, pb.project_name, pb.prefix , pb.color_project;" or die("Error:" . mysqli_error($con));
                                 $result = mysqli_query($con, $query);
-                                $bucket  = '<button class="nav-link border  text-start active" id="v-pills-all-tab" data-bs-toggle="pill" data-bs-target="#v-pills-all" type="button" role="tab" aria-controls="v-pills-all" aria-selected="true">';
+                                $bucket  = '<button class="nav-link border  text-start active" onclick="set_bucket(&#39;all&#39;);getFilterInputValues();" id="v-pills-all-tab" data-bs-toggle="pill" data-bs-target="#v-pills-all" type="button" role="tab" aria-controls="v-pills-all" aria-selected="true">';
                                 $bucket  .= '<div class="row"><div style="place-self: center;"class="col-2"><img class="me-2 rounded" src="https://ui-avatars.com/api/?name=ALL>&background=999999&color=fff&rounded=false&size=25">';
                                 $bucket  .= '</div><div class="col-10">All Bucket</div></div></button>';
                                 while($row = mysqli_fetch_array($result)) {
                                     $bucket  .=
-                                    '<button class="nav-link border text-start" onclick="set_bucket(&#39;'.$row['prefix'].'&#39;);getFilterInputValues()" id="v-pills-'.$row['prefix'].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-'.$row['prefix'].'" type="button" role="tab" aria-controls="v-pills-'.$row['prefix'].'" aria-selected="true">';
+                                    '<button class="nav-link border text-start" onclick="set_bucket(&#39;'.$row['prefix'].'&#39;);getFilterInputValues();" id="v-pills-'.$row['prefix'].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-'.$row['prefix'].'" type="button" role="tab" aria-controls="v-pills-'.$row['prefix'].'" aria-selected="true">';
                                     $bucket  .= '
                                             <div class="row">
                                                 <div style="place-self: center;" class="col-2">
