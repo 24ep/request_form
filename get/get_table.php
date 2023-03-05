a5abe0b5aa717f579ab20aea1aff0fd09de3c756
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-03-05 14:33:15 +0700

    debug

---
+++ get/get_table.php
@@ -0,0 +1,56 @@
+<?php
+$table_id = $_GET['table_id'];
+function displayTable($host, $username, $password, $database, $table, $columns,$table_id ) {
+    // Establish a connection to the database
+    $con = mysqli_connect($host, $username, $password) or die("Error: " . mysqli_error($con));
+    mysqli_select_db($con, $database);
+
+    // Construct the SQL query
+    $query = "SELECT " . implode(", ", $columns) . " FROM " . $table;
+
+    // Execute the query and retrieve the results
+    $result = mysqli_query($con, $query);
+
+    // Create an HTML table to display the data with Bootstrap styles
+    echo "<table class='table table-striped table-bordered' id='".$table_id."'>";
+    echo "<thead class='thead-dark'>";
+    echo "<tr>";
+    foreach ($columns as $column) {
+        echo "<th>" . ucfirst($column) . "</th>"; // Capitalize the column names
+    }
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    // Loop through the query results and display each row as a table row
+    while($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        foreach ($columns as $column) {
+            echo "<td>" . $row[$column] . "</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+
+    // Close the database connection
+    mysqli_close($con);
+}
+
+
+?>
+ <script>
+  $(document).ready( function () {
+  $('#<?php echo $table_id; ?>').DataTable({
+        lengthMenu: [
+            [10,20, 50, 100, -1],
+            [10, 20,50, 100, 'All'],
+        ],
+    });
+
+} );
+
+
+
+</script>
---
+++ page/bucket.php
@@ -0,0 +1,39 @@
+<nav class="navbar p-3 pb-0 m-0 bg-secondary bg-opacity-25 text-secondary  m-0">
+  <div class="container-fluid">
+  <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a onclick="get_page('configurable')">Configurable</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Bucket management</li>
+        </ol>
+    </nav>
+
+    <div class="d-flex">
+      <!-- <button class="btn btn-outline-success" onclick="attribute_detail_page('','','','create')" >Create new attribute</button> -->
+
+    </div>
+  </div>
+</nav>
+<h5 class="p-2 ps-4 pb-4 bg-secondary bg-opacity-25 text-secondary  m-0" style="text-transform: uppercase"><strong>Account management</strong></h5>
+
+
+<?php
+include("../get/get_table.php");
+displayTable(
+    "localhost", "cdse_admin", "@aA417528639",
+    "all_in_one_project", "project_bucket",
+     array(
+        "id",
+        "prefix",
+        "color_project",
+        "project_name",
+        "status"));
+?>
+
+  <div class="container-fluid">
+    <div class="d-flex">
+
+      <!-- <button class="btn btn-outline-success disabled">Create new account</button> -->
+    </div>
+  </div>
+
+
