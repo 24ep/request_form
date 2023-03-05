a2d6a87e5bb63bef478213235be819f73c4c39d8
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-03-05 21:33:56 +0700

    debug

--- get/get_list_cr.php
+++ get/get_list_cr.php
@@ -87,10 +87,9 @@ function list_ts_non_status($filter,$ts_command_limit ,$status,$bucket_filter){
     on ac.username = ticket.case_officer
     Left join all_in_one_project.project_bucket pb
     on pb.prefix  = ticket.ticket_template
-    -- and ticket.status not in ('archive','cancel')
     where (".$filter.") and lower(ticket.status) not in ('cancel','archive') ".$bucket_filter;
     $result = mysqli_query($con, $query);
-echo $query;
+    echo $query;
     echo "  <li class='row mb-3' style='color: #b3b3b3;font-weight: 600;text-align-last: center;'>
     <div class='col'>Id</div>
     <div class='col-4'>Title</div>
