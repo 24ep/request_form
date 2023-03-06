71b61d3a36a39b138667e0185b1cc810a51f17d7
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-03-06 15:43:26 +0700

    debug

--- get/get_table.php
+++ get/get_table.php
@@ -28,9 +28,11 @@ function displayTable($host, $username, $password, $database, $table, $columns,$
     while($row = mysqli_fetch_array($result)) {
         echo "<tr>";
         foreach ($columns as $column) {
-            echo "<td><input class='border-0 bg-white' id='tb_".$row['id']."_".$column."' value='" .$row[$column] . "'</td>";
+            echo '<td><input class="border-0 bg-white"
+            update_value_attribute('.$row['id'] .', &#39;na_edit_'.$row[$column].'&#39; , &#39;na&#39; , &#39;'.$database.'&#39; , &#39;'.$table.'&#39; , &#39;id&#39;);
+            value="' .$row[$column] . '"</td>';
         }
-        echo "<td><button class='btn btn-dark btn-sm' onclick='project_bucket_detail_page(".$row['id'].",". $row[$column] .",".$table_id.",update)'>Edit</button></td>";
+        echo "<td><button class='btn btn-dark btn-sm' onclick='project_bucket_detail_page(".$row['id'].",&#39;". $row[$column] ."&#39;,&#39;".$table_id."&#39;,&#39;update&#39;)'>Edit</button></td>";
         echo "</tr>";
     }
 
