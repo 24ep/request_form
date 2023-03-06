03f597ee40af25cd6e70803096d0b0140ba3ef20
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-03-06 15:55:03 +0700

    debug

---
+++ page/origin_of_issue.php
@@ -0,0 +1,72 @@
+<nav class="navbar p-3 pb-0 m-0 bg-secondary bg-opacity-25 text-secondary  m-0">
+  <div class="container-fluid">
+  <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a onclick="get_page('configurable')">Configurable</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Origin of issue</li>
+        </ol>
+    </nav>
+
+    <div class="d-flex">
+      <!-- <button class="btn btn-outline-success" onclick="attribute_detail_page('','','','create')" >Create new attribute</button> -->
+
+    </div>
+  </div>
+</nav>
+<h5 class="p-2 ps-4 pb-4 bg-secondary bg-opacity-25 text-secondary  m-0" style="text-transform: uppercase"><strong>Origin of issue</strong></h5>
+
+
+<?php
+include("../get/get_table.php");
+displayTable(
+    "localhost", "cdse_admin", "@aA417528639",
+    "all_in_one_project", "origin_of_issue",
+     array(
+        "id",
+        "origin",
+        "type",
+        "impact",
+        "incident_management",
+        "bu_pic",
+        "cto_pic",
+        "eta_root_cause_fix",
+        "status",
+        "system_related"
+      ),
+      "origin_of_issue");
+?>
+
+  <div class="container-fluid">
+    <div class="d-flex">
+
+      <!-- <button class="btn btn-outline-success disabled">Create new account</button> -->
+    </div>
+  </div>
+  <script>
+  $(document).ready( function () {
+  $('#project_bucket').DataTable({
+        lengthMenu: [
+            [10,20, 50, 100, -1],
+            [10, 20,50, 100, 'All'],
+        ],
+    });
+
+} );
+
+function project_bucket_detail_page(id,attribute_code,table_name,action){
+    // create new attribute
+      $.post("base/page/origin_of_issue_detail.php", {
+                id:id,
+                attribute_code: attribute_code,
+                table_name: table_name,
+                action:action
+            },
+            function(data) {
+                $('#col_detail').html(data);
+            });
+  }
+
+
+</script>
+
+
---
+++ page/origin_of_issue_detail.php
@@ -0,0 +1,75 @@
+<!-- get accout informations -->
+
+<?php
+ session_start();
+   $database = 'all_in_one_project';
+   $table = 'origin_of_issue';
+   $primary_key_id = 'id';
+   $prefix_table = 'oi';
+
+   $id=$_POST['id'];
+
+
+
+   $con= mysqli_connect("localhost","cdse_admin","@aA417528639") or die("Error: " . mysqli_error($con));
+
+      $query = "SELECT  * FROM ".$database.".".$table." where ".$primary_key_id." = '".$id."'" or die("Error:" . mysqli_error($con));
+      $result = mysqli_query($con, $query);
+      while($row = mysqli_fetch_array($result)) {
+          $query_column = "SELECT `COLUMN_NAME`
+          FROM `INFORMATION_SCHEMA`.`COLUMNS`
+          WHERE `TABLE_SCHEMA`='".$database."'
+              AND `TABLE_NAME`='".$table."'" or die("Error:" . mysqli_error($con));
+          $result_column = mysqli_query($con, $query_column);
+          while($row_column = mysqli_fetch_array($result_column)) {
+              ${$prefix_table."_".$row_column['COLUMN_NAME']} = $row[$row_column['COLUMN_NAME']];
+          }
+      }
+?>
+
+
+<nav class="p-3 bg-white shadow-sm">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><?php echo $ac_project_name;?></a>
+    <small  href="#"><?php echo $ac_description; ?></small>
+  </div>
+</nav>
+
+<?php
+include('../get/get_attribute.php');
+
+//get attribute set menu
+$query = "SELECT distinct attribute_set,table_name,db_name,primary_key_id,prefix FROM u749625779_cdscontent.job_attribute where allow_display= 1 and table_name in ('".$table ."') order by sort_attribute_set" or die("Error:" . mysqli_error($con));
+  $result = mysqli_query($con, $query);
+  $d_attribute_set="";
+  $d_attribute_section="";
+  $i=0;
+  while($row = mysqli_fetch_array($result)) {
+    $i++;
+    if($i==1){
+      $active_section_bt = ' active';
+      $active_section_bd = ' show active';
+    }else{
+      $active_section_bt = ' ';
+      $active_section_bd = ' ';
+    }
+    echo '<div id="call_update_ac_complete"></div>';
+    echo '<ul class="list-group">';
+    $d_attribute_set .=  '  <button style="text-align:-webkit-left" class="nav-link '.$active_section_bt.'" id="v-pills-'.str_replace(" ","_",$row['attribute_set']).'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-'.str_replace(" ","_",$row['attribute_set']).'" type="button" role="tab" aria-controls="v-pills-'.str_replace(" ","_",$row['attribute_set']).'" aria-selected="false">'.ucwords($row['attribute_set']).'</button>';
+    $d_attribute_section .= '<div class="tab-pane fade  shadow-sm bg-white p-3 rounded '.$active_section_bd.'" id="v-pills-'.str_replace(" ","_",$row['attribute_set']).'" role="tabpanel" aria-labelledby="v-pills-'.str_replace(" ","_",$row['attribute_set']).'-tab">'.get_attribute_section($row['attribute_set'],$row['table_name'],$row['db_name'],$row['primary_key_id'],$row['prefix']).'</div>';
+    echo '</ul>';
+
+  }
+  echo'
+  <div class="d-flex align-items-start pt-4 pe-5 me-5 ms-2">
+      <div class="nav flex-column nav-pills me-4" style="text-align-last: left;" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+
+      '.$d_attribute_set.'
+      </div>
+      <div class="tab-content" id="v-pills-tabContent">
+      '.$d_attribute_section.'
+      </div>
+  </div>
+';
+
+?>
