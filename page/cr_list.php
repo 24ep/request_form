5c72acdfd72ff3f434a42a1d8723fe4a2e708288
Jaroonwit Poolnai <poojaroonwit@central.co.th>
2023-03-06 14:09:43 +0700

    debug

--- page/cr_list.php
+++ page/cr_list.php
@@ -172,7 +172,16 @@
 
 <script>
 function filter_backlogs(){
-    document.getElementById("filter_status").value = 'Pending,Inprogress,Waiting Execution,Waiting CTO,Waiting Buyer'
+    var stringFromDB = 'Pending,Inprogress,Waiting Execution,Waiting CTO,Waiting Buyer';
+    var array = stringFromDB.split(",");
+    var select = document.getElementById("filter_status");
+    for(count=0, count<array.length, count++) {
+    for(i=0; i<select.options.length; i++) {
+        if(select.options[i].value == array[count]) {
+        select.options[i].selected="selected";
+        }
+    }
+    }
 }
 function getFilterInputValues() {
 
