3f8495c1b54725765e15046534f5add03440b386
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-04 20:18:15 +0700

    debug

--- page/taxonomy_qc.php
+++ page/taxonomy_qc.php
@@ -62,7 +62,7 @@
             var sheetName = 'original_data';
 
             var checkDate = new Date().toISOString(); // Replace with the new check date
-            var range = sheetName + '!A' + (i + 1) + ':F' + (i + 1);
+            var range = sheetName + '!A:F';
             var values = [];
             var request = gapi.client.sheets.spreadsheets.values.get({
                 spreadsheetId: spreadsheetId,
