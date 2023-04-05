147415c9d8066212549cc59261af96cf4f57e711
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-05 10:18:56 +0700

    debug

--- page/taxonomy_qc.php
+++ page/taxonomy_qc.php
@@ -43,7 +43,7 @@
 
 
 
-<script>
+<!-- <script>
 
     // Load the Google Sheets API client library
     gapi.load('client', start);
@@ -92,7 +92,7 @@
             };
             var updateRequest = gapi.client.sheets.spreadsheets.values.update({
                 spreadsheetId: spreadsheetId,
-                range: sheetName + '!A' + (i ) + ':F' + (i ),
+                range: sheetName + '!F' + (i +1),
                 valueInputOption: 'USER_ENTERED',
                 resource: {
                   values: [row]
@@ -120,7 +120,75 @@
 
 
 
+</script> -->
+
+<script>
+    // Load the Google Sheets API client library
+    gapi.load('client', start);
+
+    function start(sku, status) {
+        // Initialize the API client library
+        gapi.client.init({
+            apiKey: 'AIzaSyB0sTxGv1N6vNFfUeij9U6KycrfezZi92U',
+            discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+            clientId: '514529310578-jkv0lqnhr27jkaec3e0qu292d2ip295g.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/spreadsheets',
+            plugin_name: 'streamy'
+        }).then(function () {
+            // Call the Sheets API to update a value in a cell
+            var spreadsheetId = 'YOUR_SPREADSHEET_ID';
+            var sheetName = 'DEMO';
+            var checkDate = new Date().toISOString(); // Replace with the new check date
+            var range = sheetName + '!A:F';
+
+            var request = gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: spreadsheetId,
+                range: range,
+                majorDimension: 'ROWS',
+            });
+
+            request.then(function (response) {
+                var data = response.result.values;
+                if (data && data.length > 0) {
+                    // Get the column index for the 'status' column
+                    var headerRow = data[0];
+                    var statusIndex = headerRow.indexOf('status');
+                    for (var i = 0; i < data.length; i++) {
+                        var row = data[i];
+                        if (row[0] === sku) {
+                            row[statusIndex] = status; // Set the status value in the 'status' column
+                            var updateRequest = gapi.client.sheets.spreadsheets.values.update({
+                                spreadsheetId: spreadsheetId,
+                                range: sheetName + '!F' + (i + 1),
+                                valueInputOption: 'USER_ENTERED',
+                                resource: {
+                                    values: [[status]]
+                                }
+                            });
+
+                            updateRequest.then(function (response) {
+                                console.log(response.result);
+                            }, function (reason) {
+                                console.error('Error: ' + reason.result.error.message);
+                            });
+                            break; // Exit the loop once the SKU is found
+                        }
+                    }
+                } else {
+                    console.log('No data found.');
+                }
+            }, function (reason) {
+                console.error('Error: ' + reason.result.error.message);
+            });
+        });
+    }
+
+    // Call the updateSheetData() function with the SKU and status values as arguments
+    function updateSheetData(sku, status) {
+        start(sku, status);
+    }
 </script>
+
 
 
 
