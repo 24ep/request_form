0df09c7fbacfc9a96fb8d6f6e98ea3f30ebd3ae3
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-06 23:34:50 +0700

    debug

--- get/get_taxonomy.php
+++ get/get_taxonomy.php
@@ -88,7 +88,7 @@ while($row = mysqli_fetch_array($result)) {
             }
 
             $new_attribute .= "</select>";
-            $new_attribute .= "<small id='original_'".$row_att["attribute_code"]."' style='font-size:10px;display:none;color: #c3c3c3;'>Original value : ".$row[$row_att['attribute_code']]."</small>";
+            $new_attribute .= "<small id='original_".$row_att["attribute_code"]."' style='font-size:10px;display:none;color: #c3c3c3;'>Original value : ".$row[$row_att['attribute_code']]."</small>";
             $new_attribute .="</div>";
             $new_attribute .="<div class='col-6 mt-4' style='display:".$display."'>";
             $new_attribute .='<input  onclick="ShowSmallOriginalValue(&#39;'.$row_att["attribute_code"].'&#39;)" type="radio"  style="display:'.$display.'" class="btn-check" name="options-outlined-'.$row_att["attribute_code"].'"
