1e6ea0d020f039d651290790d7f22b94a01bc85c
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-11 16:36:00 +0700

    debug

--- get/get_taxonomy.php
+++ get/get_taxonomy.php
@@ -10,18 +10,18 @@ $model_selected =  $_POST['model_selected'];
 
 
 if($model_selected=="retail"){
-    $model_selected = "('retail')";
+    $model_selected = "retail";
     $query_condition ="tr.model = 'retail' and ((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
     (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
     (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and tr.auto_enrichment = 'Y'";
 }
 if($model_selected=="mkp"){
-    $model_selected = "('mkp')";
-    $query_condition ="tr.status = 'PENDING'  and
+    $model_selected = "mkp";
+    $query_condition ="tr.mkp_qc = 'Y' and tr.status = 'PENDING'  and
     (tr.check_by is null or tr.check_by ='".$_SESSION['username']."') and tr.model = 'mkp')";
 }
 if($model_selected=="non_selected"){
-    $model_selected = "('retail')";
+    $model_selected = "non_selected";
     $query_condition ="tr.model = 'retail' and ((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
     (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
     (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and tr.auto_enrichment = 'Y'";
