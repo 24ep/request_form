2837dd2076bb0a087e9c4a8bfa9358f7dd3c162e
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-11 14:39:31 +0700

    debug

--- get/get_taxonomy.php
+++ get/get_taxonomy.php
@@ -8,12 +8,7 @@ $con= mysqli_connect("localhost","cdse_admin","@aA417528639","taxonomy") or die(
 $model_selected =  $_POST['model_selected'];
 
 
-if($model_selected=="non_selected"){
-    $model_selected = "('retail','mkp')";
-    $query_condition ="((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
-    (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
-    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and `auto_enrichment` = 'Y')";
-}
+
 if($model_selected=="retail"){
     $model_selected = "('retail')";
     $query_condition ="((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
