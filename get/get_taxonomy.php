135f46fe87c1011f02b2b3b2318b8ab605729538
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-11 15:52:01 +0700

    debug

--- get/get_taxonomy.php
+++ get/get_taxonomy.php
@@ -13,18 +13,18 @@ if($model_selected=="retail"){
     $model_selected = "('retail')";
     $query_condition ="((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
     (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
-    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and `auto_enrichment` = 'Y' and model = 'retail'";
+    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and tr.auto_enrichment = 'Y' and tr.model = 'retail'";
 }
 if($model_selected=="mkp"){
     $model_selected = "('mkp')";
     $query_condition ="((tr.status = 'PENDING' ) and
-    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')";
+    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."') and tr.model = 'retail'";
 }
 if($model_selected=="non_selected"){
     $model_selected = "('retail','mkp')";
     $query_condition =" ((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
     (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
-    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and `auto_enrichment` = 'Y' and model = 'retail'";
+    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and tr.auto_enrichment = 'Y' and tr.model = 'retail'";
 }
 mysqli_query($con, "SET NAMES 'utf8' ");
 $new_attribute="";
