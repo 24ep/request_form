f6fbde4314c9bc526c9dae7c60e832d39fa84a08
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-11 15:48:40 +0700

    debug

--- get/get_taxonomy.php
+++ get/get_taxonomy.php
@@ -13,7 +13,7 @@ if($model_selected=="retail"){
     $model_selected = "('retail')";
     $query_condition ="((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
     (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
-    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and `auto_enrichment` = 'Y'";
+    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and `auto_enrichment` = 'Y' and model = 'retail'";
 }
 if($model_selected=="mkp"){
     $model_selected = "('mkp')";
@@ -24,7 +24,7 @@ if($model_selected=="non_selected"){
     $model_selected = "('retail','mkp')";
     $query_condition =" ((tr.status <> 'QC_PASSED' and tr.status <> 'REVISED') or tr.status is null) and
     (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
-    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and `auto_enrichment` = 'Y'";
+    (tr.check_by is null or tr.check_by ='".$_SESSION['username']."')) and `auto_enrichment` = 'Y' and model = 'retail'";
 }
 mysqli_query($con, "SET NAMES 'utf8' ");
 $new_attribute="";
