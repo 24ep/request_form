296189ada8f684621856cf8bea03f14a93675800
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-04-27 16:25:25 +0700

    debug

--- get/get_taxonomy.php
+++ get/get_taxonomy.php
@@ -18,7 +18,7 @@ if($model_selected=="mkp"){
 }
 if($model_selected=="non_selected"){
     $model_selected = "non_selected";
-    $query_condition ="batch is null and tr.qty > 0 and tr.model = 'retail' and (tr.status = 'WAITING FOR QC' or tr.status = 'SKIP') and
+    $query_condition ="batch is null and tr.qty > 0 and tr.model = 'retail' and (tr.status = 'WAITING FOR QC' ) and
     (tr.in_80_sale_contribute = 'Y' or tr.in_top_200 = 'Y') and
     (tr.check_by is null or tr.check_by ='".$_SESSION['username']."'))";
 }
