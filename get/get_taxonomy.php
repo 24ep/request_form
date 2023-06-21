a490303d429d1e46f8ae540049410bb937777fbc
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-06-21 19:47:45 +0700

    debug

--- get/get_taxonomy.php
+++ get/get_taxonomy.php
@@ -125,13 +125,13 @@ while($row = mysqli_fetch_array($result)) {
 }
 // echo $new_attribute;
 //stamp name
-$sql = "update taxonomy.taxonomy_raw set
+$sql = "update taxonomy.taxonomy_raw_f2 set
 check_by = '".$_SESSION['username']."'
 where brand = '".$brand."'"
 or die("Error:" . mysqli_error($con));
 $query = mysqli_query($con,$sql);
 //stamp name
-$sql = "update taxonomy.taxonomy_raw set
+$sql = "update taxonomy.taxonomy_raw_f2 set
 check_by = '".$_SESSION['username']."',
 feed_time = CURRENT_TIMESTAMP
 where sku = '".$sku."'"
