f147f25b7263c2bece6a491561a43d936f95ac8d
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2023-12-13 11:52:03 +0700

    debug

--- get/get_function_badge.php
+++ get/get_function_badge.php
@@ -7,13 +7,13 @@
           $date_wiht_formate = date_format($date_wiht_formate,"Y-m-d");
           $date_wiht_formate_diff = (strtotime($date_wiht_formate)-strtotime($current_day))/  ( 60 * 60 * 24 );
           if($date_wiht_formate_diff==0){
-            $ef_badge = '<span class="badge rounded-pill bg-danger " style="margin-left:5px">'.$date.'</span>';
+            $ef_badge = '<span class="badge rounded-pill bg-danger " style="margin-left:5px">'.$date_wiht_formate.'</span>';
           }elseif($date_wiht_formate_diff==1){
-            $ef_badge = '<span class="badge rounded-pill bg-warning text-dark" style="margin-left:5px">'.$date.'</span>';
+            $ef_badge = '<span class="badge rounded-pill bg-warning text-dark" style="margin-left:5px">'.$date_wiht_formate.'</span>';
           }elseif($date_wiht_formate_diff<0){
-            $ef_badge = '<span class="badge rounded-pill bg-danger" style="margin-left:5px">'.$date.'</span>';
+            $ef_badge = '<span class="badge rounded-pill bg-danger" style="margin-left:5px">'.$date_wiht_formate.'</span>';
           }else{
-            $ef_badge  = $date;
+            $ef_badge  = '<span class="badge rounded-pill bg-secondary" style="margin-left:5px">'.$date_wiht_formate.'</span>';
           }
         }
         return $ef_badge;
