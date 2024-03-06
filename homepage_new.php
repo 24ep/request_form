5a5385e467cc8a097d21790a8747a4e3380aea97
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-03-06 17:28:00 +0700

    debug

--- homepage_new.php
+++ homepage_new.php
@@ -981,19 +981,7 @@ s.onload = function(sdk) {
 }
 document.head.append(s)
 </script>
-<?php
-if($_GET['direct']<>null and $_GET['prefix']<>null){ ?>
-<script>
-if (<?php echo $_GET['prefix']; ?> == 'NS') {
-    call_edit_add_new_modal(<?php echo$_GET['direct']; ?>);
-}
-if {
-    cr_id_toggle(<?php echo$_GET['direct']; ?>);
-}
-</script>
-<?php
-                            }
-                            ?>
+
 <!-- <script>
                             Notiflix.Report.warning(
                                 'แจ้งปิดรับงาน',
