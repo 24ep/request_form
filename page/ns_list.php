2d302c034f9c1e9a5e2676454c3f1cb58ebd1042
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-03-07 14:58:23 +0700

    debug

--- page/ns_list.php
+++ page/ns_list.php
@@ -315,6 +315,7 @@ function action_submit_add_new_job() {
             var result = data.includes("Error");
             if (result == false) {
                 Notiflix.Notify.success("Ticket have been create already ! NS-" + data);
+                getFilterInputValues();
                 snapshot_data("all_in_one_project", "add_new_job", "id", data, "add_new_job");
             } else {
                 Notiflix.Report.failure(
