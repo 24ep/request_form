74efe7ec52669f5c79c7dc13cbfa565457c9eb9b
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-03-08 15:56:50 +0700

    debug

--- action/action_check_exist_email.php
+++ action/action_check_exist_email.php
@@ -1,14 +1,15 @@
 <?php
- $con= mysqli_connect("service-gate.a.aivencloud.com","avnadmin","AVNS_lAORtpjxYyc9Pvhm5O4","all_in_one_project","10628") or die("Error: " . mysqli_error($con));
- mysqli_query($con, "SET NAMES 'utf8' ");
+$con = mysqli_connect("service-gate.a.aivencloud.com", "avnadmin", "AVNS_lAORtpjxYyc9Pvhm5O4", "all_in_one_project", "10628") or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
 
 $query = "SELECT work_email FROM all_in_one_project.account";
 $result = mysqli_query($con, $query) or die("Error: " . mysqli_error($con));
 
 // Check if there are rows returned
-if (mysqli_num_rows($result) > 0) {
+$stmt = $result->num_rows;
+
+if ($stmt > 0) {
     // Fetch and process the data
-  
     echo "exist-email";
 } else {
     // No data
@@ -17,7 +18,4 @@ if (mysqli_num_rows($result) > 0) {
 
 // Close the database connection
 mysqli_close($con);
-
-
-
 ?>
