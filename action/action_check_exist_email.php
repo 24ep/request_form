bcd53a83016c9c8eab9d05a9ff8f06f18e230864
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-03-08 16:38:13 +0700

    debug

--- action/action_check_exist_email.php
+++ action/action_check_exist_email.php
@@ -1,8 +1,8 @@
 <?php
-$con = mysqli_connect("service-gate.a.aivencloud.com", "avnadmin", "AVNS_lAORtpjxYyc9Pvhm5O4", "all_in_one_project", "10628") or die("Error: " . mysqli_error($con));
+$con = mysqli_connect("service-gate-cds-omni-service-gate.a.aivencloud.com", "avnadmin", "AVNS_lAORtpjxYyc9Pvhm5O4", "all_in_one_project", "10628") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 
-$query = "SELECT COUNT(work_email) AS email_count FROM all_in_one_project.account";
+$query = "SELECT COUNT(work_email) AS email_count FROM all_in_one_project.account where work_email ='".$_POST['email']."'";
 $result = mysqli_query($con, $query) or die("Error: " . mysqli_error($con));
 
 // Fetch the count from the result set
