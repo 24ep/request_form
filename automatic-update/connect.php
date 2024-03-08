3e0f6cc9497d60a595dfb77383301b9a1fb3c5db
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-03-08 10:28:46 +0700

    debug

--- automatic-update/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con= mysqli_connect("service-gate-cds-omni-service-gate.a.aivencloud.com","avnadmin","AVNS_lAORtpjxYyc9Pvhm5O4","all_in_one_project","10628") or die("Error: " . mysqli_error($con));
-mysqli_query($con, "SET NAMES 'utf8' ");
- ?>
