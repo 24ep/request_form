f33c22128f94604869ce5b060af9478f2a6db87e
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-03-08 18:54:11 +0700

    debug

--- reset_password_confirm.php
+++ reset_password_confirm.php
@@ -102,12 +102,7 @@ mysqli_close($con);
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-fTnAisiE51sXgj7GBnZaAT0s9IxpZjq5P2LIzSNcFfFA3B3At9U0XJktOM0nF8yA"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
+
     <!-- end Bootstrap js -->
 
     <script>
