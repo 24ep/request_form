5c085e3dca183ac4344dde91eb79f095215c50b5
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-03-08 19:00:43 +0700

    debug

--- reset_password_confirm.php
+++ reset_password_confirm.php
@@ -99,9 +99,7 @@ mysqli_close($con);
     </div>
 
     <!-- Bootstrap js -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
     <!-- end Bootstrap js -->
 
